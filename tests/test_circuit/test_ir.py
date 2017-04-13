5b037598c0daf672ab798e1e3ccf975aeb3a1825
Lenny Truong <leonardtruong@protonmail.com>
2017-04-13 14:51:54 -0700

    Add test for ir compile

---
+++ tests/test_circuit/test_ir.py
@@ -0,0 +1,36 @@
+from magma import * 
+from magma.ir import compile 
+
+
+def test_print_ir():
+
+    And2 = DeclareCircuit('And2', "I0", In(Bit), "I1", In(Bit), "O", Out(Bit)) 
+
+    AndN2 = DefineCircuit("And2", "I", In(Array2), "O", Out(Bit) ) 
+    and2 = And2() 
+    wire( AndN2.I[0], and2.I0 ) 
+    wire( AndN2.I[1], and2.I1 ) 
+    wire( and2.O, AndN2.O ) 
+    EndCircuit() 
+
+    main = DefineCircuit("main", "I0", In(Bit), "I1", In(Bit), "O", Out(Bit)) 
+    and2 = AndN2() 
+    main.O( and2(array(main.I0, main.I1)) ) 
+    EndCircuit() 
+
+    result = compile(main)
+    assert result == """And2 = DefineCircuit("And2", "I", Array(2,In(Bit)), "O", Out(Bit))
+inst0 = And2()
+wire(And2.I[0], inst0.I0)
+wire(And2.I[1], inst0.I1)
+wire(inst0.O, And2.O)
+EndCircuit()
+
+main = DefineCircuit("main", "I0", In(Bit), "I1", In(Bit), "O", Out(Bit))
+inst0 = And2()
+wire(main.I0, inst0.I[0])
+wire(main.I1, inst0.I[1])
+wire(inst0.O, main.O)
+EndCircuit()
+
+"""
