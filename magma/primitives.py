290027a9339c7a55b3929ce8db1a1f4db82efe70
Lenny Truong <lenny@cs.stanford.edu>
2017-07-24 14:33:09 -0700

    Style improvements for primitives.py

--- magma/primitives.py
+++ magma/primitives.py
@@ -15,19 +15,25 @@ def type_check_binary_operator(operator):
     of bits of the same length
     """
     def type_checked_operator(self, other):
-        if not (isinstance(other, list) and len(self) == len(other)) and type(self) != type(other):
+        if not (isinstance(other, list) and len(self) == len(other)) and \
+           type(self) != type(other):
             raise TypeError("unsupported operand type(s) for {}: '{}' and"
-                    "'{}'".format(operator.__name__, type(self), type(other)))
+                            "'{}'".format(operator.__name__, type(self),
+                                          type(other)))
         return operator(self, other)
     return type_checked_operator
 
+
 def declare_bit_binop(name, op):
-    circ = DeclareCircuit("{}1".format(name), 'I0', In(Bit), 'I1', In(Bit), 'O', Out(Bit))
+    circ = DeclareCircuit("{}1".format(name), 
+                          'I0', In(Bit), 'I1', In(Bit), 'O', Out(Bit))
+
     @type_check_binary_operator
     def func(self, other):
         return circ()(self, other)
     func.__name__ = op
     setattr(BitType, op, func)
+
 
 declare_bit_binop("And", "__and__")
 declare_bit_binop("Or", "__or__")
@@ -35,8 +41,11 @@ declare_bit_binop("Xor", "__xor__")
 
 BitInvert = DeclareCircuit("Invert1", 'I', In(Bit), 'O', Out(Bit))
 
+
 def __invert__(self):
     return BitInvert()(self)
+
+
 BitType.__invert__ = __invert__
 
 
@@ -44,12 +53,15 @@ def declare_bits_binop(name, op):
     @lru_cache(maxsize=None)
     def Declare(N):
         T = Bits(N)
-        return DeclareCircuit("{}{}".format(name, N), 'I0', In(T), 'I1', In(T), 'O', Out(T))
+        return DeclareCircuit("{}{}".format(name, N),
+                              'I0', In(T), 'I1', In(T), 'O', Out(T))
+
     @type_check_binary_operator
     def func(self, other):
         return Declare(self.N)()(self, other)
     func.__name__ = op
     setattr(BitsType, op, func)
+
 
 declare_bits_binop("And", "__and__")
 declare_bits_binop("Or", "__or__")
@@ -61,22 +73,28 @@ def DeclareInvertN(N):
     T = Bits(N)
     return DeclareCircuit("Invert{}".format(N), 'I', In(T), 'O', Out(T))
 
+
 def __invert__(self):
     return DeclareInvertN(self.N)()(self)
+
+
 BitsType.__invert__ = __invert__
 
 
 @lru_cache(maxsize=None)
 def DeclareShiftLeftN(N, amount):
     T = Bits(N)
-    return DeclareCircuit("ShiftLeft{}_{}".format(N, amount), 'I', In(T), 'O', Out(T))
+    return DeclareCircuit("ShiftLeft{}_{}".format(N, amount),
+                          'I', In(T), 'O', Out(T))
 
 
 @lru_cache(maxsize=None)
 def DeclareDynamicShiftLeftN(N):
     T = Bits(N)
     i1_type = Bits((N - 1).bit_length())
-    return DeclareCircuit("DynamicShiftLeft{}".format(N), 'I0', In(T), 'I1', In(i1_type), 'O', Out(T))
+    return DeclareCircuit("DynamicShiftLeft{}".format(N),
+                          'I0', In(T), 'I1', In(i1_type), 'O', Out(T))
+
 
 def __lshift__(self, other):
     if isinstance(other, IntegerTypes):
@@ -85,20 +103,25 @@ def __lshift__(self, other):
         return DeclareDynamicShiftLeftN(self.N)()(self, other)
     else:
         raise TypeError("<< not implemented for argument 2 of type {}".format(type(other)))
+
+
 BitsType.__lshift__ = __lshift__
 
 
 @lru_cache(maxsize=None)
 def DeclareShiftRightN(N, amount):
     T = Bits(N)
-    return DeclareCircuit("ShiftRight{}_{}".format(N, amount), 'I', In(T), 'O', Out(T))
+    return DeclareCircuit("ShiftRight{}_{}".format(N, amount),
+                          'I', In(T), 'O', Out(T))
 
 
 @lru_cache(maxsize=None)
 def DeclareDynamicShiftRightN(N):
     T = Bits(N)
     i1_type = Bits((N - 1).bit_length())
-    return DeclareCircuit("DynamicShiftRight{}".format(N), 'I0', In(T), 'I1', In(i1_type), 'O', Out(T))
+    return DeclareCircuit("DynamicShiftRight{}".format(N),
+                          'I0', In(T), 'I1', In(i1_type), 'O', Out(T))
+
 
 def __rshift__(self, other):
     if isinstance(other, IntegerTypes):
@@ -106,7 +129,10 @@ def __rshift__(self, other):
     elif isinstance(other, Type):
         return DeclareDynamicShiftRightN(self.N)()(self, other)
     else:
-        raise TypeError(">> not implemented for argument 2 of type {}".format(type(other)))
+        raise TypeError(">> not implemented for argument 2 of type {}".format(
+            type(other)))
+
+
 BitsType.__rshift__ = __rshift__
 
 
@@ -114,13 +140,16 @@ def declare_binop(name, _type, type_type, op, out_type=None):
     @lru_cache(maxsize=None)
     def Declare(N):
         T = _type(N)
-        return DeclareCircuit("{}{}".format(name, N), 'I0', In(T), 'I1', In(T), 'O', Out(out_type if out_type else T))
+        return DeclareCircuit("{}{}".format(name, N),
+                              'I0', In(T), 'I1', In(T),
+                              'O', Out(out_type if out_type else T))
 
     @type_check_binary_operator
     def func(self, other):
         return Declare(self.N)()(self, other)
     func.__name__ = op
     setattr(type_type, op, func)
+
 
 declare_binop("SignedAdd", SInt, SIntType, "__add__")
 declare_binop("SignedSub", SInt, SIntType, "__sub__")
