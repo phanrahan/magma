ed4291ca8737ee825cf5715b900e7221e8fd9fcf
Lenny Truong <lenny@cs.stanford.edu>
2017-07-25 20:25:04 -0700

    [BitVector] Preserve num_bits in result of logical ops

--- magma/bit_vector.py
+++ magma/bit_vector.py
@@ -2,31 +2,34 @@ from magma.compatibility import IntegerTypes, StringTypes
 from magma.bits import seq2int, int2seq
 
 class BitVector:
-    def __init__(self, value=0):
+    def __init__(self, value=0, num_bits=None):
         if isinstance(value, IntegerTypes):
-            self._num_bits = max(value.bit_length(), 1)
+            if num_bits is None:
+                num_bits = max(value.bit_length(), 1)
             self._value = value
             self._bits = int2seq(value)
         elif isinstance(value, list):
             if not all(x == 0 or x == 1 for x in value):
                 raise Exception("BitVector list initialization must be a list of 0s and 1s")
-            self.num_bits = len(value)
+            if num_bits is None:
+                num_bits = len(value)
             self._value = seq2int(value)
             self._bits = value
         else:
             raise Exception("BitVector initialization with type {} not supported".format(type(value)))
+        self.num_bits = num_bits
 
     def __and__(self, other):
         assert isinstance(other, BitVector)
-        return BitVector(self._value & other._value)
+        return BitVector(self._value & other._value, num_bits=self.num_bits)
 
     def __or__(self, other):
         assert isinstance(other, BitVector)
-        return BitVector(self._value | other._value)
+        return BitVector(self._value | other._value, num_bits=self.num_bits)
 
     def __xor__(self, other):
         assert isinstance(other, BitVector)
-        return BitVector(self._value ^ other._value)
+        return BitVector(self._value ^ other._value, num_bits=self.num_bits)
 
     def as_int(self):
         return self._value
