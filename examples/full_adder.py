b660e8af637b1ebe6be47b551d9659c29d5234b3
Lenny Truong <lenny@cs.stanford.edu>
2017-08-02 16:38:33 -0700

    Rename notebook, add example .py file

---
+++ examples/full_adder.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from magma import *
+
+class FullAdder(Circuit):
+    name = "FullAdder"
+    IO = ["a", In(Bit), "b", In(Bit), "cin", In(Bit), "out", Out(Bit), "cout", Out(Bit)]
+    @classmethod
+    def definition(io):
+        # Generate the sum
+        a_xor_b = io.a ^ io.b
+        wire(a_xor_b ^ io.cin, io.out)
+        # Generate the carry
+        a_and_b = io.a & io.b
+        b_and_cin = io.b & io.cin
+        a_and_cin = io.a & io.cin
+        wire(a_and_b | b_and_cin | a_and_cin, io.cout)
+
+
+# In[2]:
+
+
+from magma.python_simulator import PythonSimulator
+from magma.scope import Scope
+
+simulator = PythonSimulator(FullAdder)
+scope = Scope()
+test_vectors = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 0, 1, 0],
+    [1, 1, 0, 0, 1],
+    [0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+
+for a, b, cin, out, cout in test_vectors:
+    simulator.set_value(FullAdder.a, scope, bool(a))
+    simulator.set_value(FullAdder.b, scope, bool(b))
+    simulator.set_value(FullAdder.cin, scope, bool(cin))
+    simulator.evaluate()
+    assert simulator.get_value(FullAdder.out, scope) == bool(out)
+    assert simulator.get_value(FullAdder.cout, scope) == bool(cout)
+
+print("Success!")
+
