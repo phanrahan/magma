66b1abf7f5ad0bd12f17ffc231716cf6b4f81ce4
Pat Hanrahan <hanrahan@cs.stanford.edu>
2017-08-30 11:21:58 -0700

    Added simple pass framework and some tests/examples.

---
+++ magma/passes/clock.py
@@ -0,0 +1,11 @@
+from ..circuit import isdefinition
+from ..clock import wiredefaultclock
+from .passes import DefinitionPass
+
+__all__ = ['WireClockPass']
+
+class WireClockPass(DefinitionPass):
+    def __call__(self, definition):
+        for instance in definition.instances:
+            wiredefaultclock(definition, instance)
+
---
+++ magma/passes/passes.py
@@ -0,0 +1,49 @@
+from ..circuit import isdefinition
+
+__all__ = ['Pass', 'InstancePass', 'DefinitionPass']
+
+# abstract base class
+class Pass:
+    def __init__(self, main):
+        self.main = main
+
+class InstancePass(Pass):
+    def __init__(self, main):
+        super(InstancePass, self).__init__(main)
+        self.instances = []
+
+    def _run(self, definition):
+        for instance in definition.instances:
+            instancedefinition = type(instance)
+            if isdefinition(instancedefinition):
+                self._run( instancedefinition )
+            self.instances.append(instance)
+            if callable(self):
+                self(instance)
+        return self
+    
+    def run(self):
+         return self._run(self.main)
+
+class DefinitionPass(Pass):
+    def __init__(self,main):
+        super(DefinitionPass, self).__init__(main)
+        self.definitions = {}
+
+    def _run(self, definition):
+        for instance in definition.instances:
+            instancedefinition = type(instance)
+            if isdefinition(instancedefinition):
+                self._run( instancedefinition )
+
+        # call each definition only once
+        name = definition.__name__
+        if name not in self.definitions:
+            self.definitions[name] = definition
+            if callable(self):
+                self(definition)
+
+        return self
+
+    def run(self):
+         return self._run(self.main)
