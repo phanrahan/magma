d749196a0e4dbad81aad5dc64c3da759c171c8b5
Pat Hanrahan <hanrahan@cs.stanford.edu>
2017-09-01 12:52:56 -0700

    Changed flat to operate on a circuit. Added function cut. Added braid tests.

---
+++ tests/test_flatten.py
@@ -0,0 +1,22 @@
+from magma import *
+
+def test_flatten():
+    b0 = Bit(name='b0')
+    b1 = Bit(name='b1')
+    b2 = Bit(name='b2')
+    b3 = Bit(name='b3')
+    b4 = Bit(name='b4')
+    a1 = array([b0, b1])
+    a2 = array([b0, b1])
+    print(a1==a2)
+    A = Array(2,Bit)
+    B = Array(2,Bit)
+    print(A==B)
+    t = tuple_(x=array([b0, b1]), y=array([b2, b3, b4]))
+    print(type(t.name))
+    print(type(t))
+    a = array(t.flatten())
+    print(a.name)
+    print(type(a))
+
+test_flatten()
