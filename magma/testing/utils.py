e10a5a21ed6e48ebb30f5194667e8f56e43ae72d
Lenny Truong <lenny@cs.stanford.edu>
2017-09-14 09:08:30 -0700

    Update gold files and revert coreir test guard

--- magma/testing/utils.py
+++ magma/testing/utils.py
@@ -6,23 +6,23 @@ import os
 import sys
 import difflib
 
-def check_files_equal(callee_file, file1, file2):
+def check_files_equal(callee_file, file1_name, file2_name):
     """
     Check if file1 == file2 where file1 and file2 are in the same path as
     callee_file (typically __file__ for the script calling this function)
     """
     file_path = os.path.dirname(callee_file)
-    file1 = os.path.join(file_path, file1)
-    file2 = os.path.join(file_path, file2)
-    result = filecmp.cmp(file1, file2, shallow=False)
+    file1_path = os.path.join(file_path, file1_name)
+    file2_path = os.path.join(file_path, file2_name)
+    result = filecmp.cmp(file1_path, file2_path, shallow=False)
     if not result:
-        with open(file1, "r") as file1:
-            with open(file2, "r") as file2:
+        with open(file1_path, "r") as file1:
+            with open(file2_path, "r") as file2:
                 diff = difflib.unified_diff(
                     file1.readlines(),
                     file2.readlines(),
-                    fromfile=file1,
-                    tofile=file2,
+                    fromfile=file1_name,
+                    tofile=file2_name,
                 )
                 for line in diff:
                     sys.stderr.write(line)
