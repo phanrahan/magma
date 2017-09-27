861ca1c0c299033f8b8e251b07df11aa9452a19f
Lenny Truong <lenny@cs.stanford.edu>
2017-09-27 15:41:30 -0700

    Remove coreir tests that depend on primitives

--- tests/test_simulator/test_error.py
+++ tests/test_simulator/test_error.py
@@ -1,29 +1,19 @@
 from magma import *
-from magma.primitives import DefineRegister
-
 from magma.simulator import PythonSimulator
-from magma.scope import Scope
-from magma.bit_vector import BitVector
-from magma.bitutils import int2seq, seq2int
 
 def test_instance():
     N = 4
-    Register4 = DefineRegister(4)
     T = Bits(N)
 
-    class ShiftRegister(Circuit):
-        name = "ShiftRegister"
+    class Test(Circuit):
+        name = "Test"
         IO = ["I", In(T), "O", Out(T), "CLK", In(Bit)]
         @classmethod
         def definition(io):
-            regs = [Register4() for _ in range(N)]
-            wireclock(io, regs)
-            wire(io.I, getattr(regs[0], "in"))
-            fold(regs, foldargs={"in":"out"})
-            wire(regs[-1].out, io.O)
+            wire(io.I, io.O)
 
     try:
-        simulator = PythonSimulator(ShiftRegister())
+        simulator = PythonSimulator(Test())
         assert False, "Should raise a ValueError when passing an instance to the Python Simulator"
     except ValueError as e:
         pass
