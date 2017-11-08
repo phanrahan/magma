e29ac56b5ace1bd34988476a5447c310f36fe688
Lenny Truong <lenny@cs.stanford.edu>
2017-11-07 19:30:36 -0800

    Update magma coroutine testing semantics to match silica coroutines

--- magma/testing/coroutine.py
+++ magma/testing/coroutine.py
@@ -42,16 +42,6 @@ def check(circuit, sim, number_of_cycles, inputs_generator=None):
     simulator = PythonSimulator(circuit, clock=circuit.CLK)
     failed = False
     for cycle in range(number_of_cycles):
-        for i in range(2):
-            simulator.step()
-            simulator.evaluate()
-        # Coroutine has an implicit __next__ call on construction so it already
-        # is in it's initial state
-        for name, port in circuit.interface.ports.items():
-            if port.isinput():  # circuit output
-                if getattr(sim, name) != BitVector(simulator.get_value(getattr(circuit, name))):
-                    print(f"Failed on cycle {cycle}, port {name}, expected {getattr(sim, name)}, got {BitVector(simulator.get_value(getattr(circuit, name)))}")
-                    failed = True
         if inputs_generator is None:
             next(sim)
         else:
@@ -70,6 +60,16 @@ def check(circuit, sim, number_of_cycles, inputs_generator=None):
                 sim.send(inputs[0])
             else:
                 next(sim)
+        for i in range(2):
+            simulator.step()
+            simulator.evaluate()
+        # Coroutine has an implicit __next__ call on construction so it already
+        # is in it's initial state
+        for name, port in circuit.interface.ports.items():
+            if port.isinput():  # circuit output
+                if getattr(sim, name) != BitVector(simulator.get_value(getattr(circuit, name))):
+                    print(f"Failed on cycle {cycle}, port {name}, expected {getattr(sim, name)}, got {BitVector(simulator.get_value(getattr(circuit, name)))}")
+                    failed = True
     assert not failed, "Failed to pass simulation"
 
 def testvectors(circuit, sim, number_of_cycles, inputs_generator=None):
