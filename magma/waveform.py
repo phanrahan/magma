b8515128de83825e049ce185d41a475e55e038f0
Lenny Truong <lenny@cs.stanford.edu>
2017-12-05 14:26:55 -0800

    make matplotlib an optional dependency

--- magma/waveform.py
+++ magma/waveform.py
@@ -1,23 +1,27 @@
-import matplotlib.pyplot as plt
-import numpy as np
+try:
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-def waveform(signals, labels):
+    def waveform(signals, labels):
 
-    signals = np.transpose(np.array(signals))
+        signals = np.transpose(np.array(signals))
 
-    n = len(signals[0])
+        n = len(signals[0])
 
-    nsignals = len(signals)
+        nsignals = len(signals)
 
-    t = np.repeat(0.5*np.arange(n+1),2)[1:-1]
-    signals = [np.repeat(s,2) for s in signals]
+        t = np.repeat(0.5*np.arange(n+1),2)[1:-1]
+        signals = [np.repeat(s,2) for s in signals]
 
-    for i in range(nsignals):
-        plt.text(-1.5, i+.25, labels[i])
-        plt.plot(t, 0.5*signals[i]+i, 'r', linewidth = 2)
+        for i in range(nsignals):
+            plt.text(-1.5, i+.25, labels[i])
+            plt.plot(t, 0.5*signals[i]+i, 'r', linewidth = 2)
 
-    # plt.xlim([0,n])
-    # plt.ylim([-.25,nsignals])
+        # plt.xlim([0,n])
+        # plt.ylim([-.25,nsignals])
 
-    plt.gca().axis('off')
-    plt.show()
+        plt.gca().axis('off')
+        plt.show()
+except ImportError as e:
+    def waveform(signals, labels):
+        raise Exception("Please install matplotlib to use this feature.")
