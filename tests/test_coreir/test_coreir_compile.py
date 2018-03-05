6be788e031deb8a50146a12c3cae31f3dbe9a2e8
David Durst <davidbdurst@gmail.com>
2018-03-05 15:23:06 -0800

    fixed mapParallel test, but need mapParllel_test_gold

--- tests/test_coreir/test_coreir_compile.py
+++ tests/test_coreir/test_coreir_compile.py
@@ -4,7 +4,7 @@ import os
 
 def test_compile_coreir():
     width = 16
-    parallel_inputs = 4
+    numInputs = 4
     doubleT = magma.Bits(width)
     double = magma.DefineCircuit("double", "I", magma.In(doubleT), "O", magma.Out(doubleT))
     shift_amount = 2
@@ -24,24 +24,24 @@ def test_compile_coreir():
     def import_(lib, name):
         return get_lib(lib).generators[name]
 
-    mapNParams = c.new_values({"width": width, "parallelOperators": parallel_inputs, "operator": coreir_double})
+    mapParallelParams = c.new_values({"numInputs": numInputs, "operator": coreir_double})
 
-    test_module_typ = c.Record({"in": c.Array(parallel_inputs, c.Array(width,
-        c.BitIn())), "out": c.Array(parallel_inputs, c.Array(width, c.Bit()))})
+    test_module_typ = c.Record({"in": c.Array(numInputs, c.Array(width,
+        c.BitIn())), "out": c.Array(numInputs, c.Array(width, c.Bit()))})
     test_module = c.global_namespace.new_module("test_module", test_module_typ)
     test_module_def = test_module.new_definition()
-    mapN = import_("aetherlinglib", "mapN")
-    mapMod = mapN(width=width, parallelOperators=parallel_inputs, operator=coreir_double)
+    mapParallel = import_("aetherlinglib", "mapParallel")
+    mapMod = mapParallel(numInputs=numInputs, operator=coreir_double)
     mapDouble = test_module_def.add_module_instance("mapDouble", mapMod)
-    test_module_def.connect(test_module_def.interface.select("in"), mapDouble.select("in"));
-    test_module_def.connect(mapDouble.select("out"), test_module_def.interface.select("out"));
+    test_module_def.connect(test_module_def.interface.select("in"), mapDouble.select("I"));
+    test_module_def.connect(mapDouble.select("O"), test_module_def.interface.select("out"));
     test_module_def.print_()
     test_module.definition = test_module_def
     test_module.print_()
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    test_module.save_to_file(os.path.join(dir_path, "mapN_test.json"))
-    with open(os.path.join(dir_path, "mapN_test.json"), "r") as actual:
-        with open(os.path.join(dir_path, "mapN_test_gold.json"), "r") as gold:
+    test_module.save_to_file(os.path.join(dir_path, "mapParallel_test.json"))
+    with open(os.path.join(dir_path, "mapParallel_test.json"), "r") as actual:
+        with open(os.path.join(dir_path, "mapParallel_test_gold.json"), "r") as gold:
             assert actual.read() == gold.read()
-    mod = c.load_from_file(os.path.join(dir_path, "mapN_test.json"))
+    mod = c.load_from_file(os.path.join(dir_path, "mapParallel_test.json"))
     mod.print_()
