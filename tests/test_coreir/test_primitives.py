c7bf72f54967e0b5f6e741248951f21821b9a0db
Lenny Truong <lenny@stanford.edu>
2018-03-12 22:20:33 -0700

    Add comparison tests

--- tests/test_coreir/test_primitives.py
+++ tests/test_coreir/test_primitives.py
@@ -5,6 +5,7 @@ import magma.testing.verilator
 import delegator
 import inspect
 import os
+from random import randint
 
 unary_primitives = [
     ("not", operator.invert),
@@ -19,7 +20,7 @@ binary_primitives = [
     ("sub", operator.sub),
     ("mul", operator.mul),
     ("shl", operator.lshift),
-    ("lshr", operator.rshift),
+    # ("lshr", operator.rshift),
 ]
 
 comparison_primitives = [
@@ -47,15 +48,27 @@ def pytest_generate_tests(metafunc):
     if 'input1' in metafunc.fixturenames:
         metafunc.parametrize("input1", range(16))
 
-def complete(func, n, width):
+def complete(func, n, width, signed=False):
     max = 1 << width
     tests = []
     for i in range(n):
         for j in range(n):
-            test = [BitVector(i, width), BitVector(j, width)]
+            test = [BitVector(i, width, signed), BitVector(j, width, signed)]
             result = func(*test)
             test.append(result)
             tests.append(test)
+    return tests
+
+def random(func, n, width, signed=False):
+    max = 1 << width
+    tests = []
+    for i in range(n):
+        x = randint(0, max)
+        y = randint(0, max)
+        test = [BitVector(x, width, signed), BitVector(y, width, signed)]
+        result = func(*test)
+        test.append(result)
+        tests.append(test)
     return tests
 
 def test_binary_primitive(binary_primitive, width):
@@ -82,7 +95,43 @@ def test_binary_primitive(binary_primitive, width):
     build_dir = os.path.join(file_path, 'build')
     result = delegator.run(f"coreir -i {primitive_name}.json -o {primitive_name}.v", cwd=build_dir)
     assert not result.return_code, result.out + "\n" + result.err
-    tests = complete(primitive_op, 4, 16)
+    if width == 4:
+        tests = complete(primitive_op, 16, width)
+    else:
+        assert width == 16
+        tests = random(primitive_op, 512, width)
+    m.testing.verilator.compile(f"build/sim_{primitive_name}.cpp", circ, tests)
+    m.testing.verilator.run_verilator_test(f"{primitive_name}",
+            f"sim_{primitive_name}", f"{primitive_name}_wrapper")
+
+def test_comparison_primitive(comparison_primitive, width):
+
+    primitive_name, primitive_op, signed = comparison_primitive
+    prim = m.DeclareCircuit(f"primitive_name",
+            "in0", m.In(m.Array(width, m.Bit)),
+            "in1", m.In(m.Array(width, m.Bit)),
+            "out",  m.Out(m.Bit),
+            coreir_lib="coreir", coreir_name=primitive_name, coreir_genargs={"width": width})
+    circ = m.DefineCircuit(f"{primitive_name}_wrapper",
+            "I0", m.In(m.Array(width, m.Bit)),
+            "I1", m.In(m.Array(width, m.Bit)),
+            "O",  m.Out(m.Bit))
+    inst = prim()
+    m.wire(circ.I0, inst.in0)
+    m.wire(circ.I1, inst.in1)
+    m.wire(circ.O, inst.out)
+    m.EndDefine()
+    m.compile(f"build/{primitive_name}", circ, output="coreir")
+
+    file_path = os.path.dirname(__file__)
+    build_dir = os.path.join(file_path, 'build')
+    result = delegator.run(f"coreir -i {primitive_name}.json -o {primitive_name}.v", cwd=build_dir)
+    assert not result.return_code, result.out + "\n" + result.err
+    if width == 4:
+        tests = complete(primitive_op, 16, width, signed)
+    else:
+        assert width == 16
+        tests = random(primitive_op, 512, width, signed)
     m.testing.verilator.compile(f"build/sim_{primitive_name}.cpp", circ, tests)
     m.testing.verilator.run_verilator_test(f"{primitive_name}",
             f"sim_{primitive_name}", f"{primitive_name}_wrapper")
