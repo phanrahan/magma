f85c276bc78c738b198fccae9130662f343d946d
Lenny Truong <lenny@stanford.edu>
2018-04-12 14:00:26 -0700

    Remove parse_requirements logic

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
 from setuptools import setup
 import sys
-from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-
-install_requires = []
-extra_requires = {}
-for item in parse_requirements("requirements.txt", session=False):
-    req = str(item.req)
-    if item.markers is not None:
-        req += ";" + str(item.markers)
-    install_requires.append(req)
 
 setup(
     name='magma',
