96f45fc573fe9b14e180e586fd7d592c72bd6c19
David Durst <davidbdurst@gmail.com>
2018-04-18 00:21:37 -0700

    accept whole name for wrapped modules

--- magma/frontend/coreir_.py
+++ magma/frontend/coreir_.py
@@ -3,25 +3,25 @@ from magma.circuit import DefineCircuitKind, Circuit, definitionCache
 from magma import cache_definition
 
 @cache_definition
-def DefineModuleWrapper(cirb: CoreIRBackend, coreirModule, uniqueStrForArgs):
+def DefineModuleWrapper(cirb: CoreIRBackend, coreirModule, uniqueName):
     class ModuleWrapper(Circuit):
-        name = coreirModule.name + "_" + uniqueStrForArgs
+        name = uniqueName
         IO = cirb.get_ports_as_list(cirb.get_ports(coreirModule.type))
         wrappedModule = coreirModule
     return ModuleWrapper
 
 def DefineCircuitFromGeneratorWrapper(cirb: CoreIRBackend, namespace: str, generator: str,
-                                      dependentNamespaces: list, uniqueStrForArgs: str,
+                                      dependentNamespaces: list, uniqueName: str,
                                       genargs: dict = {}):
     moduleToWrap = cirb.context.import_generator(namespace,generator)(**genargs)
     cirb.context.run_passes(["rungenerators"], [namespace] + dependentNamespaces)
-    return DefineModuleWrapper(cirb, moduleToWrap, uniqueStrForArgs)
+    return DefineModuleWrapper(cirb, moduleToWrap, uniqueName)
 
 def CircuitInstanceFromGeneratorWrapper(cirb: CoreIRBackend, namespace: str, generator: str,
-                                        dependentNamespaces: list, uniqueStrForArgs: str,
+                                        dependentNamespaces: list, uniqueName: str,
                                         genargs: dict = {}, modargs: dict = {}):
     return DefineCircuitFromGeneratorWrapper(cirb, namespace, generator,
-                                             dependentNamespaces, uniqueStrForArgs,
+                                             dependentNamespaces, uniqueName,
                                              genargs)(**modargs)
 
 def GetCoreIRModule(cirb: CoreIRBackend, circuit: DefineCircuitKind):
