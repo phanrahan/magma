5aba22954eedc167f3d25e84c7f04bc184ef6f63
David Durst <davidbdurst@gmail.com>
2018-04-17 18:34:44 -0700

    gave unique name for wrapped modules

--- magma/frontend/coreir_.py
+++ magma/frontend/coreir_.py
@@ -1,25 +1,28 @@
 from magma.backend.coreir_ import CoreIRBackend
-from magma.circuit import DefineCircuitKind, Circuit
+from magma.circuit import DefineCircuitKind, Circuit, definitionCache
 from magma import cache_definition
 
 @cache_definition
-def DefineModuleWrapper(cirb: CoreIRBackend, coreirModule):
+def DefineModuleWrapper(cirb: CoreIRBackend, coreirModule, uniqueStrForArgs):
     class ModuleWrapper(Circuit):
-        name = coreirModule.name
+        name = coreirModule.name + "_" + uniqueStrForArgs
         IO = cirb.get_ports_as_list(cirb.get_ports(coreirModule.type))
         wrappedModule = coreirModule
     return ModuleWrapper
 
 def DefineCircuitFromGeneratorWrapper(cirb: CoreIRBackend, namespace: str, generator: str,
-                                dependentNamespaces: list, genargs: dict = {}):
+                                      dependentNamespaces: list, uniqueStrForArgs: str,
+                                      genargs: dict = {}):
     moduleToWrap = cirb.context.import_generator(namespace,generator)(**genargs)
     cirb.context.run_passes(["rungenerators"], [namespace] + dependentNamespaces)
-    return DefineModuleWrapper(cirb, moduleToWrap)
+    return DefineModuleWrapper(cirb, moduleToWrap, uniqueStrForArgs)
 
 def CircuitInstanceFromGeneratorWrapper(cirb: CoreIRBackend, namespace: str, generator: str,
-                                        dependentNamespaces: list, genargs: dict = {}, modargs: dict = {}):
+                                        dependentNamespaces: list, uniqueStrForArgs: str,
+                                        genargs: dict = {}, modargs: dict = {}):
     return DefineCircuitFromGeneratorWrapper(cirb, namespace, generator,
-                                             dependentNamespaces, genargs)(**modargs)
+                                             dependentNamespaces, uniqueStrForArgs,
+                                             genargs)(**modargs)
 
 def GetCoreIRModule(cirb: CoreIRBackend, circuit: DefineCircuitKind):
     """
