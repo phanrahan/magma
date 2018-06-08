007f6ae52f0166db94f830565ac9dcfab4cd96a9
Pat Hanrahan <hanrahan@cs.stanford.edu>
2018-06-08 15:34:15 -0700

    Added compile_and_regress.

--- magma/testing/__init__.py
+++ magma/testing/__init__.py
@@ -1,1 +1,2 @@
 from .utils import check_files_equal
+from .compile import compile_and_regress
---
+++ magma/testing/compile.py
@@ -0,0 +1,12 @@
+import os
+from os.path import join
+from .utils import check_files_equal
+from magma import compile
+
+BUILD = 'build'
+GOLD = 'gold'
+
+def compile_and_regress(callee_file, name, vendor, main, *largs, **kwargs):
+    compile(join(BUILD,name), main(*largs, **kwargs), vendor=vendor)
+    assert check_files_equal(callee_file, join(BUILD,name)+'.v', join(GOLD,name)+'.v')
+    assert check_files_equal(callee_file, join(BUILD,name)+'.pcf', join(GOLD,name)+'.pcf')
