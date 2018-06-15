166e8d996930ac267f749e6e44313a8c507d59a3
Pat Hanrahan <hanrahan@cs.stanford.edu>
2018-06-15 10:23:32 -0700

    Removed vendor argument to compile in compile_and_regress.

--- magma/testing/compile.py
+++ magma/testing/compile.py
@@ -1,17 +1,17 @@
-import os
-from os.path import join
 from .utils import check_files_equal
 from magma import compile
 
-BUILD = 'build'
-GOLD = 'gold'
-
 def compile_and_regress(callee_file, name, vendor, main, *largs, **kwargs):
-    compile(join(BUILD,name), main(*largs, **kwargs), vendor=vendor)
-    assert check_files_equal(callee_file, join(BUILD,name)+'.v', join(GOLD,name)+'.v')
+    build = 'build/' + name
+    gold = 'gold/' + name
+
+    compile(build, main(*largs, **kwargs))
+    assert check_files_equal(callee_file, build+'.v', gold+'.v')
+
     if vendor in ['altera', 'xilinx', 'lattice']:
         if vendor == 'altera' or vendor == 'xilinx':
-            ucf = '.ucf'
+            constraints = '.ucf'
         if vendor == 'lattice': 
-            ucf = '.pcf'
-        assert check_files_equal(callee_file, join(BUILD,name)+ucf, join(GOLD,name)+ucf)
+            constraints = '.pcf'
+        assert check_files_equal(callee_file,
+            build+constraints, gold+constraints)
