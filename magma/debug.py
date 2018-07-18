adbc8000bcb53b4af9346bfef16cfdd03a350879
Lenny Truong <lenny@stanford.edu>
2018-07-18 09:43:02 -0700

    Optimize get_callee_frame_info to not retrieve the entire stack

--- magma/debug.py
+++ magma/debug.py
@@ -3,11 +3,8 @@ import sys
 
 
 def get_callee_frame_info():
-    callee_frame = inspect.stack()[2]
-    if sys.version_info < (3, 5):
-        debug_info = callee_frame[1], callee_frame[2]
-    else:
-        debug_info = callee_frame.filename, callee_frame.lineno
+    callee_frame = inspect.getframeinfo(inspect.currentframe().f_back.f_back)
+    debug_info = callee_frame.filename, callee_frame.lineno
     return debug_info
 
 
@@ -16,7 +13,7 @@ def debug_wire(fn):
     Automatically populates the `debug_info` argument for a wire call if it's
     not already passed as an argument
     """
-    # TODO: We could check that fn has the correct interface 
+    # TODO: We could check that fn has the correct interface
     #       wire(i, o, debug_info)
     def wire(i, o, debug_info=None):
         if debug_info is None:
