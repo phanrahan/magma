992f09a82493899c8e6959f53c89a91009e6b6ed
Lenny Truong <lenny@stanford.edu>
2018-07-20 12:44:10 -0700

    Better error message in warning

--- magma/circuit.py
+++ magma/circuit.py
@@ -244,7 +244,7 @@ class AnonymousCircuitType(object):
                 i = getattr(input, key)
                 wire( value, getattr(input, key), debug_info)
             else:
-                warning('Warning: circuit does not have {}'.format(key))
+                warning('Warning: circuit {} does not have {}'.format(str(self), key))
 
         o = input.interface.outputs()
         return o[0] if len(o) == 1 else tuple(o)
