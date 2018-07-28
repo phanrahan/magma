74e0411d08896e219d04a4e56a2477793b44649b
Lenny Truong <lenny@stanford.edu>
2018-07-28 14:34:19 -0700

    Add preliminary support for If and IfExp

--- magma/circuit_def.py
+++ magma/circuit_def.py
@@ -2,6 +2,14 @@ import functools
 import ast
 import inspect
 import textwrap
+from collections import OrderedDict
+from magma.logging import warning
+import astor
+import inspect
+
+
+class CircuitDefinitionSyntaxError(Exception):
+    pass
 
 
 def get_ast(obj):
@@ -10,14 +18,70 @@ def get_ast(obj):
     return ast.parse(program_txt)
 
 
+class IfTransformer(ast.NodeTransformer):
+    def visit_If(self, node):
+        seen = OrderedDict()
+        for stmt in node.body:
+            if not isinstance(stmt, ast.Assign):
+                # TODO: Print info from original source file/line
+                raise CircuitDefinitionSyntaxError(
+                    "Expected only assignment statements in if statement")
+            if len(stmt.targets) > 1:
+                raise NotImplementedError("Assigning more than one value")
+            key = ast.dump(stmt.targets[0])
+            if key in seen:
+                # TODO: Print the line number
+                warning("Assigning to value twice inside `if` block,"
+                        " taking the last value (first value is ignored)")
+            seen[key] = stmt
+        if not hasattr(node, "orelse"):
+            raise NotImplementedError("If without else")
+        orelse_seen = set()
+        for stmt in node.orelse:
+            key = ast.dump(stmt.targets[0])
+            if key in seen:
+                if key in orelse_seen:
+                    warning("Assigning to value twice inside `else` block,"
+                            " taking the last value (first value is ignored)")
+                orelse_seen.add(key)
+                seen[key].value = ast.Call(
+                    ast.Name("mux", ast.Load()),
+                    [ast.List([seen[key].value, stmt.value],
+                              ast.Load()), node.test],
+                    [])
+            else:
+                raise NotImplementedError("Assigning to a variable once in"
+                                          " `else` block (not in then block)")
+        return [node for node in seen.values()]
+
+    def visit_IfExp(self, node):
+        if not hasattr(node, "orelse"):
+            raise NotImplementedError("If without else")
+        return ast.Call(
+            ast.Name("mux", ast.Load()),
+            [ast.List([node.body, node.orelse],
+                      ast.Load()), node.test],
+            [])
+
+
 def circuit_def(fn):
+    stack = inspect.stack()
+    defn_locals = stack[1].frame.f_locals
+    defn_globals = stack[1].frame.f_globals
     tree = get_ast(fn)
-    print(ast.dump(tree))
+    tree = IfTransformer().visit(tree)
+    tree = ast.fix_missing_locations(tree)
+    # TODO: Only remove @m.circuit_def, there could be others
+    tree.body[0].decorator_list = []
+    # print(astor.to_source(tree))
+    exec(compile(tree, filename="<ast>", mode="exec"), defn_globals,
+         defn_locals)
+
+    fn = defn_locals[fn.__name__]
 
     @classmethod
     @functools.wraps(fn)
     def wrapped(io):
         return fn(io)
 
-    raise NotImplementedError()
     return wrapped
--- tests/test_circuit_def.py
+++ tests/test_circuit_def.py
@@ -2,6 +2,50 @@ import magma as m
 from magma.testing import check_files_equal
 
 
+@m.cache_definition
+def DefineMux(height=2, width=None):
+    if width is None:
+        T = m.Bit
+    else:
+        T = m.Bits(width)
+
+    io = []
+    for i in range(height):
+        io += ["I{}".format(i), m.In(T)]
+    if height == 2:
+        select_type = m.Bit
+    else:
+        select_type = m.Bits(m.bitutils.clog2(height))
+    io += ['S', m.In(select_type)]
+    io += ['O', m.Out(T)]
+
+    class _Mux(m.Circuit):
+        name = "Mux{}x{}".format(height, width)
+        IO = io
+    return _Mux
+
+
+def Mux(height=2, width=None, **kwargs):
+    return DefineMux(height, width)(**kwargs)
+
+
+def get_length(value):
+    if isinstance(value, m._BitType):
+        return None
+    elif isinstance(value, m.ArrayType):
+        return len(value)
+    else:
+        raise NotImplementedError(f"Cannot get_length of {type(value)}")
+
+
+def mux(I, S):
+    if isinstance(S, int):
+        return I[S]
+    elif S.const():
+        return I[m.bitutils.seq2int(S.bits())]
+    return Mux(len(I), get_length(I[0]))(*I, S)
+
+
 def test_if_statement_basic():
     class TestIfStatementBasic(m.Circuit):
         IO = ["I", m.In(m.Bits(2)), "S", m.In(m.Bit), "O", m.Out(m.Bit)]
@@ -9,14 +53,16 @@ def test_if_statement_basic():
         @m.circuit_def
         def definition(io):
             if io.S:
-                m.wire(io.O, io.I[0])
+                O = io.I[0]
+                # m.wire(io.O, io.I[0])
                 # io.O = io.I[0]
                 # TODO: Alternative syntax
                 # io.O <= io.I[0]
                 # TODO: Or we could use wire syntax
                 # wire(io.O, io.I[0])
             else:
-                io.O = io.I[1]
+                O = io.I[1]
+            m.wire(O, io.O)
     m.compile("build/test_if_statement_basic", TestIfStatementBasic)
     assert check_files_equal(__file__, f"build/test_if_statement_basic.v",
                              f"gold/test_if_statement_basic.v")
