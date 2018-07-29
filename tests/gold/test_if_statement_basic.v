18de54b2cdd1697a2b8f978a7d8b916c10b88863
Lenny Truong <lenny@stanford.edu>
2018-07-28 19:14:13 -0700

    Revert "Update names to better match circuit conventions"
    This reverts commit 52ec96057a98f2e7861b3cf27ea0e8b6ad03d526.

--- tests/gold/test_if_statement_basic.v
+++ tests/gold/test_if_statement_basic.v
@@ -1,4 +1,4 @@
-module TestIfStatementBasics (input [1:0] I, input  S, output  O);
+module test_if_statement_basic (input [1:0] I, input  S, output  O);
 wire  inst0_O;
 Mux2 inst0 (.I0(I[0]), .I1(I[1]), .S(S), .O(inst0_O));
 assign O = inst0_O;
