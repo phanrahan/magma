809c4a1cf729b4f662a85ac50f6b952621059852
Lenny Truong <lenny@stanford.edu>
2018-08-30 14:01:54 -0700

    Add anon bit test

--- tests/test_circuit/test_define.py
+++ tests/test_circuit/test_define.py
@@ -80,3 +80,27 @@ def test_2d_array_error(caplog):
         assert False, "Should raise exception"
     except Exception as e:
         assert str(e) == "Argument main.I of type Array(2,Array(3,Out(Bit))) is not supported, the verilog backend only supports simple 1-d array of bits of the form Array(N, Bit)"  # noqa
+
+
+@pytest.mark.parametrize("target,suffix",
+                         [("verilog", "v"), ("coreir", "json")])
+def test_anon_bit(target, suffix):
+    And2 = m.DeclareCircuit('And2', "I0", m.In(m.Bit), "I1", m.In(m.Bit),
+                            "O", m.Out(m.Bit))
+
+    main = m.DefineCircuit("main", "I", m.In(m.Bits(2)),
+                           "O", m.Out(m.Bit))
+
+    and2 = And2()
+
+    m.wire(main.I[0], and2.I0)
+    m.wire(main.I[1], and2.I1)
+    tmp = m.Bit()
+    m.wire(and2.O, tmp)
+    m.wire(tmp, main.O)
+
+    m.EndCircuit()
+
+    m.compile("build/test_anon_bit", main, target)
+    assert check_files_equal(__file__, f"build/test_anon_bit.{suffix}",
+                             f"gold/test_anon_bit.{suffix}")
