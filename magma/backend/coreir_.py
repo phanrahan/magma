90676d837bcf004b6a6cd4dd3b67444e1398ec12
rsetaluri <rsetaluri@users.noreply.github.com>
2018-11-02 14:55:20 -0700

    Check for verilogFile instead of verilog_source
    This change makes it such that only modules parsed with DefineFromVerilog (rather than DeclareFromVerilog) get emitted to CoreIR.

--- magma/backend/coreir_.py
+++ magma/backend/coreir_.py
@@ -302,8 +302,8 @@ class CoreIRBackend:
         # If this module was imported from verilog, do not go through the
         # general module construction flow. Instead just attach the verilog
         # source as metadata and return the module.
-        if hasattr(definition, "verilog_source"):
-            verilog_metadata = {"verilog_string": definition.verilog_source}
+        if hasattr(definition, "verilogFile"):
+            verilog_metadata = {"verilog_string": definition.verilogFile}
             coreir_module.add_metadata("verilog", json.dumps(verilog_metadata))
             return coreir_module
 
