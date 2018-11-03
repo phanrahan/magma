48e118231a2f14ac669572147d37e786b33950a5
Leonard Truong <leonardtruong@fb.com>
2018-11-03 08:33:31 -0700

    Add missing file

---
+++ magma/util.py
@@ -0,0 +1,9 @@
+import magma as m
+
+
+def BitOrBits(width):
+    if width is None:
+        return m.Bit
+    if not isinstance(width, int):
+        raise ValueError(f"Expected width to be None or int, got {width}")
+    return m.Bits(width)
