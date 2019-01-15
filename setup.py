40cf2e082c5f50f388317ab3d3fba2ba8a60ddd0
Lenny Truong <lenny@stanford.edu>
2019-01-14 16:03:32 -0800

    Bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='magma-lang',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
