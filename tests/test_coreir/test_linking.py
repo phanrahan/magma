805c6f3af4c36ef6db2f903bc4a31a746db0be51
Lenny Truong <lenny@stanford.edu>
2019-02-06 11:24:36 -0800

    Update linker test gold output

--- tests/test_coreir/test_linking.py
+++ tests/test_coreir/test_linking.py
@@ -1,6 +1,7 @@
 import os
 import magma as m
 from magma.frontend.coreir_ import coreir_typegen, DeclareCoreIRGenerator
+from magma.testing import check_files_equal
 
 # def smax_type_gen(context, values):
 #     width = values['width'].value
@@ -10,12 +11,13 @@ from magma.frontend.coreir_ import coreir_typegen, DeclareCoreIRGenerator
 #         "out": context.Array(width, context.Bit())
 #     })
 
+
 @coreir_typegen
-def smax_type_gen(width : int):
+def smax_type_gen(width: int):
     return Tuple(
-        in0 = m.Array(width, m.In(m.Bit)),
-        in1 = m.Array(width, m.In(m.BitIn)),
-        out = m.Array(width, m.In(m.Bit))
+        in0=m.Array(width, m.In(m.Bit)),
+        in1=m.Array(width, m.In(m.BitIn)),
+        out=m.Array(width, m.In(m.Bit))
     )
 
 
@@ -25,7 +27,9 @@ def test_declare_generator():
 
     class LinkerTest(m.Circuit):
         name = "LinkerTest0"
-        IO = ["I0", m.In(m.Bits(width)), "I1", m.In(m.Bits(width)), "O", m.Out(m.Bits(width))]
+        IO = ["I0", m.In(m.Bits(width)), "I1", m.In(m.Bits(width)),
+              "O", m.Out(m.Bits(width))]
+
         @classmethod
         def definition(self):
             Smax = DefineSmax(width=width)
@@ -35,7 +39,8 @@ def test_declare_generator():
             m.wire(self.O, smax.out)
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    m.compile(os.path.join(dir_path, "build/linker_test0"), LinkerTest, output="coreir")
-    with open(os.path.join(dir_path, "build/linker_test0.json"), "r") as actual:
-        with open(os.path.join(dir_path, "gold/linker_test0.json"), "r") as expected:
-            assert actual.read() == expected.read()
+    m.compile(os.path.join(dir_path, "build/linker_test0"), LinkerTest,
+              output="coreir")
+
+    assert check_files_equal(__file__, "build/linker_test0.json",
+                             "gold/linker_test0.json")
