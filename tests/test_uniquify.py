220d59af6032e1c8ae70987f300c1795be03eb0b
Raj Setaluri <raj.setaluri@gmail.com>
2019-02-13 11:15:38 -0800

    Add key error test

--- tests/test_uniquify.py
+++ tests/test_uniquify.py
@@ -63,3 +63,36 @@ def test_uniquify_unequal():
     assert check_files_equal(__file__,
                              "build/uniquify_unequal.json",
                              "gold/uniquify_unequal.json")
+
+
+def test_key_error():
+    import mantle
+    Mux2x6 = mantle.DefineMux(2, 6)
+
+    MuxWrapper_2_6 = m.DefineCircuit("MuxWrapper_2_6", "I", m.Array(2,m.In(m.Bits(6))), "S", m.In(m.Bits(1)), "O", m.Out(m.Bits(6)))
+    Mux2x6_inst0 = Mux2x6()
+    m.wire(MuxWrapper_2_6.I[0], Mux2x6_inst0.I0)
+    m.wire(MuxWrapper_2_6.I[1], Mux2x6_inst0.I1)
+    m.wire(MuxWrapper_2_6.S[0], Mux2x6_inst0.S)
+    m.wire(Mux2x6_inst0.O, MuxWrapper_2_6.O)
+    m.EndCircuit()
+
+    MuxWrapper_2_6_copy = m.DefineCircuit("MuxWrapper_2_6", "I", m.Array(2,m.In(m.Bits(6))), "S", m.In(m.Bits(1)), "O", m.Out(m.Bits(6)))
+    Mux2x6_inst0 = Mux2x6()
+    m.wire(MuxWrapper_2_6_copy.I[0], Mux2x6_inst0.I0)
+    m.wire(MuxWrapper_2_6_copy.I[1], Mux2x6_inst0.I1)
+    m.wire(MuxWrapper_2_6_copy.S[0], Mux2x6_inst0.S)
+    m.wire(Mux2x6_inst0.O, MuxWrapper_2_6_copy.O)
+    m.EndCircuit()
+
+    MuxWithDefaultWrapper_2_6_19_0 = m.DefineCircuit("MuxWithDefaultWrapper_2_6_19_0", "I", m.Array(2,m.In(m.Bits(6))), "S", m.In(m.Bits(19)), "O", m.Out(m.Bits(6)))
+    MuxWrapper_2_6_inst0 = MuxWrapper_2_6()
+    MuxWrapper_2_6_inst1 = MuxWrapper_2_6_copy()
+    m.wire(MuxWithDefaultWrapper_2_6_19_0.I, MuxWrapper_2_6_inst0.I)
+    m.wire(MuxWithDefaultWrapper_2_6_19_0.I, MuxWrapper_2_6_inst1.I)
+    m.wire(MuxWithDefaultWrapper_2_6_19_0.S[0], MuxWrapper_2_6_inst0.S[0])
+    m.wire(MuxWithDefaultWrapper_2_6_19_0.S[0], MuxWrapper_2_6_inst1.S[0])
+    m.wire(MuxWrapper_2_6_inst1.O, MuxWithDefaultWrapper_2_6_19_0.O)
+    m.EndCircuit()
+
+    m.compile(MuxWithDefaultWrapper_2_6_19_0.name, MuxWithDefaultWrapper_2_6_19_0, output="coreir")
