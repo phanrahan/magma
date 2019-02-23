287e4964250619d3beed0a3a675bc6f2a71f7231
Lenny Truong <lenny@stanford.edu>
2019-02-22 19:09:47 -0800

    Add preliminary support for hierarchy in sequential

--- tests/test_syntax/test_sequential.py
+++ tests/test_syntax/test_sequential.py
@@ -60,3 +60,41 @@ def test_seq_simple(target):
             return O
 
     compile_and_check("test_seq_simple", Basic, target)
+
+
+def test_seq_hierarchy(target):
+    def DefineCustomRegister(width, init=0):
+        @m.circuit.sequential
+        class Register:
+            def __init__(self):
+                self.value: m.Bits(width) = m.bits(init, width)
+
+            def __call__(self, I: m.Bits(width)) -> m.Bits(width):
+                O = self.value
+                self.value = I
+                return O
+
+        return Register
+
+    @m.circuit.sequential
+    class ShiftRegister:
+        def __init__(self):
+            # [<exp>] means execute the Python expression during the first
+            # stage of compilation
+
+            # The type of a hierarchal output must have the same signature for
+            # the input and output (can have a tuple for multiple inputs). This
+            # reflects the ability to read and write to the variable with the
+            # same meaning
+
+            # Only supported in __init__ body for now
+            self.x: m.Bits(2) = [DefineCustomRegister(2, init=0)()]
+            self.y: m.Bits(2) = [DefineCustomRegister(2, init=1)()]
+
+        def __call__(self, I: m.Bits(2)) -> m.Bits(2):
+            O = self.y
+            self.y = self.x
+            self.x = I
+            return O
+
+    compile_and_check("test_seq_hierarchy", ShiftRegister, target)
