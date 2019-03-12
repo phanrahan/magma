a7b3e87e0862822c12c393bd61cac977375efef2
Raj Setaluri <raj.setaluri@gmail.com>
2019-03-12 16:10:31 -0700

    Remove fault dependency

---
+++ tests/test_syntax/TestBasic_driver.cpp
@@ -0,0 +1,112 @@
+#include "VTestBasic.h"
+#include "verilated.h"
+#include <iostream>
+#include <verilated_vcd_c.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+// Based on https://www.veripool.org/projects/verilator/wiki/Manual-verilator#CONNECTING-TO-C
+vluint64_t main_time = 0;       // Current simulation time
+// This is a 64-bit integer to reduce wrap over issues and
+// allow modulus.  You can also use a double, if you wish.
+
+double sc_time_stamp () {       // Called by $time in Verilog
+    return main_time;           // converts to double, to match
+                                // what SystemC does
+}
+
+#if VM_TRACE
+VerilatedVcdC* tracer;
+#endif
+
+void my_assert(
+    unsigned int got,
+    unsigned int expected,
+    int i,
+    const char* port) {
+  if (got != expected) {
+    std::cerr << std::endl;  // end the current line
+    std::cerr << "Got      : 0x" << std::hex << got << std::endl;
+    std::cerr << "Expected : 0x" << std::hex << expected << std::endl;
+    std::cerr << "i        : " << std::dec << i << std::endl;
+    std::cerr << "Port     : " << port << std::endl;
+#if VM_TRACE
+    tracer->close();
+#endif
+    exit(1);
+  }
+}
+
+int main(int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  VTestBasic* top = new VTestBasic;
+
+#if VM_TRACE
+  Verilated::traceEverOn(true);
+  tracer = new VerilatedVcdC;
+  top->trace(tracer, 99);
+  mkdir("logs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  tracer->open("logs/TestBasic.vcd");
+#endif
+
+  top->I = 1;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  top->I = 2;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  my_assert(top->O, 1, 4, "TestBasic.O");
+  top->I = 3;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  my_assert(top->O, 2, 7, "TestBasic.O");
+  top->I = 0;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  top->eval();
+  main_time++;
+  #if VM_TRACE
+  tracer->dump(main_time);
+  #endif
+  top->CLK ^= 1;
+  my_assert(top->O, 3, 10, "TestBasic.O");
+
+
+#if VM_TRACE
+  tracer->close();
+#endif
+}
--- tests/test_syntax/test_sequential.py
+++ tests/test_syntax/test_sequential.py
@@ -1,7 +1,6 @@
 import magma as m
 from test_combinational import compile_and_check, phi
 from collections import Sequence
-import fault
 import coreir
 
 default_port_mapping = {
@@ -85,6 +84,20 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("target", ["coreir", "coreir-verilog"])
 
 
+def _run_verilator(circuit, directory):
+    import subprocess
+    def run_from_directory(cmd):
+        return subprocess.call(cmd, cwd=directory, shell=True)
+    top = circuit.name
+    assert not run_from_directory(
+        f"make -C obj_dir -j -f V{top}.mk V{top}")
+    assert not run_from_directory(
+        f"verilator -Wall -Wno-INCABSPATH -Wno-DECLFILENAME --cc {top}.v "
+        f"--exe {top}_driver.cpp --top-module {top}")
+    assert not run_from_directory(
+        f"./obj_dir/V{top}")
+
+
 def test_seq_simple(target):
     @m.circuit.sequential
     class TestBasic:
@@ -100,21 +113,7 @@ def test_seq_simple(target):
 
     compile_and_check("TestBasic", TestBasic, target)
     if target == "coreir-verilog":
-        tester = fault.Tester(TestBasic, clock=TestBasic.CLK)
-        tester.circuit.I = 1
-        tester.step(2)
-        tester.circuit.I = 2
-        tester.step(2)
-        tester.circuit.O.expect(1)
-        tester.circuit.I = 3
-        tester.step(2)
-        tester.circuit.O.expect(2)
-        tester.circuit.I = 0
-        tester.step(2)
-        tester.circuit.O.expect(3)
-        tester.compile_and_run(target="verilator",
-                               directory="tests/test_syntax/build",
-                               skip_compile=True)
+        _run_verilator(TestBasic, directory="tests/test_syntax/build")
 
 
 def test_seq_hierarchy(target):
@@ -155,18 +154,4 @@ def test_seq_hierarchy(target):
 
     compile_and_check("TestShiftRegister", TestShiftRegister, target)
     if target == "coreir-verilog":
-        tester = fault.Tester(TestShiftRegister, clock=TestShiftRegister.CLK)
-        tester.circuit.I = 1
-        tester.step(2)
-        tester.circuit.I = 2
-        tester.step(2)
-        tester.circuit.O.expect(1)
-        tester.circuit.I = 3
-        tester.step(2)
-        tester.circuit.O.expect(2)
-        tester.circuit.I = 0
-        tester.step(2)
-        tester.circuit.O.expect(3)
-        tester.compile_and_run(target="verilator",
-                               directory="tests/test_syntax/build",
-                               skip_compile=True)
+        _run_verilator(TestShiftRegister, directory="tests/test_syntax/build")
