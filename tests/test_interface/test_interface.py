c6225622688d523433c902dc4c230f3c6a71e0d8
Lenny Truong <lenny@stanford.edu>
2019-03-21 10:22:46 -0700

    Safer check, fixup tests

--- tests/test_interface/test_interface.py
+++ tests/test_interface/test_interface.py
@@ -6,10 +6,10 @@ Uses pytest API for reading stdout/stderr for testing
 
 def test_1():
     I0 = DeclareInterface("a", In(Bit), "b", Out(Bits[2]))
-    assert str(I0) == '"a", In(Bit), "b", Out(Bits(2))'
+    assert str(I0) == '"a", In(Bit), "b", Out(Bits[2])'
 
     i0 = I0()
     print(i0)
-    assert str(i0) == '"a", In(Bit), "b", Out(Bits(2))'
+    assert str(i0) == '"a", In(Bit), "b", Out(Bits[2])'
 
 
