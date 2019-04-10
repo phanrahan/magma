7a9d55505e4ae6d820c472dcb3620c3ca6879985
Lenny Truong <lenny@stanford.edu>
2019-04-10 16:19:00 -0700

    Add qualify tests

--- tests/test_type/test_fixed.py
+++ tests/test_type/test_fixed.py
@@ -5,3 +5,7 @@ def test_sfixed_simple():
     x = m.SFixed[25, 2]
     assert x.W == 25
     assert x.I == 2
+    xi = m.In(x)
+    xo = m.Out(x)
+    assert xi.T.isinput()
+    assert xo.T.isoutput()
