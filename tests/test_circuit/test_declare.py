5d59c48919a1e617e1fbd0a91abf177d6e1f653c
Lenny Truong <lenny@stanford.edu>
2019-05-09 15:47:59 -0700

    Add polarity test

--- tests/test_circuit/test_declare.py
+++ tests/test_circuit/test_declare.py
@@ -6,3 +6,13 @@ def test_declare_simple():
     And2 = m.DeclareCircuit("And2", "I0", m.In(m.Bit), "I1", m.In(m.Bit), "O",
                             m.Out(m.Bit))
     assert isinstance(And2.I0, m.BitType)
+
+
+def test_declare_interface_polarity():
+    And2Decl = m.DeclareCircuit("And2", "I0", m.In(m.Bit), "I1", m.In(m.Bit),
+                                "O", m.Out(m.Bit))
+    And2Defn = m.DefineCircuit("And2", "I0", m.In(m.Bit), "I1", m.In(m.Bit),
+                               "O", m.Out(m.Bit))
+
+    assert And2Decl.interface.ports["I0"].isinput() == \
+        And2Defn.interface.ports["I0"].isinput()
