5d618f895044ec2f3d40ef1bfe717774c9ce451e
Rajsekhar Setaluri <rajsekhar@fb.com>
2019-05-30 13:38:57 -0700

    Test CoreIR compilation after adding ports

--- tests/test_editing.py
+++ tests/test_editing.py
@@ -1,8 +1,10 @@
 import magma as m
+from magma.testing import check_files_equal
 
 
 def test_add_ports_basic():
     Foo = m.DefineCircuit("Foo", "I", m.In(m.Bit), "O", m.Out(m.Bit))
+    m.wire(Foo.I, Foo.O)
     m.EndCircuit()
 
     Foo.add_port("I2", m.In(m.Bit))
@@ -12,9 +14,16 @@ def test_add_ports_basic():
     foo = Foo()
     assert isinstance(Foo.I2, m._BitType)
 
+    # Check compilation to CoreIR.
+    filename = "test_add_ports"
+    m.compile(f"build/{filename}", Foo, output="coreir")
+    check_files_equal(__file__, f"build/{filename}.json",
+                      f"gold/{filename}.json")
+
 
 def test_add_ports_previous_instance():
     Foo = m.DefineCircuit("Foo", "I", m.In(m.Bit), "O", m.Out(m.Bit))
+    m.wire(Foo.I, Foo.O)
     m.EndCircuit()
 
     # Instacne Foo *before* adding a port.
@@ -25,3 +34,9 @@ def test_add_ports_previous_instance():
     assert isinstance(Foo.I2, m._BitType)
 
     assert isinstance(Foo.I2, m._BitType)
+
+    # Check compilation to CoreIR.
+    filename = "test_add_ports"
+    m.compile(f"build/{filename}", Foo, output="coreir")
+    check_files_equal(__file__, f"build/{filename}.json",
+                      f"gold/{filename}.json")
