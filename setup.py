49881f224d44ac6850b68f3a9bf89b9ba79c8e4b
Lenny Truong <leonardtruong@fb.com>
2019-07-25 12:57:59 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='1.0.14',
+    version='1.0.15',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
