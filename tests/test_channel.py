9e749285427bd71e238bf1b58ce524a938959ace
Lenny Truong <leonardtruong@fb.com>
2019-08-22 10:17:33 -0700

    WIP channel

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -35,18 +35,18 @@ class Downsample(Coroutine):
             for y in range(4):
                 for x in range(4):
                     if ((x % 2) == 0) & ((y % 2) == 0):
+                        self.data_in.ready = 1
                         while ~(self.data_in.valid & self.data_out.ready):
                             print("Waiting for data_in.valid and data_out.ready")
                             yield
-                        self.data_in.ready = 1
                         print("Passing data through")
                         self.data_out.data = self.data_in.data
                         self.data_out.valid = 1
                     else:
+                        self.data_in.ready = 1
                         while ~self.data_in.valid:
                             print("Waiting for data_in.valid")
                             yield
-                        self.data_in.ready = 1
                         print("Dropping data")
                     yield
                     self.data_out.valid = 0
@@ -173,7 +173,6 @@ def test_producer_consuemr():
     assert consumer_finished, (producer_finished, consumer_finished)
 
 
-"""
 def test_channel():
     @m.circuit.coroutine
     def downsample(data_in: m.In(m.Channel(m.Bits[16])),
@@ -182,7 +181,7 @@ def test_channel():
             for y in range(32):
                 for x in range(32):
                     if ((x % m.bits(2, 5)) == 0) & ((y % m.bits(2, 5)) == 0):
-                        while data_in.is_empty() | data_out.is_full():
+                        while ~data_in.valid | ~data_out.ready:
                             yield
                         data = data_in.pop(blocking=False)
                         data_out.push(data, blocking=False)
@@ -198,4 +197,72 @@ def test_channel():
         tester.eval()
         tester.print("data_out=%x data_out_valid=%d", downsample.O0, downsample.O1)
     tester.compile_and_run("verilator", directory="tests/build", skip_compile=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+@m.circuit.coroutine
+def downsample(data_in: m.In(m.Channel(m.Bits[16])),
+               data_out: m.Out(m.Channel(m.Bits[16]))):
+    while True:
+        for y in range(32):
+            for x in range(32):
+                self.data_out.data = self.data_in.data
+                self.data_out.valid = ((x % m.bits(2, 5)) == 0) & \
+                                      ((y % m.bits(2, 5)) == 0)
+                yield
+
 """
+1) only sending downstream valid (e.g. for downsample)
+2) sending downstream valid and upstream ready, handle upstream valid
+   (downstream ready stalls registers) (e.g. for upsample)
+"""
+
+
+@m.circuit.coroutine
+def downsample(data_in: m.In(m.Channel(m.Bits[16])),
+               data_out: m.Out(m.Channel(m.Bits[16]))):
+    while True:
+        for y in range(32):
+            for x in range(32):
+                data = data_in.pop()
+                if ((x % m.bits(2, 5)) == 0) & ((y % m.bits(2, 5)) == 0):
+                    data_out.push(data)
+                yield
+
+
+@m.circuit.coroutine
+def downsample(data_in: m.In(m.Channel(m.Bits[16])),
+               data_out: m.Out(m.Channel(m.Bits[16]))):
+    while True:
+        for y in range(32):
+            for x in range(32):
+                # pop
+                self.data_in.ready = 1
+                while ~self.data_in.valid:
+                    yield
+                    # implicitly set ready to 0 after every resume
+                    self.data_in.ready = 0
+                    self.data_in.ready = 1
+                data = self.data_in.data
+
+                if ((x % m.bits(2, 5)) == 0) & ((y % m.bits(2, 5)) == 0):
+                    # push
+                    self.data_out.valid = 1
+                    while ~self.data_out.ready:
+                        yield
+                        # implicitly set ready to 0 after every resume
+                        self.data_in.ready = 0
+                # dropping or data_out is ready
+                yield
+                # implicitly set ready to 0 after every resume
+                self.data_in.ready = 0
