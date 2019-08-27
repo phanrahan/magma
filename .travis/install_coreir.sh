6f066c600a97bca5e5e4504633d726d6f2248539
David Durst <davidbdurst@gmail.com>
2019-08-26 18:13:40 -0700

    also untarring coreir-linux

--- .travis/install_coreir.sh
+++ .travis/install_coreir.sh
@@ -24,7 +24,7 @@ else
     # based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
     curl -s -L https://github.com/rdaly525/coreir/releases/latest | grep "href.*coreir-linux.tar.gz" | cut -d \" -f 2 | xargs -I {} wget https://github.com"{}"
     mkdir coreir_release;
-    tar -xf coreir.tar.gz -C coreir_release --strip-components 1;
+    tar -xf coreir-linux.tar.gz -C coreir_release --strip-components 1;
     export PATH=$TRAVIS_BUILD_DIR/coreir_release/bin:$PATH;
     export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/coreir_release/lib:$LD_LIBRARY_PATH;
 fi
