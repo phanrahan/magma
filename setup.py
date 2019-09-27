73e9069a2659ebf165455086ec3349d07958ac2f
Lenny Truong <lenny@stanford.edu>
2019-09-27 14:44:29 -0700

    Switch dependencyg

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "coreir>=2.0.*",
         "hwtypes>=1.0.*",
         "ast_tools>=0.0.3",
-        "veriloggen"
+        "kratos"
     ],
     python_requires='>=3.6',
     long_description=long_description,
