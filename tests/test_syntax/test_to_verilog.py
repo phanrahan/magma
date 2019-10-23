3303afce30a182d406cc4340a868a8829d032b72
Lenny Truong <lenny@stanford.edu>
2019-10-23 14:57:44 -0700

    Add pass to build kratos debug info

--- tests/test_syntax/test_to_verilog.py
+++ tests/test_syntax/test_to_verilog.py
@@ -28,7 +28,42 @@ class SimpleALU(m.Circuit):
         io.c <= io.execute_alu(io.a, io.b, io.config_)
 
 
+def build_kratos_debug_info(circuit, top_name):
+    symbols = []
+    inst_to_defn_map = {}
+    for instance in circuit.instances:
+        instance_symbols, instance_inst_to_defn_map = \
+            build_kratos_debug_info(type(instance), top_name="")
+        for symbol in instance_symbols:
+            symbol = instance.name + "." + symbol
+            if top_name != "":
+                symbol = top_name + "." + symbol
+            symbols.append(symbol)
+        for k, v in instance_inst_to_defn_map.values():
+            key = instance.name + "." + k
+            if top_name != "":
+                key = top_name + "." + key
+            inst_to_defn_map[key] = v
+        if top_name != "":
+            inst_to_defn_map[top_name + "." + instance.name] = type(instance).name
+        else:
+            inst_to_defn_map[instance.name] = type(instance).name
+    for key in circuit.interface.ports.keys():
+        if top_name != "":
+            key = top_name + "." + key
+        symbols.append(key)
+    return symbols, inst_to_defn_map
+
+
 def test_simple_alu():
+    symbols, inst_to_defn_map = \
+        build_kratos_debug_info(SimpleALU, top_name=SimpleALU.name)
+    assert symbols == ['SimpleALU.execute_alu_inst0.a',
+                       'SimpleALU.execute_alu_inst0.b',
+                       'SimpleALU.execute_alu_inst0.config_',
+                       'SimpleALU.execute_alu_inst0.O', 'SimpleALU.a',
+                       'SimpleALU.b', 'SimpleALU.c', 'SimpleALU.config_']
+    assert inst_to_defn_map == {'SimpleALU.execute_alu_inst0': 'execute_alu'}
     m.compile('build/SimpleALU', SimpleALU, output="coreir-verilog")
     assert check_files_equal(__file__, f"build/SimpleALU.v",
                              f"gold/SimpleALU.v")
