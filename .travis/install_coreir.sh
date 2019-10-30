b548bcaa510362d700933f8d9c058081f806ab6e
Ross <rdaly525@stanford.edu>
2019-10-30 16:54:50 -0700

    changed travis script to use new coreir install script

--- .travis/install_coreir.sh
+++ .travis/install_coreir.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-set -e
-
-if [ "$TRAVIS_BRANCH" == "coreir-dev" ]; then
-    mkdir deps;
-    mkdir deps/bin;
-    mkdir deps/lib;
-    mkdir deps/include;
-    cd deps;
-    git clone -b dev https://github.com/rdaly525/coreir.git;
-    cd coreir;
-    export CXX="g++-4.9";
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/deps ..
-    make install;
-    cd ..;  # build
-    cd ..;  # coreir
-    cd ..;  # deps
-    export PATH=$TRAVIS_BUILD_DIR/deps/bin:$PATH;
-    export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/deps/lib:$LD_LIBRARY_PATH;
-    pip install --upgrade git+git://github.com/leonardt/pycoreir.git@dev;
+mkdir deps
+cd deps;
+if [[ "$TRAVIS_BRANCH" == "coreir-dev" ]]; then
+  git clone --single-branch --branch dev https://github.com/rdaly525/coreir.git
+  cd coreir/build
+  cmake -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/deps ..
+  make install;
+  cd ..;  # build
+  cd ..;  # coreir
 else
-    # based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
-    curl -s -L https://github.com/rdaly525/coreir/releases/latest | grep "href.*coreir-linux.tar.gz" | cut -d \" -f 2 | xargs -I {} wget https://github.com"{}"
-    mkdir coreir_release;
-    tar -xf coreir-linux.tar.gz -C coreir_release --strip-components 1;
-    export PATH=$TRAVIS_BUILD_DIR/coreir_release/bin:$PATH;
-    export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/coreir_release/lib:$LD_LIBRARY_PATH;
+  # based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
+  curl -s -L https://github.com/rdaly525/coreir/releases/latest | grep "href.*coreir-${TRAVIS_OS_NAME}.tar.gz" | cut -d \" -f 2 | xargs -I {} wget https://github.com"{}"
+  mkdir coreir_release;
+  tar -xf coreir-${TRAVIS_OS_NAME}.tar.gz -C coreir_release --strip-components 1;
+  mkdir bin;
+  mkdir lib;
+  mkdir include;
+  cd coreir_release;
+
+  make install prefix=$TRAVIS_BUILD_DIR/deps;
+  cd ..; #coreir_release
 fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  # avoid strange libjpeg error (see https://github.com/sgrif/pq-sys/issues/1
+  # for some more info)
+  export DYLD_LIBRARY_PATH=/System/Library/Frameworks/ImageIO.framework/Versions/A/Resources/:$DYLD_LIBRARY_PATH
+  export DYLD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/deps/lib:$DYLD_LIBRARY_PATH
+else
+  export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/deps/lib:$LD_LIBRARY_PATH
+fi
+
+export PATH=$TRAVIS_BUILD_DIR/deps/bin:$PATH
+cd ..;  # deps
