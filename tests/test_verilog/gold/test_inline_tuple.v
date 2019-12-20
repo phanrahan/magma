125ed01c73b44705090417cc71ad3b4afbe3ccc0
Lenny Truong <leonardtruong@fb.com>
2019-12-20 07:59:47 -0800

    Add nested arrays

--- tests/test_verilog/gold/test_inline_tuple.v
+++ tests/test_verilog/gold/test_inline_tuple.v
@@ -1,26 +1,38 @@
 // Module `InnerDelayUnit` defined externally
-module DelayUnit (input CLK, input [4:0] INPUT_data, output INPUT_ready, input INPUT_valid, output [4:0] OUTPUT_data, input OUTPUT_ready, output OUTPUT_valid);
-wire inner_delay_INPUT_ready;
-wire [4:0] inner_delay_OUTPUT_data;
-wire inner_delay_OUTPUT_valid;
-InnerDelayUnit inner_delay(.INPUT_data(INPUT_data), .INPUT_ready(inner_delay_INPUT_ready), .INPUT_valid(INPUT_valid), .OUTPUT_data(inner_delay_OUTPUT_data), .OUTPUT_ready(OUTPUT_ready), .OUTPUT_valid(inner_delay_OUTPUT_valid));
-assign INPUT_ready = inner_delay_INPUT_ready;
-assign OUTPUT_data = inner_delay_OUTPUT_data;
-assign OUTPUT_valid = inner_delay_OUTPUT_valid;
+module DelayUnit (input CLK, input [4:0] INPUT_0_data, output INPUT_0_ready, input INPUT_0_valid, input [4:0] INPUT_1_data, output INPUT_1_ready, input INPUT_1_valid, output [4:0] OUTPUT_0_data, input OUTPUT_0_ready, output OUTPUT_0_valid, output [4:0] OUTPUT_1_data, input OUTPUT_1_ready, output OUTPUT_1_valid);
+wire inner_delay_INPUT_0_ready;
+wire inner_delay_INPUT_1_ready;
+wire [4:0] inner_delay_OUTPUT_0_data;
+wire inner_delay_OUTPUT_0_valid;
+wire [4:0] inner_delay_OUTPUT_1_data;
+wire inner_delay_OUTPUT_1_valid;
+InnerDelayUnit inner_delay(.INPUT_0_data(INPUT_1_data), .INPUT_0_ready(inner_delay_INPUT_0_ready), .INPUT_0_valid(INPUT_1_valid), .INPUT_1_data(INPUT_0_data), .INPUT_1_ready(inner_delay_INPUT_1_ready), .INPUT_1_valid(INPUT_0_valid), .OUTPUT_0_data(inner_delay_OUTPUT_0_data), .OUTPUT_0_ready(OUTPUT_1_ready), .OUTPUT_0_valid(inner_delay_OUTPUT_0_valid), .OUTPUT_1_data(inner_delay_OUTPUT_1_data), .OUTPUT_1_ready(OUTPUT_0_ready), .OUTPUT_1_valid(inner_delay_OUTPUT_1_valid));
+assign INPUT_0_ready = inner_delay_INPUT_1_ready;
+assign INPUT_1_ready = inner_delay_INPUT_0_ready;
+assign OUTPUT_0_data = inner_delay_OUTPUT_1_data;
+assign OUTPUT_0_valid = inner_delay_OUTPUT_1_valid;
+assign OUTPUT_1_data = inner_delay_OUTPUT_0_data;
+assign OUTPUT_1_valid = inner_delay_OUTPUT_0_valid;
 endmodule
 
-module Main (input CLK, input [4:0] I_data, output I_ready, input I_valid, output [4:0] O_data, input O_ready, output O_valid);
-wire DelayUnit_inst0_INPUT_ready;
-wire [4:0] DelayUnit_inst0_OUTPUT_data;
-wire DelayUnit_inst0_OUTPUT_valid;
-DelayUnit DelayUnit_inst0(.CLK(CLK), .INPUT_data(I_data), .INPUT_ready(DelayUnit_inst0_INPUT_ready), .INPUT_valid(I_valid), .OUTPUT_data(DelayUnit_inst0_OUTPUT_data), .OUTPUT_ready(O_ready), .OUTPUT_valid(DelayUnit_inst0_OUTPUT_valid));
-assign I_ready = DelayUnit_inst0_INPUT_ready;
-assign O_data = DelayUnit_inst0_OUTPUT_data;
-assign O_valid = DelayUnit_inst0_OUTPUT_valid;
-assert property { @(posedge CLK) I_valid |-> ##3 O_ready };
+module Main (input CLK, input [4:0] I_0_data, output I_0_ready, input I_0_valid, input [4:0] I_1_data, output I_1_ready, input I_1_valid, output [4:0] O_0_data, input O_0_ready, output O_0_valid, output [4:0] O_1_data, input O_1_ready, output O_1_valid);
+wire DelayUnit_inst0_INPUT_0_ready;
+wire DelayUnit_inst0_INPUT_1_ready;
+wire [4:0] DelayUnit_inst0_OUTPUT_0_data;
+wire DelayUnit_inst0_OUTPUT_0_valid;
+wire [4:0] DelayUnit_inst0_OUTPUT_1_data;
+wire DelayUnit_inst0_OUTPUT_1_valid;
+DelayUnit DelayUnit_inst0(.CLK(CLK), .INPUT_0_data(I_1_data), .INPUT_0_ready(DelayUnit_inst0_INPUT_0_ready), .INPUT_0_valid(I_1_valid), .INPUT_1_data(I_0_data), .INPUT_1_ready(DelayUnit_inst0_INPUT_1_ready), .INPUT_1_valid(I_0_valid), .OUTPUT_0_data(DelayUnit_inst0_OUTPUT_0_data), .OUTPUT_0_ready(O_1_ready), .OUTPUT_0_valid(DelayUnit_inst0_OUTPUT_0_valid), .OUTPUT_1_data(DelayUnit_inst0_OUTPUT_1_data), .OUTPUT_1_ready(O_0_ready), .OUTPUT_1_valid(DelayUnit_inst0_OUTPUT_1_valid));
+assign I_0_ready = DelayUnit_inst0_INPUT_1_ready;
+assign I_1_ready = DelayUnit_inst0_INPUT_0_ready;
+assign O_0_data = DelayUnit_inst0_OUTPUT_1_data;
+assign O_0_valid = DelayUnit_inst0_OUTPUT_1_valid;
+assign O_1_data = DelayUnit_inst0_OUTPUT_0_data;
+assign O_1_valid = DelayUnit_inst0_OUTPUT_0_valid;
+assert property { @(posedge CLK) I_0_valid |-> ##3 O_1_ready };
 
-assert property { @(posedge CLK) DelayUnit_inst0.INPUT_valid |-> ##3 DelayUnit_inst0.OUTPUT_ready };
+assert property { @(posedge CLK) DelayUnit_inst0.INPUT_1_valid |-> ##3 DelayUnit_inst0.OUTPUT_0_ready };
 
-assert property { @(posedge CLK) DelayUnit_inst0inner_delay.inner_delay.INPUT.inner_delay.INPUT_valid |-> ##3 DelayUnit_inst0inner_delay.inner_delay.OUTPUT.inner_delay.OUTPUT_ready };
+assert property { @(posedge CLK) DelayUnit_inst0.inner_delay.INPUT.INPUT_0_valid |-> ##3 DelayUnit_inst0.inner_delay.OUTPUT.OUTPUT_1_ready };
 endmodule
 
