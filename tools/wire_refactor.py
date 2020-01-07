6d1ec364caa6dbf56bffaefb4327f320ba486db4
Caleb Donovick <donovick@cs.stanford.edu>
2020-01-07 14:15:24 -0800

    Add documentation

--- tools/wire_refactor.py
+++ tools/wire_refactor.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+f'''
+This script will refactor old magma wire
+statement (<=) to use the new syntax (@=).
+
+Usage:
+    ./{__file__} <src_file> <dst_file>
+
+This will break code with expression statements
+that "look" like a wire statement.
+For example:
+    ```
+    def foo():
+        0 <= 1
+    ```
+    ->
+    ```
+    def foo()
+        0 @= 1 #Error!
+    ```
+
+Will attempt to fix likely bugs caused by operator precedence:
+    `x <= y < z` becomes `x @= y < z`
+This techincally changes semantics of the code, however, in almost
+all instances this is the desired transformation.
+'''
 import ast
 import re
 import warnings
@@ -6,7 +31,8 @@ import warnings
 import astor
 
 class WireCollector(ast.NodeTransformer):
-    def __init__(self):
+    def __init__(self, lines):
+        self.lines = lines
         self.wire_stmts = []
     def visit_Expr(self, expr_stmt: ast.Expr):
         expr = expr_stmt.value
@@ -24,9 +50,12 @@ class WireCollector(ast.NodeTransformer):
                 # is the char of interest
                 if len(expr.comparators) != 1:
                     warnings.warn(
-                        f'`{astor.to_source(expr_stmt)[:-1]}` is a compound comparison.  '
-                        f'Will assume that {astor.to_source(expr.left)[:-1]} is the intended wire target.  '
-                         'This was almost certainly a bug, but if you knew what you were doing this just broke your code.'
+                        f'`{self.lines[expr_stmt.lineno - 1]}` contains a '
+                        f'compound comparison.  Will assume that '
+                        f'{astor.to_source(expr.left)[:-1]} is the intended '
+                        f'wire target.  This was almost certainly a bug, but '
+                        f'if you knew what you were doing this just broke your '
+                        f'code.'
                     )
                 self.wire_stmts.append((
                     # -1 because for some reason this is 1 indexed. smh.
@@ -38,7 +67,7 @@ wire_re = re.compile(r'.+(\<\=)\s*')
 new_wire_re = re.compile(r'.+(\@\=)\s*')
 def rewrite_wires(src_txt):
     src_ast = ast.parse(src_txt)
-    wire_collector = WireCollector()
+    wire_collector = WireCollector(src_txt.splitlines())
     wire_collector.visit(src_ast)
     src_lines = src_txt.split('\n')
     for lineno, expr_offset, end_offset in wire_collector.wire_stmts:
@@ -72,23 +101,59 @@ def rewrite_wires(src_txt):
 
 if __name__ == '__main__':
     import sys
+    import difflib
     if len(sys.argv) != 3:
-        raise RuntimeError(f'Usage: {sys.argv[0]} <src_file> <dst_file>')
+        print(f'Usage: {sys.argv[0]} <src_file> <dst_file>', file=sys.stderr)
+        sys.exit(1)
+
+    if sys.argv[1] == sys.argv[2]:
+        print('Refactoring in place is not recommended, '
+              'this script has known limitations and may break your code.')
+        response = input('continue? y[es] or n[o]: ')
+        while response not in ('y', 'n', 'yes', 'no'):
+            response = input('please select y[es] or n[o]: ')
+
+        if response[0] == 'n':
+            sys.exit(0)
+
+        assert response[0] == 'y'
+
+
     with open(sys.argv[1], 'r') as f:
         src_txt = f.read()
 
     try:
         ast.parse(src_txt)
     except SyntaxError:
-        raise RuntimeError(f'{sys.argv[1]} is not a valid python file') from None
+        print(f'{sys.argv[1]} is not a valid python file.', file=sys.stderr)
+        sys.exit(1)
 
-    new_src_txt= rewrite_wires(src_txt)
+    new_src_txt = rewrite_wires(src_txt)
 
     # Make sure we still have have valid python
     try:
         ast.parse(new_src_txt)
     except SyntaxError:
         raise RuntimeError(f'Rewrite resulted in invalid python') from None
+
+    diff = difflib.unified_diff(
+        src_txt.splitlines(),
+        new_src_txt.splitlines(),
+        fromfile=sys.argv[0],
+        tofile=sys.argv[1] + ' (pending)',
+        lineterm=''
+    )
+
+    print('\n'.join(diff))
+
+    response = input('write changes? y[es] or n[o]: ')
+    while response not in ('y', 'n', 'yes', 'no'):
+        response = input('please select y[es] or n[o]: ')
+
+    if response[0] == 'n':
+        sys.exit(0)
+
+    assert response[0] == 'y'
 
     with open(sys.argv[2], 'w') as f:
         f.write(new_src_txt)
