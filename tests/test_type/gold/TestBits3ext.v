0fa290092cecd4d0fa15423cfbb2c7e4b91f82c3
Lenny Truong <lenny@stanford.edu>
2020-01-10 17:14:48 -0800

    Update golds

--- tests/test_type/gold/TestBits3ext.v
+++ tests/test_type/gold/TestBits3ext.v
@@ -4,7 +4,7 @@ endmodule
 
 module TestExt (input [2:0] I, output [5:0] O);
 wire bit_const_0_None_out;
-corebit_const #(.value(0)) bit_const_0_None(.out(bit_const_0_None_out));
+corebit_const #(.value(1'b0)) bit_const_0_None(.out(bit_const_0_None_out));
 assign O = {bit_const_0_None_out,bit_const_0_None_out,bit_const_0_None_out,I[2],I[1],I[0]};
 endmodule
 
