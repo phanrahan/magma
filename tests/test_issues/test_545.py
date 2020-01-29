eba95a64794e149465fa855056e1d058df718f2e
Caleb Donovick <donovick@cs.stanford.edu>
2020-01-29 09:54:40 -0800

    import stubs

--- tests/test_issues/test_545.py
+++ tests/test_issues/test_545.py
@@ -1,6 +1,18 @@
+from magma.circuit import sequential
+from magma import Bit
+
+import sys
+import os
+
+TEST_SYNTAX_PATH = os.path.join(os.path.dirname(__file__), '../test_syntax')
+
+sys.path.append(TEST_SYNTAX_PATH)
+
+from test_sequential import DefineRegister, phi
+
 def test_545():
-    import magma
-    @magma.circuit.sequential
+    @sequential
     class PE:
-        def __call__(self, in0 : magma.Bit, in1: magma.Bit) -> magma.Bit:
+        def __call__(self, in0: Bit, in1: Bit) -> Bit:
             return in0 & in1
+
