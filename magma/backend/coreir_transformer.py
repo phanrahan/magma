848d78803cc3f394bf5f4792b22850e4fb93b90f
Leonard (Lenny) Truong <lenny@stanford.edu>
2020-02-03 14:19:49 -0800

    Update coreir_transformer.py

--- magma/backend/coreir_transformer.py
+++ magma/backend/coreir_transformer.py
@@ -17,7 +17,6 @@ from ..interface import InterfaceKind
 from ..is_definition import isdefinition
 from ..logging import root_logger
 from ..passes import InstanceGraphPass
-from ..t import MagmaProtocol
 from ..tuple import Tuple
 from .util import get_codegen_debug_info
 from .verilog import VerilogCompiler
