f273508d3e2b413e6df551a3592914dbcbf5670b
Lenny Truong <lenny@stanford.edu>
2020-02-18 11:38:27 -0800

    Add tuple test

---
+++ tests/test_simulator/test_mux_tuple.py
@@ -0,0 +1,45 @@
+import magma as m
+from magma.circuit import DeclareCircuit
+from magma.bitutils import clog2
+from hwtypes import BitVector
+from magma.simulator import PythonSimulator
+
+
+def _declare_muxn(height, width):
+    def simulate(self, value_store, state_store):
+        sel = BitVector[clog2(height)](value_store.get_value(self.I.sel))
+        out = BitVector[width](value_store.get_value(self.I.data[int(sel)]))
+        value_store.set_value(self.O, out)
+
+    I_fields = dict(data=m.Array[height, m.Bits[width]],
+                    sel=m.Bits[clog2(height)])
+
+    return DeclareCircuit(f"mux{height}x{width}",
+                          "I", m.In(m.Product.from_fields("anon",
+                                                          I_fields)),
+                          "O", m.Out(m.Bits[width]),
+                          simulate=simulate)
+
+
+def test_muxn():
+    class Main(m.Circuit):
+        IO = ["I0", m.In(m.Bits[5]),
+              "I1", m.In(m.Bits[5]),
+              "S", m.In(m.Bits[1]),
+              "O", m.Out(m.Bits[5])]
+
+        @classmethod
+        def definition(io):
+            in_ = m.namedtuple(data=m.array([io.I0, io.I1]), sel=io.S)
+            io.O @= _declare_muxn(2, 5)()(in_)
+
+    sim = PythonSimulator(Main)
+    for i in range(5):
+        I0 = BitVector.random(5)
+        I1 = BitVector.random(5)
+        S = BitVector.random(1)
+        sim.set_value(Main.I0, I0)
+        sim.set_value(Main.I1, I1)
+        sim.set_value(Main.S, S)
+        sim.evaluate()
+        assert sim.get_value(Main.O) == (I1 if S else I0)
