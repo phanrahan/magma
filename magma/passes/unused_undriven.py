f9cd53bccc0498308ec011feee68ae5f30fec7f6
Lenny Truong <leonardtruong@fb.com>
2020-02-26 14:20:01 -0800

    Fix decl issue

--- magma/passes/unused_undriven.py
+++ magma/passes/unused_undriven.py
@@ -9,20 +9,24 @@ class UndrivenUnusedPass(CircuitPass):
 
     def __call__(self, circuit):
         with circuit.open():
-            self.process(circuit.interface.ports.values())
+            self.process(circuit, circuit.interface.ports.values())
             for instance in circuit.instances:
-                self.process(instance.interface.ports.values())
+                self.process(circuit, instance.interface.ports.values())
 
 
 class DriveUndriven(UndrivenUnusedPass):
-    def process(self, port_values):
+    def process(self, circuit, port_values):
         for value in port_values:
             if value.is_input() and value.trace() is None:
+                # Turn decls into definition
+                circuit._is_definition = True
                 value.undriven()
 
 
 class TerminateUnused(UndrivenUnusedPass):
-    def process(self, port_values):
+    def process(self, circuit, port_values):
         for value in port_values:
             if value.is_output() and value.wired() is False:
+                # Turn decls into definition
+                circuit._is_definition = True
                 value.unused()
