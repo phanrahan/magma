c28c8e55d42db46e53874f53af59dc2d4392fca1
Lenny Truong <leonardtruong@fb.com>
2020-02-26 14:52:30 -0800

    Update golds

--- tests/test_syntax/gold/RdPtr.v
+++ tests/test_syntax/gold/RdPtr.v
@@ -1,3 +1,4 @@
+// Module `Mux2xOutUInt10` defined externally
 module coreir_reg_arst #(
     parameter width = 1,
     parameter arst_posedge = 1,
@@ -21,17 +22,6 @@ module coreir_reg_arst #(
   assign out = outReg;
 endmodule
 
-module coreir_mux #(
-    parameter width = 1
-) (
-    input [width-1:0] in0,
-    input [width-1:0] in1,
-    input sel,
-    output [width-1:0] out
-);
-  assign out = sel ? in1 : in0;
-endmodule
-
 module coreir_const #(
     parameter width = 1,
     parameter value = 1
@@ -49,40 +39,6 @@ module coreir_add #(
     output [width-1:0] out
 );
   assign out = in0 + in1;
-endmodule
-
-module commonlib_muxn__N2__width10 (
-    input [9:0] in_data_0,
-    input [9:0] in_data_1,
-    input [0:0] in_sel,
-    output [9:0] out
-);
-wire [9:0] _join_out;
-coreir_mux #(
-    .width(10)
-) _join (
-    .in0(in_data_0),
-    .in1(in_data_1),
-    .sel(in_sel[0]),
-    .out(_join_out)
-);
-assign out = _join_out;
-endmodule
-
-module Mux2xOutUInt10 (
-    input [9:0] I0,
-    input [9:0] I1,
-    input S,
-    output [9:0] O
-);
-wire [9:0] coreir_commonlib_mux2x10_inst0_out;
-commonlib_muxn__N2__width10 coreir_commonlib_mux2x10_inst0 (
-    .in_data_0(I0),
-    .in_data_1(I1),
-    .in_sel(S),
-    .out(coreir_commonlib_mux2x10_inst0_out)
-);
-assign O = coreir_commonlib_mux2x10_inst0_out;
 endmodule
 
 module RdPtr_comb (
--- tests/test_syntax/gold/RegisterMode.v
+++ tests/test_syntax/gold/RegisterMode.v
@@ -1,3 +1,5 @@
+// Module `Mux2xOutBits4` defined externally
+// Module `Mux2xOutBit` defined externally
 module coreir_reg #(
     parameter width = 1,
     parameter clk_posedge = 1,
@@ -14,17 +16,6 @@ module coreir_reg #(
     outReg <= in;
   end
   assign out = outReg;
-endmodule
-
-module coreir_mux #(
-    parameter width = 1
-) (
-    input [width-1:0] in0,
-    input [width-1:0] in1,
-    input sel,
-    output [width-1:0] out
-);
-  assign out = sel ? in1 : in0;
 endmodule
 
 module coreir_eq #(
@@ -67,58 +58,6 @@ module corebit_const #(
     output out
 );
   assign out = value;
-endmodule
-
-module commonlib_muxn__N2__width4 (
-    input [3:0] in_data_0,
-    input [3:0] in_data_1,
-    input [0:0] in_sel,
-    output [3:0] out
-);
-wire [3:0] _join_out;
-coreir_mux #(
-    .width(4)
-) _join (
-    .in0(in_data_0),
-    .in1(in_data_1),
-    .sel(in_sel[0]),
-    .out(_join_out)
-);
-assign out = _join_out;
-endmodule
-
-module commonlib_muxn__N2__width1 (
-    input [0:0] in_data_0,
-    input [0:0] in_data_1,
-    input [0:0] in_sel,
-    output [0:0] out
-);
-wire [0:0] _join_out;
-coreir_mux #(
-    .width(1)
-) _join (
-    .in0(in_data_0),
-    .in1(in_data_1),
-    .sel(in_sel[0]),
-    .out(_join_out)
-);
-assign out = _join_out;
-endmodule
-
-module Mux2xOutBits4 (
-    input [3:0] I0,
-    input [3:0] I1,
-    input S,
-    output [3:0] O
-);
-wire [3:0] coreir_commonlib_mux2x4_inst0_out;
-commonlib_muxn__N2__width4 coreir_commonlib_mux2x4_inst0 (
-    .in_data_0(I0),
-    .in_data_1(I1),
-    .in_sel(S),
-    .out(coreir_commonlib_mux2x4_inst0_out)
-);
-assign O = coreir_commonlib_mux2x4_inst0_out;
 endmodule
 
 module Register_comb (
@@ -165,22 +104,6 @@ coreir_reg #(
     .out(reg_P_inst0_out)
 );
 assign O = Register_comb_inst0_O1;
-endmodule
-
-module Mux2xOutBit (
-    input I0,
-    input I1,
-    input S,
-    output O
-);
-wire [0:0] coreir_commonlib_mux2x1_inst0_out;
-commonlib_muxn__N2__width1 coreir_commonlib_mux2x1_inst0 (
-    .in_data_0(I0),
-    .in_data_1(I1),
-    .in_sel(S),
-    .out(coreir_commonlib_mux2x1_inst0_out)
-);
-assign O = coreir_commonlib_mux2x1_inst0_out[0];
 endmodule
 
 module RegisterMode_comb (
