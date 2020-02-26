daf00581baf2f5fd556f362b5628a6183623add2
Lenny Truong <leonardtruong@fb.com>
2020-02-26 14:17:19 -0800

    Update gold

--- tests/test_syntax/gold/TestReplaceArray.v
+++ tests/test_syntax/gold/TestReplaceArray.v
@@ -1,16 +1,33 @@
-module coreir_const #(parameter width = 1, parameter value = 1) (output [width-1:0] out);
+module coreir_const #(
+    parameter width = 1,
+    parameter value = 1
+) (
+    output [width-1:0] out
+);
   assign out = value;
 endmodule
 
-module TestReplaceArray_comb (output [1:0] O);
+module TestReplaceArray_comb (
+    output [1:0] O
+);
 wire [1:0] const_3_2_out;
-coreir_const #(.value(2'h3), .width(2)) const_3_2(.out(const_3_2_out));
+coreir_const #(
+    .value(2'h3),
+    .width(2)
+) const_3_2 (
+    .out(const_3_2_out)
+);
 assign O = const_3_2_out;
 endmodule
 
-module TestReplaceArray (input CLK, output [1:0] O);
+module TestReplaceArray (
+    input CLK,
+    output [1:0] O
+);
 wire [1:0] TestReplaceArray_comb_inst0_O;
-TestReplaceArray_comb TestReplaceArray_comb_inst0(.O(TestReplaceArray_comb_inst0_O));
+TestReplaceArray_comb TestReplaceArray_comb_inst0 (
+    .O(TestReplaceArray_comb_inst0_O)
+);
 assign O = TestReplaceArray_comb_inst0_O;
 endmodule
 
