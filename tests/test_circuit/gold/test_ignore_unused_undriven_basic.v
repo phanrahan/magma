5869c9e55a0e98b85e199fa7c4183678ec6ab12b
Lenny Truong <leonardtruong@fb.com>
2020-02-26 13:10:37 -0800

    Update golds

--- tests/test_circuit/gold/test_ignore_unused_undriven_basic.v
+++ tests/test_circuit/gold/test_ignore_unused_undriven_basic.v
@@ -1,13 +1,15 @@
-module corebit_undriven (output out);
+module corebit_undriven (
+    output out
+);
 
 endmodule
 
-module corebit_term (input in);
-
-endmodule
-
-module Main (input I, output O);
-corebit_term corebit_term_inst0(.in(I));
-corebit_undriven corebit_undriven_inst0(.out(O));
+module Main (
+    input I,
+    output O
+);
+corebit_undriven corebit_undriven_inst0 (
+    .out(O)
+);
 endmodule
 
--- tests/test_circuit/gold/test_ignore_unused_undriven_hierarchy.v
+++ tests/test_circuit/gold/test_ignore_unused_undriven_hierarchy.v
@@ -1,24 +1,40 @@
-module corebit_undriven (output out);
+module corebit_undriven (
+    output out
+);
 
 endmodule
 
-module corebit_term (input in);
-
-endmodule
-
-module Foo (input I0, input I1, output O0, output O1);
-corebit_term corebit_term_inst0(.in(I1));
-corebit_undriven corebit_undriven_inst0(.out(O0));
+module Foo (
+    input I0,
+    input I1,
+    output O0,
+    output O1
+);
+corebit_undriven corebit_undriven_inst0 (
+    .out(O0)
+);
 assign O1 = I0;
 endmodule
 
-module Main (input I0, input I1, output O0, output O1);
+module Main (
+    input I0,
+    input I1,
+    output O0,
+    output O1
+);
 wire Foo_inst0_O1;
 wire corebit_undriven_inst1_out;
-Foo Foo_inst0(.I0(I0), .I1(corebit_undriven_inst1_out), .O0(O0), .O1(Foo_inst0_O1));
-corebit_term corebit_term_inst0(.in(I1));
-corebit_term corebit_term_inst1(.in(Foo_inst0_O1));
-corebit_undriven corebit_undriven_inst0(.out(O1));
-corebit_undriven corebit_undriven_inst1(.out(corebit_undriven_inst1_out));
+Foo Foo_inst0 (
+    .I0(I0),
+    .I1(corebit_undriven_inst1_out),
+    .O0(O0),
+    .O1(Foo_inst0_O1)
+);
+corebit_undriven corebit_undriven_inst0 (
+    .out(O1)
+);
+corebit_undriven corebit_undriven_inst1 (
+    .out(corebit_undriven_inst1_out)
+);
 endmodule
 
