ce5b089bc1d84dba02aa46577a083a663d1684a5
Lenny Truong <leonardtruong@fb.com>
2020-02-26 14:22:51 -0800

    Update golds

--- tests/test_syntax/gold/TestProductAccess.v
+++ tests/test_syntax/gold/TestProductAccess.v
@@ -1,4 +1,3 @@
-// Module `Mux2xTuplea0_OutBits8_a1_OutBits8` defined externally
 module coreir_reg #(
     parameter width = 1,
     parameter clk_posedge = 1,
@@ -15,6 +14,78 @@ module coreir_reg #(
     outReg <= in;
   end
   assign out = outReg;
+endmodule
+
+module coreir_mux #(
+    parameter width = 1
+) (
+    input [width-1:0] in0,
+    input [width-1:0] in1,
+    input sel,
+    output [width-1:0] out
+);
+  assign out = sel ? in1 : in0;
+endmodule
+
+module commonlib_muxn__N2__width8 (
+    input [7:0] in_data_0,
+    input [7:0] in_data_1,
+    input [0:0] in_sel,
+    output [7:0] out
+);
+wire [7:0] _join_out;
+coreir_mux #(
+    .width(8)
+) _join (
+    .in0(in_data_0),
+    .in1(in_data_1),
+    .sel(in_sel[0]),
+    .out(_join_out)
+);
+assign out = _join_out;
+endmodule
+
+module Mux2xOutBits8 (
+    input [7:0] I0,
+    input [7:0] I1,
+    input S,
+    output [7:0] O
+);
+wire [7:0] coreir_commonlib_mux2x8_inst0_out;
+commonlib_muxn__N2__width8 coreir_commonlib_mux2x8_inst0 (
+    .in_data_0(I0),
+    .in_data_1(I1),
+    .in_sel(S),
+    .out(coreir_commonlib_mux2x8_inst0_out)
+);
+assign O = coreir_commonlib_mux2x8_inst0_out;
+endmodule
+
+module Mux2xTuplea0_OutBits8_a1_OutBits8 (
+    input [7:0] I0_a0,
+    input [7:0] I0_a1,
+    input [7:0] I1_a0,
+    input [7:0] I1_a1,
+    output [7:0] O_a0,
+    output [7:0] O_a1,
+    input S
+);
+wire [7:0] Mux2xOutBits8_inst0_O;
+wire [7:0] Mux2xOutBits8_inst1_O;
+Mux2xOutBits8 Mux2xOutBits8_inst0 (
+    .I0(I0_a0),
+    .I1(I1_a0),
+    .S(S),
+    .O(Mux2xOutBits8_inst0_O)
+);
+Mux2xOutBits8 Mux2xOutBits8_inst1 (
+    .I0(I0_a1),
+    .I1(I1_a1),
+    .S(S),
+    .O(Mux2xOutBits8_inst1_O)
+);
+assign O_a0 = Mux2xOutBits8_inst0_O;
+assign O_a1 = Mux2xOutBits8_inst1_O;
 endmodule
 
 module TestProductAccess_comb (
