1bf85656f98c1381fbcb3cb1a52c420ec1a9fe71
Lenny Truong <leonardtruong@fb.com>
2020-02-26 15:42:05 -0800

    Pin kratos version for macos issue

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "coreir>=2.0.*",
         "hwtypes>=1.0.*",
         "ast_tools>=0.0.10",
-        "kratos"
+        "kratos==0.0.25"
     ],
     python_requires='>=3.6',
     long_description=long_description,
