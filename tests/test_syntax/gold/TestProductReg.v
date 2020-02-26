4bc1a5d0314ac49aaeb4fe17e2d70b4b7f399d4c
Lenny Truong <leonardtruong@fb.com>
2020-02-26 14:36:29 -0800

    Fix golds

--- tests/test_syntax/gold/TestProductReg.v
+++ tests/test_syntax/gold/TestProductReg.v
@@ -1,4 +1,3 @@
-// Module `Mux2xTuplea0_OutBit_a1_OutSInt8` defined externally
 module coreir_reg_arst #(
     parameter width = 1,
     parameter arst_posedge = 1,
@@ -20,6 +19,112 @@ module coreir_reg_arst #(
     else outReg <= in;
   end
   assign out = outReg;
+endmodule
+
+module coreir_mux #(
+    parameter width = 1
+) (
+    input [width-1:0] in0,
+    input [width-1:0] in1,
+    input sel,
+    output [width-1:0] out
+);
+  assign out = sel ? in1 : in0;
+endmodule
+
+module commonlib_muxn__N2__width8 (
+    input [7:0] in_data_0,
+    input [7:0] in_data_1,
+    input [0:0] in_sel,
+    output [7:0] out
+);
+wire [7:0] _join_out;
+coreir_mux #(
+    .width(8)
+) _join (
+    .in0(in_data_0),
+    .in1(in_data_1),
+    .sel(in_sel[0]),
+    .out(_join_out)
+);
+assign out = _join_out;
+endmodule
+
+module commonlib_muxn__N2__width1 (
+    input [0:0] in_data_0,
+    input [0:0] in_data_1,
+    input [0:0] in_sel,
+    output [0:0] out
+);
+wire [0:0] _join_out;
+coreir_mux #(
+    .width(1)
+) _join (
+    .in0(in_data_0),
+    .in1(in_data_1),
+    .sel(in_sel[0]),
+    .out(_join_out)
+);
+assign out = _join_out;
+endmodule
+
+module Mux2xOutSInt8 (
+    input [7:0] I0,
+    input [7:0] I1,
+    input S,
+    output [7:0] O
+);
+wire [7:0] coreir_commonlib_mux2x8_inst0_out;
+commonlib_muxn__N2__width8 coreir_commonlib_mux2x8_inst0 (
+    .in_data_0(I0),
+    .in_data_1(I1),
+    .in_sel(S),
+    .out(coreir_commonlib_mux2x8_inst0_out)
+);
+assign O = coreir_commonlib_mux2x8_inst0_out;
+endmodule
+
+module Mux2xOutBit (
+    input I0,
+    input I1,
+    input S,
+    output O
+);
+wire [0:0] coreir_commonlib_mux2x1_inst0_out;
+commonlib_muxn__N2__width1 coreir_commonlib_mux2x1_inst0 (
+    .in_data_0(I0),
+    .in_data_1(I1),
+    .in_sel(S),
+    .out(coreir_commonlib_mux2x1_inst0_out)
+);
+assign O = coreir_commonlib_mux2x1_inst0_out[0];
+endmodule
+
+module Mux2xTuplea0_OutBit_a1_OutSInt8 (
+    input I0_a0,
+    input [7:0] I0_a1,
+    input I1_a0,
+    input [7:0] I1_a1,
+    output O_a0,
+    output [7:0] O_a1,
+    input S
+);
+wire Mux2xOutBit_inst0_O;
+wire [7:0] Mux2xOutSInt8_inst0_O;
+Mux2xOutBit Mux2xOutBit_inst0 (
+    .I0(I0_a0),
+    .I1(I1_a0),
+    .S(S),
+    .O(Mux2xOutBit_inst0_O)
+);
+Mux2xOutSInt8 Mux2xOutSInt8_inst0 (
+    .I0(I0_a1),
+    .I1(I1_a1),
+    .S(S),
+    .O(Mux2xOutSInt8_inst0_O)
+);
+assign O_a0 = Mux2xOutBit_inst0_O;
+assign O_a1 = Mux2xOutSInt8_inst0_O;
 endmodule
 
 module TestProductReg_comb (
--- tests/test_syntax/gold/TestReplaceArrayStrKey.v
+++ tests/test_syntax/gold/TestReplaceArrayStrKey.v
@@ -1,16 +1,33 @@
-module coreir_const #(parameter width = 1, parameter value = 1) (output [width-1:0] out);
+module coreir_const #(
+    parameter width = 1,
+    parameter value = 1
+) (
+    output [width-1:0] out
+);
   assign out = value;
 endmodule
 
-module TestReplaceArrayStrKey_comb (output [1:0] O);
+module TestReplaceArrayStrKey_comb (
+    output [1:0] O
+);
 wire [1:0] const_3_2_out;
-coreir_const #(.value(2'h3), .width(2)) const_3_2(.out(const_3_2_out));
+coreir_const #(
+    .value(2'h3),
+    .width(2)
+) const_3_2 (
+    .out(const_3_2_out)
+);
 assign O = const_3_2_out;
 endmodule
 
-module TestReplaceArrayStrKey (input CLK, output [1:0] O);
+module TestReplaceArrayStrKey (
+    input CLK,
+    output [1:0] O
+);
 wire [1:0] TestReplaceArrayStrKey_comb_inst0_O;
-TestReplaceArrayStrKey_comb TestReplaceArrayStrKey_comb_inst0(.O(TestReplaceArrayStrKey_comb_inst0_O));
+TestReplaceArrayStrKey_comb TestReplaceArrayStrKey_comb_inst0 (
+    .O(TestReplaceArrayStrKey_comb_inst0_O)
+);
 assign O = TestReplaceArrayStrKey_comb_inst0_O;
 endmodule
 
