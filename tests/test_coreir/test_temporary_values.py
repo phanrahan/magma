6b34388dc6f0e149f18eb166053081d00b31a5bc
Lenny Truong <leonardtruong@fb.com>
2020-02-27 15:31:52 -0800

    Remove definition methods

--- tests/test_coreir/test_temporary_values.py
+++ tests/test_coreir/test_temporary_values.py
@@ -6,11 +6,9 @@ def test_temporary_named_value():
     class Main(m.Circuit):
         io = m.IO(I=m.In(m.Bit), O=m.Out(m.Bit))
 
-        @classmethod
-        def definition(io):
-            x = m.Bit(name="x")
-            m.wire(io.I, x)
-            m.wire(x, io.O)
+        x = m.Bit(name="x")
+        m.wire(io.I, x)
+        m.wire(x, io.O)
 
     m.compile("build/test_temporary_named_value", Main)
     check_files_equal(__file__, "build/test_temporary_named_value.v",
