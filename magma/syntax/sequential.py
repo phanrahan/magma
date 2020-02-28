fcb57c56db6450cc0dedbd5beb21b9acafd50348
Lenny Truong <leonardtruong@fb.com>
2020-02-27 16:34:55 -0800

    Update sequential

--- magma/syntax/sequential.py
+++ magma/syntax/sequential.py
@@ -138,10 +138,9 @@ def get_initial_value_map(init_func, defn_env):
         assert stmt.target.value.id == "self"
         # TODO: Should we deal with multiple assignments? For now we take the
         # last one
-        m.DefineCircuit("tmp")
-        eval_type = eval(astor.to_source(stmt.annotation).rstrip(), defn_env)
-        eval_value = eval(astor.to_source(stmt.value).rstrip(), defn_env)
-        m.EndCircuit()
+        class Tmp(m.Circuit):
+            eval_type = eval(astor.to_source(stmt.annotation).rstrip(), defn_env)
+            eval_value = eval(astor.to_source(stmt.value).rstrip(), defn_env)
         initial_value_map[stmt.target.attr] = (stmt.value, stmt.annotation,
                                                eval_type, eval_value)
     return initial_value_map
