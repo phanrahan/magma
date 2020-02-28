9dd52d6db75ac7b1f22badd6311ff707a61a7889
Lenny Truong <leonardtruong@fb.com>
2020-02-27 16:20:52 -0800

    Update join test

--- tests/test_higher/test_join.py
+++ tests/test_higher/test_join.py
@@ -1,12 +1,13 @@
-from magma import *
+import magma as m
 
-def test():
-    And2 = DeclareCircuit('And2', "I0", In(Bit), "I1", In(Bit), "O", Out(Bit))
+def test_join():
+    class And2(m.Circuit):
+        io = m.IO(I0=m.In(m.Bit), I1=m.In(m.Bit), O=m.Out(m.Bit))
 
     and0 = And2(name='and0')
     and1 = And2(name='and1')
 
-    a = join(and0, and1)
+    a = m.join(and0, and1)
     assert repr(a) == 'AnonymousCircuitType("I0", array([and0.I0, and1.I0]), "I1", array([and0.I1, and1.I1]), "O", array([and0.O, and1.O]))'
 
-test()
+test_join()
