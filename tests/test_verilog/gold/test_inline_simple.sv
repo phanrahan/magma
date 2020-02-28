67a806fa14775560c382fddba05bee23aee3ef06
Lenny Truong <leonardtruong@fb.com>
2020-02-28 10:19:15 -0800

    Update gold

--- tests/test_verilog/gold/test_inline_simple.sv
+++ tests/test_verilog/gold/test_inline_simple.sv
@@ -1,9 +1,3 @@
-module corebit_term (
-    input in
-);
-
-endmodule
-
 module FF(input I, output reg O, input CLK);
 always @(posedge CLK) begin
   O <= I;
@@ -18,9 +12,6 @@ FF FF_inst0 (
     .I(I),
     .O(O),
     .CLK(CLK)
-);
-corebit_term corebit_term_inst0 (
-    .in(I)
 );
 
 assert property (@(posedge CLK) I |-> ##1 O);
