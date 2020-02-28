e2eb0267b1a9a9f24793af9b076deb98b8839a72
Lenny Truong <leonardtruong@fb.com>
2020-02-27 16:20:19 -0800

    Update fork test

--- tests/test_higher/test_fork.py
+++ tests/test_higher/test_fork.py
@@ -1,7 +1,8 @@
-from magma import *
+import magma as m
 
-def test():
-    And2 = DeclareCircuit('And2', "I0", In(Bit), "I1", In(Bit), "O", Out(Bit))
+def test_fork():
+    class And2(m.Circuit):
+        io = m.IO(I0=m.In(m.Bit), I1=m.In(m.Bit), O=m.Out(m.Bit))
     #print(repr(And2))
     and2 = And2(name='and2')
     #print(repr(type(and2)))
@@ -9,15 +10,16 @@ def test():
     #print(repr(and2))
     assert str(and2.interface) == '"I0", In(Bit), "I1", In(Bit), "O", Out(Bit)'
 
-    Or2 = DeclareCircuit('Or2', "I0", In(Bit), "I1", In(Bit), "O", Out(Bit))
+    class Or2(m.Circuit):
+        io = m.IO(I0=m.In(m.Bit), I1=m.In(m.Bit), O=m.Out(m.Bit))
     or2 = Or2(name='or2')
     #print(or2)
     #print(repr(type(or2)))
     assert str(or2.interface) == '"I0", In(Bit), "I1", In(Bit), "O", Out(Bit)'
 
-    lut = fork(and2,or2)
+    lut = m.fork(and2,or2)
     assert len(lut.interface) == 3
     #print(type(lut))
     #print(repr(lut))
 
-test()
+test_fork()
