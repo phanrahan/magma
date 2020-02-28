0aaf48aa763ffc4296a0c551324502ce37291bc1
Lenny Truong <leonardtruong@fb.com>
2020-02-27 16:19:10 -0800

    Fix deprecation in flat

--- tests/test_higher/test_flat.py
+++ tests/test_higher/test_flat.py
@@ -1,16 +1,19 @@
-from magma import *
+import magma as m
 
 def test_flat():
-    F = DeclareCircuit('F', "I", In(Array[2,Bits[2]]), "O", Out(Bit))
+    class F(m.Circuit):
+        io = m.IO(I=m.In(m.Array[2, m.Bits[2]]), O=m.Out(m.Bit))
     f = F(name="f")
-    g = flat(f)
+    g = m.flat(f)
     assert len(g.interface) == 2
     assert len(g.I) == 4
 
 def test_partition():
-    F = DeclareCircuit('F', "I", In(Bits[4]), "O", Out(Bit))
+    class F(m.Circuit):
+        io = m.IO(I=m.In(m.Bits[4]), O=m.Out(m.Bit))
+
     f = F(name="f")
-    g = partition(f, 2)
+    g = m.partition(f, 2)
     assert len(g.interface) == 3
     assert len(g.I0) == 2
     assert len(g.I1) == 2
