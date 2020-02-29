beb1b111401a3b6b9ad4289ee7230c053fdf221a
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-02-28 18:08:55 -0800

    Cleanup test_interface.py

--- tests/test_interface/test_interface.py
+++ tests/test_interface/test_interface.py
@@ -1,23 +1,19 @@
-from magma import *
+import magma as m
 from magma.testing.utils import has_error
-"""
-http://doc.pytest.org/en/latest/capture.html#accessing-captured-output-from-a-test-function
-Uses pytest API for reading stdout/stderr for testing
-"""
+
 
 def test_1():
-    I0 = DeclareInterface("a", In(Bit), "b", Out(Bits[2]))
+    I0 = m.DeclareInterface("a", m.In(m.Bit), "b", m.Out(m.Bits[2]))
     assert str(I0) == '"a", In(Bit), "b", Out(Bits[2])'
 
     i0 = I0()
-    print(i0)
     assert str(i0) == '"a", In(Bit), "b", Out(Bits[2])'
 
 
 def test_io_class_basic():
     expected_names = ["y", "x"]
     expected_types = [m.In(m.Bit), m.Out(m.Bits[8])]
-    io = IO(y=m.In(m.Bit), x=m.Out(m.Bits[8]))
+    io = m.IO(y=m.In(m.Bit), x=m.Out(m.Bits[8]))
     assert list(io.ports.keys()) == expected_names
     ports = list(io.ports.values())
     assert all(type(ports[i]) is expected_types[i].flip() for i in range(2))
@@ -28,7 +24,7 @@ def test_io_class_basic():
 
 
 def test_io_class_bind(caplog):
-    io = IO(y=m.In(m.Bit), x=m.Out(m.Bits[8]))
+    io = m.IO(y=m.In(m.Bit), x=m.Out(m.Bits[8]))
 
     class _Foo(m.Circuit):
         pass
