e69a50669be72126d2296fe08734226d6b31f0d8
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-03-02 12:56:45 -0800

    Fix circuit declaration repr logic

--- tests/test_circuit/test_declare.py
+++ tests/test_circuit/test_declare.py
@@ -1,18 +1,30 @@
-
 import magma as m
 
 
+class _And2(m.Circuit):
+    name = "And2"
+    io = m.IO(I0=m.In(m.Bit), I1=m.In(m.Bit), O=m.Out(m.Bit))
+
+
+def test_declare_repr():
+    assert str(_And2) == 'And2(I0: In(Bit), I1: In(Bit), O: Out(Bit))'
+    assert repr(_And2) == ('And2 = DeclareCircuit("And2", "I0", In(Bit), "I1", '
+                           'In(Bit), "O", Out(Bit))')
+    and2 = _And2(name="and2")
+    assert str(and2) == "and2<And2(I0: In(Bit), I1: In(Bit), O: Out(Bit))>"
+    assert str(and2.I0) == "I0"
+
+
 def test_declare_simple():
-    And2 = m.DeclareCircuit("And2", "I0", m.In(m.Bit), "I1", m.In(m.Bit), "O",
-                            m.Out(m.Bit))
-    assert isinstance(And2.I0, m.Bit)
+    assert isinstance(_And2.I0, m.Bit)
 
 
 def test_declare_interface_polarity():
-    And2Decl = m.DeclareCircuit("And2", "I0", m.In(m.Bit), "I1", m.In(m.Bit),
-                                "O", m.Out(m.Bit))
-    And2Defn = m.DefineCircuit("And2", "I0", m.In(m.Bit), "I1", m.In(m.Bit),
-                               "O", m.Out(m.Bit))
 
-    assert And2Decl.interface.ports["I0"].is_input() == \
-        And2Defn.interface.ports["I0"].is_input()
+    class _And2Defn(m.Circuit):
+        name = "And2"
+        io = m.IO(I0=m.In(m.Bit), I1=m.In(m.Bit), O=m.Out(m.Bit))
+        io.O <= io.I0  # just something to make this a definition
+
+    assert (_And2.interface.ports["I0"].is_input() ==
+            _And2Defn.interface.ports["I0"].is_input())
