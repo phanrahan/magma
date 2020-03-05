30246b83e4899dee986de9741aa27cbfc8997172
Lenny Truong <leonardtruong@fb.com>
2020-03-05 10:27:13 -0800

    Release 2.0.20

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.0.19',
+    version='2.0.20',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
