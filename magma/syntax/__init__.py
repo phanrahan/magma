e3c2ad05e799dac67868cfe43cf7a518eaab8011
Lenny Truong <lenny@stanford.edu>
2020-03-10 17:31:06 -0700

    Remove old import

--- magma/syntax/__init__.py
+++ magma/syntax/__init__.py
@@ -1,3 +1,3 @@
 from magma.syntax.combinational import combinational
 from magma.syntax.verilog import build_kratos_debug_info
-from .coroutine import coroutine, generator
+from .coroutine import coroutine
