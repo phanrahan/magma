9e67d65fbcfc76242dd686448590028f3b6c91ab
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-04-06 17:15:47 -0700

    Release 2.0.35

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.0.34',
+    version='2.0.35',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
