2ce77302be12d8dec2fc5f859f112a152b03f688
Lenny Truong <leonardtruong@fb.com>
2020-04-13 16:57:51 -0700

    Typo fix

--- magma/clock.py
+++ magma/clock.py
@@ -101,7 +101,7 @@ def _wire_clock_port(port, clocktype, defnclk):
 def _get_clocks(port, clocktype):
     if isinstance(port, Tuple):
         return sum([_get_clocks(elem, clocktype) for elem in port], [])
-    if isinstance(port, Tuple):
+    if isinstance(port, Array):
         result = _get_clocks(port[0], clocktype)
         # Only traverse all children circuit if first child has a clock
         if result:
