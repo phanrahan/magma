7d077e47c36f8f853901eaabf82c0e6f49d9e16f
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-04-18 02:19:43 -0700

    Use CoreIR verilog compilation C API

--- magma/backend/coreir_compiler.py
+++ magma/backend/coreir_compiler.py
@@ -11,23 +11,6 @@ from ..logging import root_logger
 _logger = root_logger()
 
 
-def _make_verilog_cmd(deps, basename, opts):
-    cmd = "coreir"
-    if deps:
-        cmd += f" -l {','.join(deps)}"
-    cmd += f" -i {basename}.json"
-    if opts.get("split", ""):
-        split = opts["split"]
-        cmd += f" -o \"{split}/*.v\" -s"
-    else:
-        cmd += f" -o {basename}.v"
-    if opts.get("inline", False):
-        cmd += " --inline"
-    if opts.get("verilator_debug", False):
-        cmd += " --verilator_debug"
-    return cmd
-
-
 class CoreIRCompiler(Compiler):
     def __init__(self, main, basename, opts):
         super().__init__(main, basename, opts)
@@ -35,53 +18,57 @@ class CoreIRCompiler(Compiler):
         self.passes = opts.get("passes", [])
         if "markdirty" not in self.passes:
             self.passes.append("markdirty")
-        self.deps = self.__deps()
+        self.deps = self._deps()
+        self.backend = coreir_frontend.GetCoreIRBackend()
 
     def compile(self):
-        self.__compile_coreir()
-        if self.opts.get("output_verilog", False):
-            self.__compile_verilog()
-            return
-        if self.opts.get("header_file", False):
-            _logger.warning("[coreir-compiler] header_file only supported "
-                            "when output_verilog=True, ignoring")
-        if self.opts.get("header_str", False):
-            _logger.warning("[coreir-compiler] header_str only supported "
-                            "when output_verilog=True, ignoring")
-        if self.opts.get("footer_str", False):
-            _logger.warning("[coreir-compiler] footer_str only "
-                            "supported when output_verilog=True, ignoring")
-
-    def __compile_coreir(self):
-        backend = coreir_frontend.GetCoreIRBackend()
         InsertCoreIRWires(self.main).run()
         InsertWrapCasts(self.main).run()
+        backend = self.backend
         backend.compile(self.main)
         backend.context.run_passes(self.passes, self.namespaces)
-        main_key = self.main.coreir_name
-        backend.modules[main_key].save_to_file(self.basename + ".json")
+        output_json = (self.opts.get("output_intermediate", False) or
+                       not self.opts.get("output_verilog", False))
+        if output_json:
+            filename = f"{self.basename}.json"
+            backend.modules[self.main.coreir_name].save_to_file(filename)
+        if self.opts.get("output_verilog", False):
+            self._compile_verilog()
+            return
+        has_header_or_footer = (self.opts.get("header_file", "") or
+                                self.opts.get("header_str", "") or
+                                self.opts.get("footer_str", ""))
+        if has_header_or_footer:
+            _logger.warning("[coreir-compiler] header/footer only supported "
+                            "when output_verilog=True, ignoring")
 
-    def __compile_verilog(self):
-        # TODO(rsetaluri): Expose compilation to verilog in pycoreir rather than
-        # calling the binary from the command line.
-        cmd = _make_verilog_cmd(self.deps, self.basename, self.opts)
-
-        ret = subprocess.run(cmd, shell=True).returncode
-        if ret:
-            raise RuntimeError(f"CoreIR cmd '{cmd}' failed with code {ret}")
+    def _compile_verilog(self):
+        top = self.backend.modules[self.main.coreir_name]
+        filename = f"{self.basename}.v"
+        opts = dict(
+            split=self.opts.get("split", ""),
+            inline=self.opts.get("inline", False),
+            verilator_debug=self.opts.get("verilator_debug", False),
+        )
+        ret = self.backend.context.compile_to_verilog(top, filename, **opts)
+        if not ret:
+            raise RuntimeError(f"CoreIR compilation to verilog failed")
         self._process_header_footer()
 
-        backend = coreir_frontend.GetCoreIRBackend()
-        # TODO: We need fresh bind_files for each compile call
-        for name, file in backend.sv_bind_files.items():
+        # TODO(leonardt): We need fresh bind_files for each compile call.
+        for name, file in self.backend.sv_bind_files.items():
             filename = os.path.join(os.path.dirname(self.basename), name)
             with open(f"{filename}.sv", "w") as f:
                 f.write(file)
 
         if self.opts.get("sv", False):
-            subprocess.run(["mv", f"{self.basename}.v", f"{self.basename}.sv"])
+            if self.opts.get("split", False):
+                _logger.warning("sv not supported with split, ignoring")
+            else:
+                args = (f"{self.basename}.v", f"{self.basename}.sv")
+                subprocess.run(["mv", *args])
 
-    def __deps(self):
+    def _deps(self):
         deps = self.opts.get("coreir_libs", set())
         pass_ = InstanceGraphPass(self.main)
         pass_.run()
@@ -95,20 +82,18 @@ class CoreIRCompiler(Compiler):
 
     def _process_header_footer(self):
         header = ""
-        footer = ""
-        if self.opts.get("header_file", False):
+        if self.opts.get("header_file", ""):
             with open(self.opts["header_file"], "r") as header_file:
                 header = header_file.read() + "\n"
-        if self.opts.get("header_str", False):
+        if self.opts.get("header_str", ""):
             header += self.opts["header_str"] + "\n"
-        if self.opts.get("footer_str", False):
-            footer = self.opts["footer_str"]
-        if header is not "" or footer is not "":
-            if self.opts.get("split", False):
-                _logger.warning(
-                    "header/footer logic not supported with split, ignoring")
-                return
-            with open(f"{self.basename}.v", "r") as verilog_file:
-                verilog = verilog_file.read()
-            with open(f"{self.basename}.v", "w") as verilog_file:
-                verilog_file.write(header + verilog + "\n" + footer)
+        footer = self.opts.get("footer_str", "")
+        if not header and not footer:
+            return
+        if self.opts.get("split", ""):
+            _logger.warning("header/footer not supported with split, ignoring")
+            return
+        with open(f"{self.basename}.v", "r") as verilog_file:
+            verilog = verilog_file.read()
+        with open(f"{self.basename}.v", "w") as verilog_file:
+            verilog_file.write(header + verilog + "\n" + footer)
