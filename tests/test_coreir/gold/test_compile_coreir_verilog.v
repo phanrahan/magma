34f194b8fdb0ebd8222fb515af819cb7832dcd66
Lenny Truong <lenny@stanford.edu>
2020-05-29 10:01:25 -0700

    Remove old gold

--- tests/test_coreir/gold/test_compile_coreir_verilog.v
+++
@@ -1,7 +0,0 @@
-// Module `And2` defined externally
-module main (input [1:0] I, output O);
-wire And2_inst0_O;
-And2 And2_inst0(.I0(I[0]), .I1(I[1]), .O(And2_inst0_O));
-assign O = And2_inst0_O;
-endmodule
-
