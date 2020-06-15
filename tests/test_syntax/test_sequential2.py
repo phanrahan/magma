efdf9dc288c38304e2c9adbcefd4bda066c9f0c7
Lenny Truong <lenny@stanford.edu>
2020-06-15 10:44:43 -0700

    Add seq post pass test

--- tests/test_syntax/test_sequential2.py
+++ tests/test_syntax/test_sequential2.py
@@ -1,3 +1,6 @@
+import os
+import inspect
+
 import ast_tools
 
 import magma as m
@@ -67,8 +70,13 @@ def test_sequential2_hierarchy():
                              f"gold/TestSequential2Hierarchy.v")
 
 
-def test_sequential2_pre_unroll():
-    @m.sequential2(pre_passes=[ast_tools.passes.loop_unroll()])
+def test_sequential2_pre_unroll(capsys):
+    l0 = inspect.currentframe().f_lineno + 1
+
+    @m.sequential2(pre_passes=[ast_tools.passes.loop_unroll()],
+                   post_passes=[
+                       ast_tools.passes.debug(dump_source_filename=True,
+                                              dump_source_lines=True)])
     class LoopUnroll:
         def __init__(self):
             self.regs = [[Register(4) for _ in range(3)] for _ in range(2)]
@@ -80,6 +88,23 @@ def test_sequential2_pre_unroll():
                     self.regs[i][2 - j] = self.regs[i][1 - j]
                 self.regs[1 - i][0] = self.regs[i][-1] if m.Bit(i == 0) else I
             return O
+
+    assert capsys.readouterr().out == f"""\
+BEGIN SOURCE_FILENAME
+{os.path.abspath(__file__)}
+END SOURCE_FILENAME
+
+BEGIN SOURCE_LINES
+{l0+9}:        def __call__(self, I: m.Bits[4]) -> m.Bits[4]:
+{l0+10}:            O = self.regs[1][-1]
+{l0+11}:            for i in ast_tools.macros.unroll(range(2)):
+{l0+12}:                for j in ast_tools.macros.unroll(range(2)):
+{l0+13}:                    self.regs[i][2 - j] = self.regs[i][1 - j]
+{l0+14}:                self.regs[1 - i][0] = self.regs[i][-1] if m.Bit(i == 0) else I
+{l0+15}:            return O
+END SOURCE_LINES
+
+"""
 
     m.compile("build/TestSequential2NestedLoopUnroll", LoopUnroll)
     assert check_files_equal(__file__, f"build/TestSequential2NestedLoopUnroll.v",
