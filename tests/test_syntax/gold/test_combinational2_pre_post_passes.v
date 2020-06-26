b9fda36adeea91c58b8010a05195a4e1e926c993
Lenny Truong <lenny@stanford.edu>
2020-06-26 10:36:58 -0700

    Fix seq2 product bug

--- tests/test_syntax/gold/test_combinational2_pre_post_passes.v
+++ tests/test_syntax/gold/test_combinational2_pre_post_passes.v
@@ -1,7 +1,49 @@
+module commonlib_muxn__N2__width3 (
+    input [2:0] in_data_0,
+    input [2:0] in_data_1,
+    input [0:0] in_sel,
+    output [2:0] out
+);
+assign out = in_sel[0] ? in_data_1 : in_data_0;
+endmodule
+
+module Mux2xOutBits3 (
+    input [2:0] I0,
+    input [2:0] I1,
+    input S,
+    output [2:0] O
+);
+commonlib_muxn__N2__width3 coreir_commonlib_mux2x3_inst0 (
+    .in_data_0(I0),
+    .in_data_1(I1),
+    .in_sel(S),
+    .out(O)
+);
+endmodule
+
 module pre_unroll (
     input [2:0] I,
     output [2:0] O
 );
-assign O = I[0] ? 3'h0 : I[1] ? 3'h1 : I[2] ? 3'h2 : 3'h4;
+wire [2:0] Mux2xOutBits3_inst0_O;
+wire [2:0] Mux2xOutBits3_inst1_O;
+Mux2xOutBits3 Mux2xOutBits3_inst0 (
+    .I0(3'h4),
+    .I1(3'h2),
+    .S(I[2]),
+    .O(Mux2xOutBits3_inst0_O)
+);
+Mux2xOutBits3 Mux2xOutBits3_inst1 (
+    .I0(Mux2xOutBits3_inst0_O),
+    .I1(3'h1),
+    .S(I[1]),
+    .O(Mux2xOutBits3_inst1_O)
+);
+Mux2xOutBits3 Mux2xOutBits3_inst2 (
+    .I0(Mux2xOutBits3_inst1_O),
+    .I1(3'h0),
+    .S(I[0]),
+    .O(O)
+);
 endmodule
 
