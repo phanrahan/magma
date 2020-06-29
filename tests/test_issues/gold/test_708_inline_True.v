99f6f5b49ce2e6a75a3b4a632c13a46d7f449ebd
Lenny Truong <lenny@stanford.edu>
2020-06-29 13:19:05 -0700

    Update gold

--- tests/test_issues/gold/test_708_inline_True.v
+++ tests/test_issues/gold/test_708_inline_True.v
@@ -16,27 +16,22 @@ module coreir_reg #(
   assign out = outReg;
 endmodule
 
-module commonlib_muxn__N2__width8 (
-    input [7:0] in_data_0,
-    input [7:0] in_data_1,
-    input [0:0] in_sel,
-    output [7:0] out
-);
-assign out = in_sel[0] ? in_data_1 : in_data_0;
-endmodule
-
 module Mux2xTuplex_OutUInt8 (
     input [7:0] I0_x,
     input [7:0] I1_x,
     output [7:0] O_x,
     input S
 );
-commonlib_muxn__N2__width8 coreir_commonlib_mux2x8_inst0 (
-    .in_data_0(I0_x),
-    .in_data_1(I1_x),
-    .in_sel(S),
-    .out(O_x)
-);
+reg [7:0] coreir_commonlib_mux2x8_inst0_out;
+always @(*) begin
+if (S == 0) begin
+    coreir_commonlib_mux2x8_inst0_out = I0_x;
+end else begin
+    coreir_commonlib_mux2x8_inst0_out = I1_x;
+end
+end
+
+assign O_x = coreir_commonlib_mux2x8_inst0_out;
 endmodule
 
 module Test_comb (
