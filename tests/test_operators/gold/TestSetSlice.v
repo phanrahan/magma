938a45620a2916d4cfc5c6a4e83b6e03e8d50e85
Lenny Truong <lenny@stanford.edu>
2020-06-30 17:58:08 -0700

    Update gold

--- tests/test_operators/gold/TestSetSlice.v
+++ tests/test_operators/gold/TestSetSlice.v
@@ -34,17 +34,17 @@ wire Mux2xOutBit_inst7_O;
 wire Mux2xOutBit_inst8_O;
 wire Mux2xOutBit_inst9_O;
 wire [3:0] magma_Bits_4_sub_inst0_out;
-wire [3:0] magma_Bits_4_sub_inst1_out;
 wire [3:0] magma_Bits_4_sub_inst10_out;
-wire [3:0] magma_Bits_4_sub_inst11_out;
+wire [3:0] magma_Bits_4_sub_inst12_out;
+wire [3:0] magma_Bits_4_sub_inst14_out;
+wire [3:0] magma_Bits_4_sub_inst16_out;
+wire [3:0] magma_Bits_4_sub_inst18_out;
 wire [3:0] magma_Bits_4_sub_inst2_out;
-wire [3:0] magma_Bits_4_sub_inst3_out;
+wire [3:0] magma_Bits_4_sub_inst20_out;
+wire [3:0] magma_Bits_4_sub_inst22_out;
 wire [3:0] magma_Bits_4_sub_inst4_out;
-wire [3:0] magma_Bits_4_sub_inst5_out;
 wire [3:0] magma_Bits_4_sub_inst6_out;
-wire [3:0] magma_Bits_4_sub_inst7_out;
 wire [3:0] magma_Bits_4_sub_inst8_out;
-wire [3:0] magma_Bits_4_sub_inst9_out;
 wire [5:0] magma_Bits_6_lshr_inst0_out;
 wire [5:0] magma_Bits_6_lshr_inst1_out;
 wire [5:0] magma_Bits_6_lshr_inst10_out;
@@ -60,99 +60,99 @@ wire [5:0] magma_Bits_6_lshr_inst9_out;
 Mux2xOutBit Mux2xOutBit_inst0 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst0_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h0) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h0)),
+    .S(1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h0)),
     .O(Mux2xOutBit_inst0_O)
 );
 Mux2xOutBit Mux2xOutBit_inst1 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst1_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h1) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h1)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h1)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h1)),
     .O(Mux2xOutBit_inst1_O)
 );
 Mux2xOutBit Mux2xOutBit_inst10 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst10_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'ha) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'ha)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'ha)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'ha)),
     .O(Mux2xOutBit_inst10_O)
 );
 Mux2xOutBit Mux2xOutBit_inst11 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst11_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'hb) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'hb)),
+    .S(1'b1 & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'hb)),
     .O(Mux2xOutBit_inst11_O)
 );
 Mux2xOutBit Mux2xOutBit_inst2 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst2_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h2) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h2)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h2)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h2)),
     .O(Mux2xOutBit_inst2_O)
 );
 Mux2xOutBit Mux2xOutBit_inst3 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst3_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h3) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h3)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h3)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h3)),
     .O(Mux2xOutBit_inst3_O)
 );
 Mux2xOutBit Mux2xOutBit_inst4 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst4_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h4) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h4)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h4)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h4)),
     .O(Mux2xOutBit_inst4_O)
 );
 Mux2xOutBit Mux2xOutBit_inst5 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst5_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h5) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h5)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h5)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h5)),
     .O(Mux2xOutBit_inst5_O)
 );
 Mux2xOutBit Mux2xOutBit_inst6 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst6_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h6) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h6)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h6)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h6)),
     .O(Mux2xOutBit_inst6_O)
 );
 Mux2xOutBit Mux2xOutBit_inst7 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst7_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h7) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h7)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h7)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h7)),
     .O(Mux2xOutBit_inst7_O)
 );
 Mux2xOutBit Mux2xOutBit_inst8 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst8_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h8) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h8)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h8)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h8)),
     .O(Mux2xOutBit_inst8_O)
 );
 Mux2xOutBit Mux2xOutBit_inst9 (
     .I0(1'b1),
     .I1(magma_Bits_6_lshr_inst9_out[0]),
-    .S((({1'b0,1'b0,x[1:0]}) <= 4'h9) & ((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) > 4'h9)),
+    .S((1'b1 & (({1'b0,1'b0,x[1:0]}) <= 4'h9)) & ((4'((4'(({1'b0,1'b0,x[1:0]}) + 4'h6)) - 4'h1)) >= 4'h9)),
     .O(Mux2xOutBit_inst9_O)
 );
 assign magma_Bits_4_sub_inst0_out = 4'(4'h0 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst1_out = 4'(4'h1 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst10_out = 4'(4'ha - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst11_out = 4'(4'hb - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst2_out = 4'(4'h2 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst3_out = 4'(4'h3 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst4_out = 4'(4'h4 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst5_out = 4'(4'h5 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst6_out = 4'(4'h6 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst7_out = 4'(4'h7 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst8_out = 4'(4'h8 - ({1'b0,1'b0,x[1:0]}));
-assign magma_Bits_4_sub_inst9_out = 4'(4'h9 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst10_out = 4'(4'h5 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst12_out = 4'(4'h6 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst14_out = 4'(4'h7 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst16_out = 4'(4'h8 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst18_out = 4'(4'h9 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst2_out = 4'(4'h1 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst20_out = 4'(4'ha - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst22_out = 4'(4'hb - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst4_out = 4'(4'h2 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst6_out = 4'(4'h3 - ({1'b0,1'b0,x[1:0]}));
+assign magma_Bits_4_sub_inst8_out = 4'(4'h4 - ({1'b0,1'b0,x[1:0]}));
 assign magma_Bits_6_lshr_inst0_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst0_out[3:0]});
-assign magma_Bits_6_lshr_inst1_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst1_out[3:0]});
-assign magma_Bits_6_lshr_inst10_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst10_out[3:0]});
-assign magma_Bits_6_lshr_inst11_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst11_out[3:0]});
-assign magma_Bits_6_lshr_inst2_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst2_out[3:0]});
-assign magma_Bits_6_lshr_inst3_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst3_out[3:0]});
-assign magma_Bits_6_lshr_inst4_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst4_out[3:0]});
-assign magma_Bits_6_lshr_inst5_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst5_out[3:0]});
-assign magma_Bits_6_lshr_inst6_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst6_out[3:0]});
-assign magma_Bits_6_lshr_inst7_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst7_out[3:0]});
-assign magma_Bits_6_lshr_inst8_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst8_out[3:0]});
-assign magma_Bits_6_lshr_inst9_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst9_out[3:0]});
+assign magma_Bits_6_lshr_inst1_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst2_out[3:0]});
+assign magma_Bits_6_lshr_inst10_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst20_out[3:0]});
+assign magma_Bits_6_lshr_inst11_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst22_out[3:0]});
+assign magma_Bits_6_lshr_inst2_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst4_out[3:0]});
+assign magma_Bits_6_lshr_inst3_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst6_out[3:0]});
+assign magma_Bits_6_lshr_inst4_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst8_out[3:0]});
+assign magma_Bits_6_lshr_inst5_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst10_out[3:0]});
+assign magma_Bits_6_lshr_inst6_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst12_out[3:0]});
+assign magma_Bits_6_lshr_inst7_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst14_out[3:0]});
+assign magma_Bits_6_lshr_inst8_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst16_out[3:0]});
+assign magma_Bits_6_lshr_inst9_out = I >> ({1'b0,1'b0,magma_Bits_4_sub_inst18_out[3:0]});
 assign O = {Mux2xOutBit_inst11_O,Mux2xOutBit_inst10_O,Mux2xOutBit_inst9_O,Mux2xOutBit_inst8_O,Mux2xOutBit_inst7_O,Mux2xOutBit_inst6_O,Mux2xOutBit_inst5_O,Mux2xOutBit_inst4_O,Mux2xOutBit_inst3_O,Mux2xOutBit_inst2_O,Mux2xOutBit_inst1_O,Mux2xOutBit_inst0_O};
 endmodule
 
