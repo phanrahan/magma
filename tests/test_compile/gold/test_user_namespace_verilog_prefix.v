ae628b9b3cb281f4243aea4d3607b6d03eee78fe
Lenny Truong <lenny@stanford.edu>
2020-07-16 13:39:58 -0700

    Fix usernamespace decl bug

--- tests/test_compile/gold/test_user_namespace_verilog_prefix.v
+++ tests/test_compile/gold/test_user_namespace_verilog_prefix.v
@@ -1,11 +1,20 @@
+// Module `_Decl` defined externally
 module my_namespace_Bar (
     input I_x,
     input I_y,
     output O_x,
     output O_y
 );
-assign O_x = I_x;
-assign O_y = I_y;
+wire _Decl_inst0_O_x;
+wire _Decl_inst0_O_y;
+my_namespace__Decl _Decl_inst0 (
+    .I_x(I_x),
+    .I_y(I_y),
+    .O_x(_Decl_inst0_O_x),
+    .O_y(_Decl_inst0_O_y)
+);
+assign O_x = _Decl_inst0_O_x;
+assign O_y = _Decl_inst0_O_y;
 endmodule
 
 module my_namespace_Foo (
@@ -16,7 +25,7 @@ module my_namespace_Foo (
 );
 wire Bar_inst0_O_x;
 wire Bar_inst0_O_y;
-Bar Bar_inst0 (
+my_namespace_Bar Bar_inst0 (
     .I_x(I_x),
     .I_y(I_y),
     .O_x(Bar_inst0_O_x),
