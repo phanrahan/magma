caeb7655de114078c644ec8d3ff937bb37e03250
Lenny Truong <lenny@stanford.edu>
2020-07-17 15:12:30 -0700

    Prototype inline combinational

---
+++ magma/syntax/inline_combinational.py
@@ -0,0 +1,92 @@
+import ast
+from typing import MutableMapping, Optional
+
+import astor
+
+from ast_tools.stack import SymbolTable
+from ast_tools.passes import (apply_ast_passes, Pass, PASS_ARGS_T, ssa,
+                              if_to_phi, bool_to_bit)
+from ast_tools.common import gen_free_prefix
+from ast_tools.immutable_ast import immutable, mutable
+
+from magma.syntax.combinational2 import combinational2
+from magma.bit import Bit
+from magma.wire import wire
+
+
+class _ToCombinationalRewriter(ast.NodeTransformer):
+    def __init__(self, free_prefix, target_map):
+        self.prefix = free_prefix
+        self.target_map = target_map
+        self.name_count = 0
+
+    def _gen_new_name(self):
+        new_name = f"{self.prefix}{self.name_count}"
+        self.name_count += 1
+        return new_name
+
+    def visit_Attribute(self, node):
+        if isinstance(node.ctx, ast.Store):
+            key = immutable(node)
+            if key not in self.target_map:
+                retval_name = self._gen_new_name()
+                self.target_map[key] = retval_name
+            return ast.Name(self.target_map[key], ast.Store())
+        return node
+
+
+class _RewriteToCombinational(Pass):
+    """
+    Replace assignment targets with a temporary value (so it is handled
+    properly by SSA), store the mapping from temporary values so they can be
+    wired up later, "inputs" will just read from the enclosing scope
+    """
+    def __init__(self, target_map):
+        super().__init__()
+        self.target_map = target_map
+
+    def rewrite(
+        self, tree: ast.AST, env: SymbolTable, metadata: MutableMapping
+    ) -> PASS_ARGS_T:
+        # prefix used for temporaries
+        prefix = gen_free_prefix(tree, env)
+        tree = _ToCombinationalRewriter(prefix, self.target_map).visit(tree)
+        elts = ()
+        for value in self.target_map.values():
+            elts += (ast.Name(value, ast.Load()),)
+        if len(elts) > 1:
+            retval = ast.Tuple(elts, ast.Load())
+        else:
+            retval = elts[0]
+        tree.body.append(ast.Return(retval))
+        return tree, env, metadata
+
+
+class inline_combinational(apply_ast_passes):
+    def __init__(self, pre_passes=[], post_passes=[],
+                 debug: bool = False,
+                 env: Optional[SymbolTable] = None,
+                 path: Optional[str] = None,
+                 file_name: Optional[str] = None
+                 ):
+        self.target_map = {}
+        passes = (pre_passes + [_RewriteToCombinational(self.target_map),
+                                ssa(strict=False), bool_to_bit(),
+                                if_to_phi(lambda s, t, f: s.ite(t, f))] +
+                  post_passes)
+        super().__init__(passes=passes, env=env, debug=debug, path=path,
+                         file_name=file_name)
+
+    def exec(self, etree, stree, env, metadata):
+        fn = super().exec(etree, stree, env, metadata)
+        annotations = {}
+        # TODO: Introspect type, for now we assume Bit
+        if len(self.target_map) > 1:
+            annotations["return"] = tuple(Bit for _ in self.target_map)
+        else:
+            annotations["return"] = Bit
+        result = fn()
+        if not isinstance(result, tuple):
+            result = (result,)
+        for key, value in zip(self.target_map.keys(), result):
+            wire(eval(astor.to_source(mutable(key)).rstrip(), {}, env), value)
---
+++ tests/test_syntax/gold/test_inline_comb_basic.v
@@ -0,0 +1,74 @@
+module coreir_reg #(
+    parameter width = 1,
+    parameter clk_posedge = 1,
+    parameter init = 1
+) (
+    input clk,
+    input [width-1:0] in,
+    output [width-1:0] out
+);
+  reg [width-1:0] outReg=init;
+  wire real_clk;
+  assign real_clk = clk_posedge ? clk : ~clk;
+  always @(posedge real_clk) begin
+    outReg <= in;
+  end
+  assign out = outReg;
+endmodule
+
+module Register (
+    input I,
+    output O,
+    input CLK
+);
+wire [0:0] reg_P_inst0_out;
+coreir_reg #(
+    .clk_posedge(1'b1),
+    .init(1'h0),
+    .width(1)
+) reg_P_inst0 (
+    .clk(CLK),
+    .in(I),
+    .out(reg_P_inst0_out)
+);
+assign O = reg_P_inst0_out[0];
+endmodule
+
+module Mux2xOutBit (
+    input I0,
+    input I1,
+    input S,
+    output O
+);
+reg [0:0] coreir_commonlib_mux2x1_inst0_out;
+always @(*) begin
+if (S == 0) begin
+    coreir_commonlib_mux2x1_inst0_out = I0;
+end else begin
+    coreir_commonlib_mux2x1_inst0_out = I1;
+end
+end
+
+assign O = coreir_commonlib_mux2x1_inst0_out[0];
+endmodule
+
+module Main (
+    input I,
+    input invert,
+    output O,
+    input CLK
+);
+wire Mux2xOutBit_inst0_O;
+Mux2xOutBit Mux2xOutBit_inst0 (
+    .I0(O),
+    .I1(~ O),
+    .S(invert),
+    .O(Mux2xOutBit_inst0_O)
+);
+Register Register_inst0 (
+    .I(Mux2xOutBit_inst0_O),
+    .O(O),
+    .CLK(CLK)
+);
+endmodule
+
