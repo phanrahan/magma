6f75e3df7da4146e00967d8ae64f8b32e9949f0c
Lenny Truong <lenny@stanford.edu>
2020-07-20 14:20:05 -0700

    Add support for normal assign

--- tests/test_syntax/test_inline_comb.py
+++ tests/test_syntax/test_inline_comb.py
@@ -4,7 +4,7 @@ from magma.testing import check_files_equal
 
 def test_inline_comb_basic():
     class Main(m.Circuit):
-        io = m.IO(invert=m.In(m.Bit), O=m.Out(m.Bit))
+        io = m.IO(invert=m.In(m.Bit), O0=m.Out(m.Bit), O1=m.Out(m.Bit))
         io += m.ClockIO()
         reg = m.Register(m.Bit)()
 
@@ -12,10 +12,13 @@ def test_inline_comb_basic():
         def logic():
             if io.invert:
                 reg.I @= ~reg.O
+                O1 = ~reg.O
             else:
                 reg.I @= reg.O
+                O1 = reg.O
 
-        io.O @= reg.O
+        io.O0 @= reg.O
+        io.O1 @= O1
 
     m.compile("build/test_inline_comb_basic", Main, inline=True)
     assert check_files_equal(__file__, f"build/test_inline_comb_basic.v",
