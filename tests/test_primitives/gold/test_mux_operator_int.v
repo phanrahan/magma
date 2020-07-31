fbe4ad7d70fef49b91cf5dd18c26cafa3628a12a
Lenny Truong <lenny@stanford.edu>
2020-07-31 12:41:01 -0700

    Add gold files

---
+++ tests/test_primitives/gold/test_mux_operator_int.v
@@ -0,0 +1,74 @@
+module coreir_mux #(
+    parameter width = 1
+) (
+    input [width-1:0] in0,
+    input [width-1:0] in1,
+    input sel,
+    output [width-1:0] out
+);
+  assign out = sel ? in1 : in0;
+endmodule
+
+module corebit_const #(
+    parameter value = 1
+) (
+    output out
+);
+  assign out = value;
+endmodule
+
+module commonlib_muxn__N2__width1 (
+    input [0:0] in_data_0,
+    input [0:0] in_data_1,
+    input [0:0] in_sel,
+    output [0:0] out
+);
+wire [0:0] _join_out;
+coreir_mux #(
+    .width(1)
+) _join (
+    .in0(in_data_0),
+    .in1(in_data_1),
+    .sel(in_sel[0]),
+    .out(_join_out)
+);
+assign out = _join_out;
+endmodule
+
+module Mux2xOutBit (
+    input I0,
+    input I1,
+    input S,
+    output O
+);
+wire [0:0] coreir_commonlib_mux2x1_inst0_out;
+commonlib_muxn__N2__width1 coreir_commonlib_mux2x1_inst0 (
+    .in_data_0(I0),
+    .in_data_1(I1),
+    .in_sel(S),
+    .out(coreir_commonlib_mux2x1_inst0_out)
+);
+assign O = coreir_commonlib_mux2x1_inst0_out[0];
+endmodule
+
+module test_mux_operator_int (
+    input I,
+    input S,
+    output O
+);
+wire Mux2xOutBit_inst0_O;
+wire bit_const_0_None_out;
+Mux2xOutBit Mux2xOutBit_inst0 (
+    .I0(bit_const_0_None_out),
+    .I1(I),
+    .S(S),
+    .O(Mux2xOutBit_inst0_O)
+);
+corebit_const #(
+    .value(1'b0)
+) bit_const_0_None (
+    .out(bit_const_0_None_out)
+);
+assign O = Mux2xOutBit_inst0_O;
+endmodule
+
