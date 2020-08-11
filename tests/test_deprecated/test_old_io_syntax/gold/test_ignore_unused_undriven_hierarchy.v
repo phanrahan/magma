0776b108e1dab2628fff4baead5f51ad16a0bac8
Lenny Truong <lenny@stanford.edu>
2020-08-11 11:48:50 -0700

    Update golds

--- tests/test_deprecated/test_old_io_syntax/gold/test_ignore_unused_undriven_hierarchy.v
+++ tests/test_deprecated/test_old_io_syntax/gold/test_ignore_unused_undriven_hierarchy.v
@@ -16,10 +16,8 @@ module Foo (
     output O0,
     output O1
 );
-wire corebit_term_inst0_in;
-assign corebit_term_inst0_in = I1;
 corebit_term corebit_term_inst0 (
-    .in(corebit_term_inst0_in)
+    .in(I1)
 );
 corebit_undriven corebit_undriven_inst0 (
     .out(O0)
@@ -33,27 +31,19 @@ module Main (
     output O0,
     output O1
 );
-wire Foo_inst0_I0;
-wire Foo_inst0_I1;
 wire Foo_inst0_O1;
-wire corebit_term_inst0_in;
-wire corebit_term_inst1_in;
 wire corebit_undriven_inst1_out;
-assign Foo_inst0_I0 = I0;
-assign Foo_inst0_I1 = corebit_undriven_inst1_out;
 Foo Foo_inst0 (
-    .I0(Foo_inst0_I0),
-    .I1(Foo_inst0_I1),
+    .I0(I0),
+    .I1(corebit_undriven_inst1_out),
     .O0(O0),
     .O1(Foo_inst0_O1)
 );
-assign corebit_term_inst0_in = I1;
 corebit_term corebit_term_inst0 (
-    .in(corebit_term_inst0_in)
+    .in(I1)
 );
-assign corebit_term_inst1_in = Foo_inst0_O1;
 corebit_term corebit_term_inst1 (
-    .in(corebit_term_inst1_in)
+    .in(Foo_inst0_O1)
 );
 corebit_undriven corebit_undriven_inst0 (
     .out(O1)
