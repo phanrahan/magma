b991b967650851e702b9a74a09177a6b33c6ce50
Leonard (Lenny) Truong <lenny@stanford.edu>
2020-09-14 17:15:30 -0700

    Update inline_verilog.py

--- magma/inline_verilog.py
+++ magma/inline_verilog.py
@@ -44,8 +44,7 @@ def _make_inline_value(cls, inline_value_map, value):
 def _make_temporary(defn, value, num, inline_wire_prefix, parent=None):
     with defn.open():
         # Insert a wire so it can't be inlined out
-        temp_name = inline_wire_prefix
-        temp_name += f"{num}"
+        temp_name = f"{inline_wire_prefix}{num}"
         temp = Wire(type(value))(name=temp_name)
         temp.I @= value
     if parent is not None:
