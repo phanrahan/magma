d697d1ca83ccf66e6726a373308745a4ce88fdd3
Lenny Truong <lenny@stanford.edu>
2021-02-16 17:24:45 -0800

    Revert "update name"
    This reverts commit 8d2c2d64c93cd154b616dfb97f29cbfa0d033c01.

--- magma/backend/coreir/coreir_runtime.py
+++ magma/backend/coreir/coreir_runtime.py
@@ -2,7 +2,7 @@ from coreir import Context
 
 
 _coreir_context = Context()
-_namespace_module_map = {}  # map from context to namespaces to modules
+_module_map = {}  # map from context to modules
 
 
 def coreir_context():
@@ -12,15 +12,15 @@ def coreir_context():
 
 def reset_coreir_context():
     global _coreir_context
-    global _namespace_module_map
+    global _module_map
     try:
-        del _namespace_module_map[_coreir_context]
+        del _module_map[_coreir_context]
     except KeyError:  # _coreir_context (singleton) not mapped
         pass
     _coreir_context.delete()
     _coreir_context = Context()
 
 
-def namespace_module_map():
-    global _namespace_module_map
-    return _namespace_module_map
+def module_map():
+    global _module_map
+    return _module_map
