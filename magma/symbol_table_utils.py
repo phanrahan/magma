f410f26b5a8cce315fcac8910e1917c86f6e1e72
Raj Setaluri <raj.setaluri@gmail.com>
2021-03-03 22:57:56 -0600

    Refactor python wrapper implementation

--- magma/symbol_table_utils.py
+++ magma/symbol_table_utils.py
@@ -1,19 +1,94 @@
 import dataclasses
-from typing import Sequence, Tuple
+from typing import Any, List, Mapping, Sequence, Tuple, Optional
 
 from magma.symbol_table import SymbolTableInterface, SymbolTable
 
 
-def _get_mapped_module_name(module_name, local, total):
-    try:
-        return local[module_name]
-    except KeyError:
-        pass
-    try:
-        return total[module_name]
-    except KeyError:
-        pass
-    return module_name
+@dataclasses.dataclass
+class _Object:
+    name: str
+    scope: int
+    root: Optional['_Object'] = None
+
+    def __post_init__(self):
+        self._renames: List[_Rename] = []
+
+    @property
+    def renames(self):
+        return self._renames.copy()
+
+    def root_or_self(self):
+        if self.root is not None:
+            return self.root
+        return self
+
+    def tail(self):
+        if not self._renames:
+            return self
+        if len(self._renames) == 1:
+            return self._renames[0].obj.tail()
+        raise ValueError(self)
+
+    def key(self):
+        return (self.scope, self.name)
+
+    def add_rename(self, rename: '_Rename'):
+        self._renames.append(rename)
+
+    def print_tree(self, print_self=True, indent=0):
+        if print_self:
+            print (" " * indent + repr(self))
+        for rename in self._renames:
+            rename.obj.print_tree(indent=indent + 1)
+
+
+@dataclasses.dataclass
+class _Rename:
+    obj: _Object
+    modifiers: Tuple[str] = ()
+
+
+@dataclasses.dataclass
+class _Module(_Object):
+    def __post_init__(self):
+        super().__post_init__()
+        self._instances: List['_Instance'] = {}
+        self._ports: List['_Port'] = {}
+
+    @property
+    def instances(self):
+        return self._instances.values()
+
+    @property
+    def ports(self):
+        return self._ports.values()
+
+    def get_rename(self):
+        if len(self._renames) != 1:
+            raise ValueError(self)
+        return self._renames[0]
+
+    def add_instance(self, instance: '_Instance'):
+        assert instance.name not in self._instances
+        self._instances[instance.name] = instance
+
+    def get_instance(self, name: str):
+        return self._instances[name]
+
+    def add_port(self, port: '_Port'):
+        assert port.name not in self._ports
+        self._ports[port.name] = port
+
+    def get_port(self, name: str):
+        return self._ports[name]
+
+    def remove_port(self, name: str):
+        self._ports.pop(name)
+
+
+@dataclasses.dataclass
+class _Instance(_Object):
+    pass
 
 
 @dataclasses.dataclass(eq=True, frozen=True)
@@ -32,115 +107,107 @@ class _PortWrapper:
         if not len(parts) > 0:
             raise ValueError(longname)
         if len(parts) == 1:
-            return _PortWrapper(parts[0], tuple())
+            return _PortWrapper(parts[0], ())
         return _PortWrapper(parts[0], tuple(parts[1:]))
+
+
+@dataclasses.dataclass
+class _Port(_Object):
+    def __post_init__(self):
+        super().__post_init__()
+        self._wrapper = _PortWrapper.make(self.name)
+
+    @property
+    def wrapper(self):
+        return self._wrapper
+
+    def tails(self):
+        if not self._renames:
+            return [(self, ())]
+        tails = []
+        for rename in self._renames:
+            sub_tails = rename.obj.tails()
+            for obj, modifiers in sub_tails:
+                tails.append((obj, rename.modifiers + modifiers))
+        return tails
 
 
 class _TableProcessor:
     def __init__(self):
-        self._first = True
-        self._module_names = {}
-        self._module_names_lookup = {}
-        self._instance_names = {}
-        self._instance_names_lookup = {}
-        self._port_names = {}
-        self._port_names_lookup = {}
+        self._modules = {}
+        self._index = 0
 
     def process_table(self, table: SymbolTableInterface):
         self._process_module_names(table.module_names())
-        self._process_instance_names(
-            table.instance_names(), table.module_names())
-        self._process_port_names(table.port_names(), table.module_names())
-        self._first = False
+        self._process_instance_names(table.instance_names())
+        self._process_port_names(table.port_names())
+        self._index += 1
 
     def update_master(self, master: 'MasterSymbolTable'):
-        for in_module_name, out_module_name in self._module_names.items():
-            SymbolTable.set_module_name(master, in_module_name, out_module_name)
-        for key, value in self._instance_names.items():
-            in_module_name, in_instance_name = key
-            _, out_instance_name = value
-            SymbolTable.set_instance_name(
-                master, in_module_name, in_instance_name, out_instance_name)
-        for key, value in self._port_names.items():
-            in_module_name, in_port = key
-            _, out_port = value
-            SymbolTable.set_port_name(
-                master, in_module_name, in_port.longname(), out_port.longname())
+        root_modules = list(
+            filter(lambda m: m.scope == 0, self._modules.values()))
+        for module in root_modules:
+            SymbolTable.set_module_name(master, module.name, module.tail().name)
+            for instance in module.instances:
+                SymbolTable.set_instance_name(
+                    master, module.name, instance.name, instance.tail().name)
+            for port in module.ports:
+                for tail, modifiers in port.tails():
+                    src_port = _PortWrapper(port.name, modifiers).longname()
+                    SymbolTable.set_port_name(
+                        master, module.name, src_port, tail.name)
 
     def _process_module_names(self, module_names):
         for in_module_name, out_module_name in module_names.items():
-            try:
-                update_key = self._module_names_lookup.pop(in_module_name)
-            except KeyError:
-                if in_module_name in self._module_names:
-                    err = Exception(
-                        f"Duplicate mapping of module name {in_module_name}")
-                    raise err from None
-                if not self._first:
-                    raise Exception(
-                        f"Unexpected module name {in_module_name}") from None
-                update_key = in_module_name
-            self._module_names[update_key] = out_module_name
-            self._module_names_lookup[out_module_name] = update_key
+            if self._index == 0:
+                src = _Module(in_module_name, self._index)
+                root = src
+                self._modules[src.key()] = src
+            else:
+                src = self._modules[(self._index, in_module_name)]
+                root = src.root
+            dst = _Module(out_module_name, self._index + 1, root=root)
+            src.add_rename(_Rename(dst))
+            self._modules[dst.key()] = dst
 
-    def _process_instance_names(self, instance_names, module_names):
+    def _process_instance_names(self, instance_names):
         for key, out_instance_name in instance_names.items():
-            in_module_name, _ = key
-            out_module_name = _get_mapped_module_name(
-                in_module_name, module_names, self._module_names)
-            try:
-                update_key = self._instance_names_lookup.pop(key)
-            except KeyError:
-                update_key = key
-            new_value = out_module_name, out_instance_name
-            self._instance_names[update_key] = new_value
-            self._instance_names_lookup[new_value] = update_key
+            in_module_name, in_instance_name = key
+            src_module = self._modules[(self._index, in_module_name)]
+            dst_module = src_module.get_rename().obj
+            if self._index == 0:
+                src_instance = _Instance(in_instance_name, self._index)
+                root_instance = src_instance
+                src_module.add_instance(src_instance)
+            else:
+                src_instance = src_module.get_instance(in_instance_name)
+                root_instance = src_instance.root
+            dst_instance = _Instance(
+                out_instance_name, self._index + 1, root=root_instance)
+            dst_module.add_instance(dst_instance)
+            src_instance.add_rename(_Rename(dst_instance))
 
-    def _process_port_names(self, port_names, module_names):
-        to_del = []
+    def _process_port_names(self, port_names):
         for key, out_port_name in port_names.items():
             in_module_name, in_port_name = key
-            out_module_name = _get_mapped_module_name(
-                in_module_name, module_names, self._module_names)
-            in_port = _PortWrapper.make(in_port_name)
-            out_port = _PortWrapper.make(out_port_name)
-
-            lookup_type = None
-            try:
-                lookup = self._port_names_lookup[in_module_name, in_port]
-                lookup_type = 0
-            except KeyError:
-                try:
-                    lookup = self._port_names_lookup[
-                        in_module_name, _PortWrapper(in_port.base, ())]
-                    lookup_type = 1
-                except KeyError:
-                    lookup_type = 2
-            if lookup_type == 0:
-                key = lookup
-                value = out_module_name, out_port
-            elif lookup_type == 1:
-                to_del.append(
-                    (lookup, (in_module_name, _PortWrapper(in_port.base, ()))))
-                key = lookup[0], _PortWrapper(lookup[1].base, in_port.fields)
-                value = out_module_name, out_port
-            elif lookup_type == 2:
-                key = in_module_name, in_port
-                value = out_module_name, out_port
+            src_module = self._modules[(self._index, in_module_name)]
+            dst_module = src_module.get_rename().obj
+            modifiers = ()
+            if self._index == 0:
+                src_port = _Port(in_port_name, self._index)
+                root_port = src_port
+                src_module.add_port(src_port)
             else:
-                raise NotImplementedError(lookup_type)
-            self._port_names[key] = value
-            self._port_names_lookup[value] = key
-
-        for forward, backward in to_del:
-            try:
-                del self._port_names[forward]
-            except KeyError:
-                pass
-            try:
-                del self._port_names_lookup[backward]
-            except KeyError:
-                pass
+                try:
+                    src_port = src_module.get_port(in_port_name)
+                except KeyError:
+                    dummy_port = _Port(in_port_name, self._index)
+                    src_port = src_module.get_port(dummy_port.wrapper.base)
+                    modifiers = dummy_port.wrapper.fields
+                root_port = src_port.root
+            dst_port = _Port(out_port_name, self._index + 1, root=root_port)
+            dst_module.add_port(dst_port)
+            src_port.add_rename(_Rename(dst_port, modifiers))
 
 
 class MasterSymbolTable(SymbolTable):
--- tests/test_symbol_table_utils.py
+++ tests/test_symbol_table_utils.py
@@ -4,45 +4,6 @@ from magma.symbol_table import SymbolTable
 from magma.symbol_table_utils import make_master_symbol_table, MasterSymbolTable
 
 
-def _make_master():
-    #return make_master_symbol_table(("st0.json", "st1.json"))
-    return MasterSymbolTable([
-        # SymbolTable.from_dict({
-        #     "module_names": {"Top": "CTop"},
-        #     "instance_names": {("Top", "inst"): "c_inst"},
-        #     "port_names": {},
-        # }),
-        # SymbolTable.from_dict({
-        #     "module_names": {"CTop": "VTop"},
-        #     "instance_names": {("CTop", "c_inst"): "v_inst"},
-        #     "port_names": {},
-        # }),
-        # SymbolTable.from_dict({
-        #     "module_names": {"VTop": "OTop"},
-        #     "instance_names": {("VTop", "v_inst"): "o_inst"},
-        #     "port_names": {},
-        # }),
-        SymbolTable.from_dict({
-            "module_names": {"x": "y", "a": "b"},
-            "instance_names": {("x", "inst0"): "inst_0",
-                               ("a", "foo"): "foo_out"},
-            "port_names": {},
-        }),
-        SymbolTable.from_dict({
-            "module_names": {#"c": "d",
-                             "y": "z"},
-            "instance_names": {("y", "inst_0"): "inst_2"},
-            "port_names": {},
-        }),
-        SymbolTable.from_dict({
-            "module_names": {"b": "f"},
-            "instance_names": {("b", "foo_out"): "foo_OUT",
-                               ("z", "inst_2"): "inst_100"},
-            "port_names": {},
-        }),
-    ])
-
-
 def test_one():
     master = MasterSymbolTable([
         SymbolTable.from_dict({
@@ -87,14 +48,15 @@ def test_two():
             "port_names": {},
         }),
         SymbolTable.from_dict({
-            "module_names": {"y": "z"},
-            "instance_names": {("y", "inst_0"): "inst_2"},
+            "module_names": {"y": "z", "b": "b"},
+            "instance_names": {("y", "inst_0"): "inst_2",
+                               ("b", "foo_out"): "foo_out"},
             "port_names": {},
         }),
         SymbolTable.from_dict({
-            "module_names": {"b": "f"},
-            "instance_names": {("b", "foo_out"): "foo_OUT",
-                               ("z", "inst_2"): "inst_100"},
+            "module_names": {"z": "z", "b": "f"},
+            "instance_names": {("z", "inst_2"): "inst_100",
+                               ("b", "foo_out"): "foo_OUT"},
             "port_names": {},
         }),
     ])
