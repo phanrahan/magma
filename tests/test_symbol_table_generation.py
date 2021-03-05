d3be4af27fffd52f159446e5c9dad0441e4ea3e7
Raj Setaluri <raj.setaluri@gmail.com>
2021-03-04 22:37:32 -0600

    Update generation tests

--- tests/test_symbol_table_generation.py
+++ tests/test_symbol_table_generation.py
@@ -61,12 +61,10 @@ def test_symbol_table_bundle_flattening():
             O=m.Out(MyBundle),
             I2=m.In(m.Bit)
         )
-
-        io.O @= MyBundle(0, 0)
+        io.O.undriven()
 
     symbol_table = _compile("build/WithTuple", WithTuple)
 
-    print (symbol_table.as_json(indent=4))
     # NOTE: These two conditions would only hold in the post-CoreIR
     # mapping. Since we only test the magma-to-CoreIR symbol table for now,
     # these will not hold.
