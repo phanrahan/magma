5101bbb0a2d997d9a6bc229592f21ba7bd97a288
Lenny Truong <lenny@stanford.edu>
2021-06-01 13:40:25 -0700

    Fix chain bug

--- magma/wire_clock.py
+++ magma/wire_clock.py
@@ -17,7 +17,7 @@ def get_clocks(port, clocktype):
         yield port
     if isinstance(port, Tuple):
         yield from itertools.chain(
-            get_clocks(elem, clocktype) for elem in port)
+            *(get_clocks(elem, clocktype) for elem in port))
     if isinstance(port, Array):
         first_clks = get_clocks(port[0], clocktype)
         try:
@@ -27,7 +27,7 @@ def get_clocks(port, clocktype):
             return None
         yield from itertools.chain(
             [first_clk], first_clks,
-            (get_clocks(elem, clocktype) for elem in port[1:]))
+            *(get_clocks(elem, clocktype) for elem in port[1:]))
 
 
 def get_clocks_from_defn(defn, clocktype):
