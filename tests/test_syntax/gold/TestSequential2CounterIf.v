1a1762a059ce9fc3c963d2a6cbb484f652961351
Raj Setaluri <raj.setaluri@gmail.com>
2021-07-21 14:46:13 -0500

    Update golds

--- tests/test_syntax/gold/TestSequential2CounterIf.v
+++ tests/test_syntax/gold/TestSequential2CounterIf.v
@@ -32,7 +32,7 @@ coreir_reg #(
 );
 endmodule
 
-module Mux2xSInt16 (
+module Mux2x_SequentialRegisterWrapperSInt16 (
     input [15:0] I0,
     input [15:0] I1,
     input S,
@@ -57,7 +57,7 @@ module Test2 (
 );
 wire [15:0] Register_inst0_O;
 wire [15:0] magma_SInt_16_add_inst0_out;
-Mux2xSInt16 Mux2xSInt16_inst0 (
+Mux2x_SequentialRegisterWrapperSInt16 Mux2x_SequentialRegisterWrapperSInt16_inst0 (
     .I0(Register_inst0_O),
     .I1(magma_SInt_16_add_inst0_out),
     .S(sel),
--- tests/test_syntax/gold/TestSequential2IteBits.v
+++ tests/test_syntax/gold/TestSequential2IteBits.v
@@ -32,6 +32,24 @@ coreir_reg #(
 );
 endmodule
 
+module Mux2x_SequentialRegisterWrapperBits8 (
+    input [7:0] I0,
+    input [7:0] I1,
+    input S,
+    output [7:0] O
+);
+reg [7:0] coreir_commonlib_mux2x8_inst0_out;
+always @(*) begin
+if (S == 0) begin
+    coreir_commonlib_mux2x8_inst0_out = I0;
+end else begin
+    coreir_commonlib_mux2x8_inst0_out = I1;
+end
+end
+
+assign O = coreir_commonlib_mux2x8_inst0_out;
+endmodule
+
 module Mux2xTuplea__SequentialRegisterWrapperBits8 (
     input [7:0] I0_a,
     input [7:0] I1_a,
@@ -50,45 +68,27 @@ end
 assign O_a = coreir_commonlib_mux2x8_inst0_out;
 endmodule
 
-module Mux2xBits8 (
-    input [7:0] I0,
-    input [7:0] I1,
-    input S,
-    output [7:0] O
-);
-reg [7:0] coreir_commonlib_mux2x8_inst0_out;
-always @(*) begin
-if (S == 0) begin
-    coreir_commonlib_mux2x8_inst0_out = I0;
-end else begin
-    coreir_commonlib_mux2x8_inst0_out = I1;
-end
-end
-
-assign O = coreir_commonlib_mux2x8_inst0_out;
-endmodule
-
 module Test (
     input CLK,
     output [7:0] O_a,
     input sel
 );
-wire [7:0] Mux2xBits8_inst0_O;
+wire [7:0] Mux2x_SequentialRegisterWrapperBits8_inst0_O;
 wire [7:0] Register_inst0_O;
-Mux2xBits8 Mux2xBits8_inst0 (
-    .I0(Register_inst0_O),
-    .I1(Register_inst0_O),
-    .S(sel),
-    .O(Mux2xBits8_inst0_O)
-);
 Mux2xTuplea__SequentialRegisterWrapperBits8 Mux2xTuplea__SequentialRegisterWrapperBits8_inst0 (
     .I0_a(Register_inst0_O),
     .I1_a(Register_inst0_O),
     .O_a(O_a),
     .S(sel)
 );
+Mux2x_SequentialRegisterWrapperBits8 Mux2x_SequentialRegisterWrapperBits8_inst0 (
+    .I0(Register_inst0_O),
+    .I1(Register_inst0_O),
+    .S(sel),
+    .O(Mux2x_SequentialRegisterWrapperBits8_inst0_O)
+);
 Register Register_inst0 (
-    .I(Mux2xBits8_inst0_O),
+    .I(Mux2x_SequentialRegisterWrapperBits8_inst0_O),
     .O(Register_inst0_O),
     .CLK(CLK)
 );
