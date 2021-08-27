06d86ee3bde2d3641dc31029b11daae1f436caf7
Lenny Truong <lenny@stanford.edu>
2021-08-27 12:25:52 -0700

    Refactor into function

--- magma/wire.py
+++ magma/wire.py
@@ -17,43 +17,10 @@ _logger = root_logger()
 _CONSTANTS = (IntegerTypes, BitVector, Bit)
 
 
-@debug_wire
-def wire(o, i, debug_info=None):
-    o = magma_value(o)
-    i = magma_value(i)
-
-    # Circular import
-    from .conversions import tuple_
-    if isinstance(o, tuple):
-        o = tuple_(o)
-    if isinstance(i, tuple):
-        i = tuple_(i)
-
-    # Wire(o, Circuit).
-    if hasattr(i, 'interface'):
-        i.wire(o, debug_info)
-        return
-
-    # Replace output Circuit with its output (should only be 1 output).
-    if hasattr(o, 'interface'):
-        outputs = o.interface.outputs()
-        if len(outputs) != 1:
-            _logger.error(
-                WiringLog(f"Can only wire circuits with one output; circuit "
-                          f"`{{}}` has outputs "
-                          f"{[output.name.name for output in outputs]}", o),
-                debug_info=debug_info
-            )
-            return
-        o = outputs[0]
-
-    # If o is an input.
-    if not isinstance(o, _CONSTANTS) and o.is_input():
-        # If i is not an input.
-        if isinstance(i, _CONSTANTS) or not i.is_input():
-            # Flip i and o.
-            i, o = o, i
-
+def _check_wiring_context(i, o, debug_info):
+    """
+    Ensures that i and o come from the same definition context
+    """
     if isinstance(i, _CONSTANTS) or isinstance(o, _CONSTANTS):
         pass
     elif (isinstance(i.name, PortViewRef) or
@@ -98,6 +65,46 @@ def wire(o, i, debug_info=None):
         pass
     else:
         raise Exception(f"Cannot wire together {o} and {i}")
+
+
+@debug_wire
+def wire(o, i, debug_info=None):
+    o = magma_value(o)
+    i = magma_value(i)
+
+    # Circular import
+    from .conversions import tuple_
+    if isinstance(o, tuple):
+        o = tuple_(o)
+    if isinstance(i, tuple):
+        i = tuple_(i)
+
+    # Wire(o, Circuit).
+    if hasattr(i, 'interface'):
+        i.wire(o, debug_info)
+        return
+
+    # Replace output Circuit with its output (should only be 1 output).
+    if hasattr(o, 'interface'):
+        outputs = o.interface.outputs()
+        if len(outputs) != 1:
+            _logger.error(
+                WiringLog(f"Can only wire circuits with one output; circuit "
+                          f"`{{}}` has outputs "
+                          f"{[output.name.name for output in outputs]}", o),
+                debug_info=debug_info
+            )
+            return
+        o = outputs[0]
+
+    # If o is an input.
+    if not isinstance(o, _CONSTANTS) and o.is_input():
+        # If i is not an input.
+        if isinstance(i, _CONSTANTS) or not i.is_input():
+            # Flip i and o.
+            i, o = o, i
+
+    _check_wiring_context(i, o, debug_info)
 
     i_T, o_T = type(i), type(o)
     if not i_T.is_wireable(o_T):
