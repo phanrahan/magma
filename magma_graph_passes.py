7ef12fda0c21dbfbc10c195762019426116c20a6
Raj Setaluri <raj.setaluri@gmail.com>
2021-09-15 16:44:25 -0700

    cleanup

--- magma_graph_passes.py
+++
@@ -1,7 +0,0 @@
-from graph_base import Graph, topological_sort
-from graph_utils import NodeTransformer
-
-
-class InsertPortNodeTransformer(NodeTransformer):
-    def visit_generic(self, node):
-        
