66089187d3f0ba154183a8849e461e932fe61982
Raj Setaluri <raj.setaluri@gmail.com>
2021-09-15 20:58:02 -0700

    cleanup

--- mlir_passes.py
+++ mlir_passes.py
@@ -4,7 +4,7 @@ from typing import Any, Iterable, List, Optional
 import magma as m
 
 from graph_lib import Graph, Node, topological_sort
-from pass_base import NodeVisitor, NodeTransformer
+from graph_visitor import NodeVisitor, NodeTransformer
 from mlir_wrapper import MlirContext, MlirValue, MlirType, MlirIntegerType
 from passes import Net
 
--- passes.py
+++ passes.py
@@ -4,7 +4,7 @@ from typing import Tuple
 import magma as m
 
 from graph_lib import Graph, Node, topological_sort
-from pass_base import NodeTransformer
+from graph_visitor import NodeTransformer
 
 
 class SplitPortEdgesTranformer(NodeTransformer):
