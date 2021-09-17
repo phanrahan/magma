cc1dea14d215d69ea23bd0c525687c177114198e
Raj Setaluri <raj.setaluri@gmail.com>
2021-09-17 09:56:54 -0700

    Fix test.py

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ def test_compile_to_mlir(ckt):
     filename = f"{ckt.name}.mlir"
     with open(filename, "w") as f:
         compile_to_mlir(ckt, f)
-    assert check_files_equal(__file__, "comb.mlir", "golds/comb.mlir")
+    assert check_files_equal(__file__, filename, f"golds/{filename}")
