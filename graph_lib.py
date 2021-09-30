d85a0cbeb2ac6a202d8b1a95be3c80878ee63344
Raj Setaluri <raj.setaluri@gmail.com>
2021-09-30 15:44:55 -0700

    Add routine to find simple cycles in graph
    Also includes a change to make anonymous mlir values be consistently,
    predictably, and distinctly named.

--- graph_lib.py
+++ graph_lib.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from typing import Any, Callable, Iterable, Mapping, Tuple, Union
+from typing import Any, Callable, Iterable, List, Mapping, Tuple, Union
 
 
 Graph = nx.MultiDiGraph
@@ -18,3 +18,29 @@ def reverse_topological_sort(g: Graph) -> Iterable[Node]:
 
 def write_to_dot(g: Graph, filename: str):
     nx.drawing.nx_pydot.write_dot(g, filename)
+
+
+def _sort_cycle(
+        cycle: List[Node], key: Callable[[Node], Any] = str) -> List[Node]:
+    """
+    Reorders @cycle by putting the lexicographically first (according to @key)
+    element at index 0, and the remainder of the cycle in the remainder of the
+    list.
+    """
+    size = len(cycle)
+    if size == 0:
+        return []
+    min_index, min_value = 0, key(cycle[0])
+    for index in range(1, size):
+        value = key(cycle[index])
+        if value == min_value:
+            raise RuntimeError("Expected distinct keys")
+        if value < min_value:
+            min_index, min_value = index, value
+    order = ((min_index + j) % size for j in range(size))
+    return [cycle[i] for i in order]
+
+
+def simple_cycles(g: Graph) -> Iterable[List[Node]]:
+    cycles = nx.algorithms.cycles.simple_cycles(g)
+    return map(_sort_cycle, cycles)
--- mlir_value.py
+++ mlir_value.py
@@ -4,13 +4,30 @@ from typing import Union
 from mlir_type import MlirType
 
 
-class _Anonymous: pass
+_anonymous_index = 0
+
+
+# NOTE(rsetaluri): We need instances of this class to have predictable,
+# consistent, and distinct string representations. We use a global counter to
+# achieve this. These properties are needed downstream; in particular in cases
+# where hashes are used to distinguish graph nodes, and to have str() be a
+# consistently ordered key for nodes (see comment in cycle breaking pass).
+@dataclasses.dataclass(frozen=True)
+class _Anonymous:
+    key: int
+
+
+def _make_anonymous() -> _Anonymous:
+    global _anonymous_index
+    key = _anonymous_index
+    _anonymous_index += 1
+    return _Anonymous(key)
 
 
 @dataclasses.dataclass(frozen=True)
 class MlirValue:
     type: MlirType
-    name: Union[str, _Anonymous] = dataclasses.field(default_factory=_Anonymous)
+    name: Union[str, _Anonymous] = dataclasses.field(default_factory=_make_anonymous)
 
 
 def mlir_value_is_anonymous(value: MlirValue) -> bool:
