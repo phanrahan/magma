8275b795ba48ceebdf6fb45754da4bca1855b88a
Lenny Truong <lenny@stanford.edu>
2021-10-04 14:44:55 -0700

    Remove TODO

--- magma/conversions.py
+++ magma/conversions.py
@@ -229,7 +229,6 @@ class Concat(Generator2):
         n1, n2 = len(t1), len(t2)
         child_T = t1.T
         if child_T is not t2.T:
-            # TODO(leonardt/array2): Support subclasses?
             raise TypeError(f"Cannot concat {t1} and {t2}")
         self.io = IO(I0=In(t1), I1=In(t2), O=Out(Array2[n1 + n2, child_T]))
         self.coreir_genargs = {"t0": t1, "t1": t2}
