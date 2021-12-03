531ea17b7f7b1551f3023ae0e713dd3a2dbcb920
Lenny Truong <lenny@stanford.edu>
2021-12-03 14:55:13 -0800

    Fixup clock logic

--- magma/clock.py
+++ magma/clock.py
@@ -29,6 +29,12 @@ class _ClockType(Digital):
             return False
         return True
 
+    def __bool__(self) -> bool:
+        return Bit.__bool__(self)
+
+    def __int__(self) -> int:
+        return Bit.__int__(self)
+
 
 class Clock(_ClockType, metaclass=DigitalMeta):
     pass
