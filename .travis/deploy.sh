d3e8c9500ec3b167df8ed067e0c0305781c94ab6
Lenny Truong <leonardtruong@fb.com>
2021-12-10 11:34:54 -0800

    Remove old auth logic

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,15 +3,6 @@
 export PATH=$TRAVIS_BUILD_DIR/miniconda/bin:$PATH
 source activate test-env
 
-echo [distutils]                                  > ~/.pypirc
-echo index-servers =                             >> ~/.pypirc
-echo "  pypi"                                    >> ~/.pypirc
-echo                                             >> ~/.pypirc
-echo [pypi]                                      >> ~/.pypirc
-echo repository=https://upload.pypi.org/legacy/  >> ~/.pypirc
-echo username=leonardt                           >> ~/.pypirc
-echo password=$PYPI_PASSWORD                     >> ~/.pypirc
-
 pip install twine
 python setup.py sdist build
 
