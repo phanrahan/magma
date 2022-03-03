59b6fc44c560d92d43c54f6a29206dbd2d27a690
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-03 14:06:30 -0800

    [1006] Update conversion tests

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,34 +3,46 @@ import magma as m
 import hwtypes as ht
 
 
-def test_concat():
-    class Foo(m.Circuit):
-        b0 = m.bit(1)
-        b1 = m.bits(1, 1)
-        bits0 = m.concat(b0, b1)
-        bits1 = m.concat(m.bits(b0), b1)
-        assert type(bits0) == m.Out(m.Bits[2])
+def _assert_identical(x, y):
+    assert all(xi is yi for xi, yi in zip(x, y))
+
+
+def test_concat_basic():
+    x = m.Bit(1)
+    y = m.Bits[1](1)
+    o0 = m.concat(x, y)
+    o1 = m.concat(m.bits(x), y)
+    _assert_identical(o0, o1)
+    _assert_identical(o0, [x, y[0]])
 
 
 def test_concat_bit():
-    class Foo(m.Circuit):
-        x = m.concat(ht.Bit(True), True, m.bits(1))
-        print([y.const() for y in x])
-        assert int(m.bits(x)) == 7
+    x = ht.Bit(True)
+    y = True
+    z = m.Bits[1](1)
+    o = m.concat(x, y, z)
+    assert o.const()
+    _assert_identical(o, [m.Bit.VCC] * 3)
 
 
 def test_ext():
-    class Foo(m.Circuit):
-        x0 = m.sint(1, 1)
-        x1 = m.zext_by(x0, 2)
-        x2 = m.zext_to(x0, 3)
-        assert repr(x1) == "SInt[3](1)"
-        assert repr(x1) == repr(x2)
+    x0 = m.SInt[1](1)
 
-        x3 = m.sext_by(x0, 2)
-        x4 = m.sext_to(x0, 3)
-        assert repr(x3) == "SInt[3](-1)"
-        assert repr(x3) == repr(x4)
+    x1 = m.zext_by(x0, 2)
+    x2 = m.zext_to(x0, 3)
+    assert isinstance(x1, m.SInt)
+    assert isinstance(x2, m.SInt)
+    assert x1.const()
+    _assert_identical(x1, x2)
+    _assert_identical(x1, [m.Bit.VCC, m.Bit.GND, m.Bit.GND])
+
+    x3 = m.sext_by(x0, 2)
+    x4 = m.sext_to(x0, 3)
+    assert isinstance(x3, m.SInt)
+    assert isinstance(x4, m.SInt)
+    assert x3.const()
+    _assert_identical(x3, x4)
+    _assert_identical(x3, [m.Bit.VCC] * 3)
 
 
 def test_concat_type_error():
@@ -40,11 +52,9 @@ def test_concat_type_error():
 
 @pytest.mark.parametrize('op', [m.uint, m.sint])
 def test_convert_extend(op):
-    class Foo(m.Circuit):
-        io = m.IO(I=m.In(m.Bits[5]))
-        value = op(io.I, 32)
-        assert len(value) == 32
-        if op is m.sint:
-            # Check sext logic
-            for x in value[5:]:
-                assert x is io.I[4]
+    x = m.Bits[5]()
+    o = op(x, 32)
+    assert len(o) == 32
+    if op is m.sint:  # check sext logic
+        hi = o[5:]
+        _assert_identical(hi, [x[4]] * len(hi))
