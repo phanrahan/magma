c73f4ae95ebf2c133a3a9964fea31d6cb3272419
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-03 11:44:20 -0800

    [1007] Deprecate magma.math functions

--- magma/math.py
+++ magma/math.py
@@ -1,13 +1,18 @@
 import math
 
+from magma.common import deprecated
 
+
+@deprecated(msg="Use bitutils.clog2 instead")
 def log2_ceil(x: int) -> int:
     return math.ceil(math.log(x, 2))
 
 
+@deprecated(msg="Use bitutils.flog2 instead")
 def log2_floor(x: int) -> int:
     return math.floor(math.log(x, 2))
 
 
+@deprecated(msg="Use bitutils.is_pow2 instead")
 def is_pow2(x: int) -> bool:
     return log2_ceil(x) == log2_floor(x)
