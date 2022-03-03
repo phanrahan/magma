e80bf35d30ec5ac715815499a392f17573665c89
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-03 09:02:56 -0800

    Update introspection test

--- tests/test_wire/test_introspection.py
+++ tests/test_wire/test_introspection.py
@@ -1,12 +1,12 @@
-import magma as m
 import pytest
 
+import magma as m
 
-@pytest.mark.parametrize("T", (m.Bits,))
-def test_value_arr_type(T):
-    class Foo(m.Circuit):
-        x = m.Out(T[5])(name="x")
-        y = m.In(T[5])(name="y")
-        for i in range(5):
-            y[i] @= x[4-i]
-        assert isinstance(y.value(), T)
+
+def test_value_bits():
+    T = m.Bits[5]
+    x = m.Out(T)(name="x")
+    y = m.In(T)(name="y")
+    for i in range(T.N):
+        y[i] @= x[T.N - i - 1]
+    assert isinstance(y.value(), T)
