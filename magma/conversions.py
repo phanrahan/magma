c61a2eeea8b6e4c8f009306f5dfca6b8b148cd61
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-03 13:42:44 -0800

    [1006] Implement Concatter.consume() pattern for concat

--- magma/conversions.py
+++ magma/conversions.py
@@ -17,7 +17,7 @@ from .bitutils import int2seq
 import hwtypes as ht
 from magma.array import Array
 from magma.circuit import coreir_port_mapping
-from magma.common import is_int
+from magma.common import is_int, Finalizable
 from magma.generator import Generator2
 from magma.interface import IO
 
@@ -34,6 +34,43 @@ __all__ += ['concat', 'repeat']
 __all__ += ['sext', 'zext', 'sext_to', 'sext_by', 'zext_to', 'zext_by']
 __all__ += ['replace']
 __all__ += ['as_bits', 'from_bits']
+
+
+class _Concatter(Finalizable):
+    def __init__(self):
+        self._ts = []
+        self._type = None
+
+    def _consume_impl(self, value):
+        if isinstance(value, ht.BitVector):
+            return value.bits(), Bits
+        if isinstance(magma_value(value), Bit):
+            return [value], Bits
+        if isinstance(value, (bool, ht.Bit)):
+            return [Bit(value)], Bits
+        if isinstance(value, SInt):
+            t = SInt if self._type is None else Bits
+            return value.ts, t
+        if isinstance(value, UInt):
+            t = UInt if self._type is None else Bits
+            return value.ts, t
+        if isinstance(value, Bits):
+            return value.ts, Bits
+        if isinstance(value, Array) and isinstance(type(value).T, DigitalMeta):
+            return value.ts, Bits
+        raise TypeError(
+            "concat expects values of type Array, BitVector, Bit, or "
+            f"bool, not {value} with type {type(value)}"
+        )
+
+    def consume(self, value):
+        new_ts, t = self._consume_impl(value)
+        self._ts.extend(new_ts)
+        self._type = t
+
+    def finalize(self):
+        return convertbits(self._ts, None, self._type, checkbit=True)
+
 
 def can_convert_to_bit(value):
     return isinstance(magma_value(value), (Digital, Array, Tuple, IntegerTypes))
@@ -205,23 +242,11 @@ def bfloat(value, n=None):
     return convertbits(value, n, BFloat, True)
 
 
-def concat(*arrays):
-    ts = []
-    for a in arrays:
-        if isinstance(a, ht.BitVector):
-            ts.extend(a.bits())
-        elif isinstance(magma_value(a), Bit):
-            ts.extend([a])
-        elif isinstance(a, (bool, ht.Bit)):
-            ts.extend([Bit(a)])
-        else:
-            if not isinstance(a, Array):
-                raise TypeError(
-                    "concat expects values of type Array, BitVector, Bit, or "
-                    f"bool, not {a} with type {type(a)}"
-                )
-            ts.extend(a.ts)
-    return array(ts)
+def concat(*args):
+    concatter = _Concatter()
+    for arg in args:
+        concatter.consume(arg)
+    return concatter.finalize()
 
 
 def repeat(value, n):
