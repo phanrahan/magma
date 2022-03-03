65b69a268f728d518d42c275f9b55918546e8049
Lenny Truong <leonardtruong@fb.com>
2022-03-03 14:21:12 -0800

    io.flip() -> interface.flip()

--- magma/interface.py
+++ magma/interface.py
@@ -263,6 +263,14 @@ class _InterfaceBase(Type):
         return any(port.is_clock()
                    for port in self.ports.values())
 
+    def flip(self):
+        return IO(**{
+            # Note: Since interface types are definition view, they are already
+            # "flipped"
+            name: type(port)
+            for name, port in self.ports.items()
+        })
+
 
 class Interface(_InterfaceBase):
     """Interface class"""
@@ -493,12 +501,6 @@ class IO(IOInterface):
         if key in self._ports:
             return self._ports[key]
         return super().__getattribute__(key)
-
-    def flip(self):
-        return IO(**{
-            name: T.flip()
-            for name, T in zip(self._decl[::2], self._decl[1::2])
-        })
 
 
 
--- tests/test_interface/test_interface_flip.py
+++ tests/test_interface/test_interface_flip.py
@@ -1,12 +1,12 @@
 import magma as m
 
 
-def test_io_flip():
+def test_interface_flip():
     class Foo(m.Circuit):
         io = m.IO(I=m.In(m.Bit), O=m.Out(m.Bit))
 
 
     class Bar(m.Circuit):
-        io = m.Flip(Foo.io)
+        io = m.Flip(Foo.interface)
         assert io.I.is_input()
         assert io.O.is_output()
