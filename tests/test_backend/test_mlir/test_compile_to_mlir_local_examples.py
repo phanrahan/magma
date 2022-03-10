09c8731806b7180cc8ff8478f830d3810ea501b2
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-09 22:09:16 -0800

    Add simple_coreir_common_lib_mux_n_wrapper flatten tuples test

--- tests/test_backend/test_mlir/test_compile_to_mlir_local_examples.py
+++ tests/test_backend/test_mlir/test_compile_to_mlir_local_examples.py
@@ -1,7 +1,19 @@
 import pytest
 
+import magma as m
+from magma.primitives.mux import CoreIRCommonLibMuxN
+
 from examples import simple_aggregates_product, aggregate_mux_wrapper
 from test_utils import get_local_examples, run_test_compile_to_mlir
+
+
+class _simple_coreir_common_lib_mux_n_wrapper(m.Circuit):
+    name = "simple_coreir_common_lib_mux_n_wrapper"
+    T = m.Product.from_fields(
+        "anon", dict(data=m.Array[8, m.Bits[6]], sel=m.Bits[3])
+    )
+    io = m.IO(I=m.In(T), O=m.Out(m.Bits[6]))
+    io.O @= CoreIRCommonLibMuxN(8, 6)()(io.I)
 
 
 @pytest.mark.parametrize("ckt", get_local_examples())
@@ -14,6 +26,7 @@ def test_compile_to_mlir(ckt):
     [
         simple_aggregates_product,
         aggregate_mux_wrapper,
+        _simple_coreir_common_lib_mux_n_wrapper,
     ]
 )
 def test_compile_to_mlir_flatten_all_tuples(ckt):
