ba19cf262e984f169155bf5d9c081e3224f86489
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-14 14:04:45 -0700

    Run passes before mlir compilation

--- magma/backend/mlir/mlir_compiler.py
+++ magma/backend/mlir/mlir_compiler.py
@@ -1,9 +1,13 @@
 import io
 from typing import Dict
 
+from magma.backend.coreir.insert_coreir_wires import insert_coreir_wires
+from magma.backend.coreir.insert_wrap_casts import insert_wrap_casts
 from magma.backend.mlir.compile_to_mlir import compile_to_mlir
 from magma.circuit import DefineCircuitKind
 from magma.compiler import Compiler
+from magma.passes.clock import wire_clocks
+from magma.passes.find_errors import find_errors_pass
 
 
 class MlirCompiler(Compiler):
@@ -13,7 +17,14 @@ class MlirCompiler(Compiler):
     def suffix(self):
         return "mlir"
 
+    def _run_passes(self):
+        insert_coreir_wires(self.main)
+        insert_wrap_casts(self.main)
+        wire_clocks(self.main)
+        find_errors_pass(self.main)
+
     def generate_code(self):
+        self._run_passes()
         sout = io.StringIO()
         compile_to_mlir(self.main, sout=sout)
         return sout.getvalue()
