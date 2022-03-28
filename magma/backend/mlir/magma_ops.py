025a207c8f26641b65ff400304a913563b522da2
Raj Setaluri <raj.setaluri@gmail.com>
2022-03-28 15:10:08 -0700

    cleanup

--- magma/backend/mlir/magma_ops.py
+++ magma/backend/mlir/magma_ops.py
@@ -7,9 +7,8 @@ from magma.bits import BitsMeta
 from magma.digital import DigitalMeta
 from magma.generator import Generator2
 from magma.interface import IO
-from magma.t import Kind, In, Out
+from magma.t import In, Out
 from magma.tuple import TupleMeta, ProductMeta
-from magma.view import PortView, InstView
 
 
 def _get_tuple_field_type(T: TupleMeta, index: Union[int, str]):
@@ -91,12 +90,4 @@ def MagmaBitsConstantOp(T: BitsMeta, value: int):
     name = f"magma_bits_constant_op_{value_or_type_to_string(T)}"
     ports = dict(O=Out(T))
     attrs = dict(value=value)
-    return InstanceWrapper(name, ports, attrs)
-
-
-def MagmaXMROp(T: Kind, xmr: PortView, parent: InstView):
-    T = T.undirected_t
-    name = f"magma_xmr_op_{value_or_type_to_string(T)}"
-    ports = dict(O=Out(T))
-    attrs = dict(T=T, xmr=xmr, parent=parent)
     return InstanceWrapper(name, ports, attrs)
