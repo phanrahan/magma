a01f6e3aa63fbdcb2b84622fac986083650df0a2
Lenny Truong <leonardtruong@fb.com>
2022-04-21 16:24:25 -0700

    Add preliminary bench

---
+++ benchmarks/debug.py
@@ -0,0 +1,42 @@
+import pygal
+import timeit
+import magma as m
+
+
+def gen_circuit(debug_mode, n=8):
+    class Foo(m.Generator2):
+        if debug_mode == 2:
+            @m.debug
+            def __init__(self, n):
+                self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bit))
+                x = m.Bits[n]()
+                x @= ~self.io.I
+                self.io.O @= x.reduce_xor()
+        elif debug_mode == 1:
+            def __init__(self, n):
+                m.config.set_debug_mode(True)
+                self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bit))
+                x = m.Bits[n]()
+                x @= ~self.io.I
+                self.io.O @= x.reduce_xor()
+                m.config.set_debug_mode(False)
+        else:
+            def __init__(self, n):
+                self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bit))
+                x = m.Bits[n]()
+                x @= ~self.io.I
+                self.io.O @= x.reduce_xor()
+    Foo(n)
+
+
+data = {}
+
+for debug_mode in [0, 1, 2]:
+    data[debug_mode] = timeit.Timer(lambda:
+                                    gen_circuit(debug_mode)).timeit(number=10)
+
+bar = pygal.Bar()
+bar.add('Off', data[0])
+bar.add('Old', data[1])
+bar.add('New', data[2])
+bar.render_to_file("debug.svg")
