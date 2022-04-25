a34d849a9246614149044f340122001c608fdb59
Lenny Truong <leonardtruong@fb.com>
2022-04-25 13:35:25 -0700

    Improve tests

--- tests/test_wire/test_unwire.py
+++ tests/test_wire/test_unwire.py
@@ -1,28 +1,25 @@
 import pytest
+import logging
+
 import magma as m
 
 
-@pytest.mark.parametrize('T', [m.Bit, m.Bits[4], m.Tuple[m.Bit, m.Bits[4]],
-                               m.Array[4, m.Bits[4]]])
-@pytest.mark.parametrize('func', [lambda x, y: x.unwire(y), m.unwire])
+Ts = [m.Bit, m.Bits[4], m.Tuple[m.Bit, m.Bits[4]], m.Array[4, m.Bits[4]]]
+funcs = [
+    # With explicit arg
+    lambda x, y: x.unwire(y), m.unwire,
+    # With implicit default arg
+    lambda x, y: x.unwire(), lambda x, y: m.unwire(x)
+]
+
+
+@pytest.mark.parametrize('T', Ts)
+@pytest.mark.parametrize('func', funcs)
 def test_unwire_with_arg(T, func, caplog):
-    class Foo(m.Circuit):
-        io = m.IO(I0=m.In(T), I1=m.In(T), O=m.Out(T))
-        io.O @= io.I0
-        func(io.O, io.I0)
-        io.O @= io.I1
+    x, y, z = T(), T(), T()
+    x @= y
+    func(x, y)
+    x @= z
 
-    assert len(caplog.records) == 0
-
-
-@pytest.mark.parametrize('T', [m.Bit, m.Bits[4], m.Tuple[m.Bit, m.Bits[4]],
-                               m.Array[4, m.Bits[4]]])
-@pytest.mark.parametrize('func', [lambda x: x.unwire(), m.unwire])
-def test_unwire_no_arg(T, func, caplog):
-    class Foo(m.Circuit):
-        io = m.IO(I0=m.In(T), I1=m.In(T), O=m.Out(T))
-        io.O @= io.I0
-        func(io.O)
-        io.O @= io.I1
-
-    assert len(caplog.records) == 0
+    assert not any(record.level >= logging.WARNING
+                   for record in caplog.records)
