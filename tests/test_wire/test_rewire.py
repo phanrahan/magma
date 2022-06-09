6693901caf823a8da5f23be77c7b91bf44361776
Lenny Truong <leonardtruong@fb.com>
2022-06-09 16:29:27 -0700

    Add rewire error mode
    Fixes https://github.com/phanrahan/magma/issues/1082

--- tests/test_wire/test_rewire.py
+++ tests/test_wire/test_rewire.py
@@ -1,7 +1,7 @@
 import pytest
 
 import magma as m
-from magma.testing.utils import has_warning
+from magma.testing.utils import has_warning, has_error
 
 
 @pytest.mark.parametrize("first,second", ((0, 0), (0, 1)))
@@ -11,3 +11,27 @@ def test_anonymous_bits_rewiring_warning(first, second, caplog):
     x = m.as_bits(x)
     x @= second
     assert has_warning(caplog)
+
+
+@pytest.fixture
+def _setup_and_teardown():
+    prev = m.config.config.rewire_log_level
+    m.config.config.rewire_log_level = "ERROR"
+    yield
+    m.config.config.rewire_log_level = prev
+
+
+def test_rewire_error(caplog, _setup_and_teardown):
+    x = m.Bit(name="x")
+    x @= 0
+    x @= 1
+    assert has_error(caplog)
+    # Check that it makes the circuit def have an error
+
+    class Foo(m.Circuit):
+        io = m.IO(O=m.Out(m.Bit))
+        io.O @= 0
+        io.O @= 1
+
+    with pytest.raises(m.compile_exception.MagmaCompileException):
+        m.compile("build/Foo", Foo)
