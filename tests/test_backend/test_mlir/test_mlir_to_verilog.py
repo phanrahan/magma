a9edc8fdee9ff2e9992ffe31992404bfc4bf4584
Raj Setaluri <raj.setaluri@gmail.com>
2022-06-09 21:55:42 -0700

    [MlirToVerilog] Add basic functionality tests

--- tests/test_backend/test_mlir/test_mlir_to_verilog.py
+++ tests/test_backend/test_mlir/test_mlir_to_verilog.py
@@ -2,8 +2,16 @@ import io
 import pytest
 
 import magma as m
-from magma.backend.mlir.mlir_to_verilog import mlir_to_verilog
+from magma.backend.mlir.mlir_to_verilog import (
+    mlir_to_verilog,
+    circt_opt_binary_exists,
+)
 from magma.testing.utils import with_config
+
+
+def _skip_if_circt_opt_binary_does_not_exist():
+    if not circt_opt_binary_exists():
+        pytest.skip("no circt-opt binary found")
 
 
 @with_config("circt_home", "/this_cant_be_a_real_directory/")
@@ -13,3 +21,26 @@ def test_no_binary():
 
     with pytest.raises(FileNotFoundError):
         mlir_to_verilog(istream.buffer, ostream.buffer)
+
+
+def test_basic():
+    _skip_if_circt_opt_binary_does_not_exist()
+
+    istream = io.TextIOWrapper(io.BytesIO())
+    ostream = io.TextIOWrapper(io.BytesIO())
+
+    mlir_to_verilog(istream.buffer, ostream.buffer)
+
+
+def test_module():
+    _skip_if_circt_opt_binary_does_not_exist()
+
+    istream = io.TextIOWrapper(io.BytesIO())
+    istream.write("module {}\n")
+    istream.seek(0)
+    ostream = io.TextIOWrapper(io.BytesIO())
+
+    mlir_to_verilog(istream.buffer, ostream.buffer)
+
+    ostream.seek(0)
+    assert ostream.read() == ""
