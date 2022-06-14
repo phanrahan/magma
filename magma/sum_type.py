6b58b9d4fb0287930e8adefa6e5d9d43413387a1
Lenny Truong <leonardtruong@fb.com>
2022-06-14 13:50:23 -0700

    WIP match logic

--- magma/sum_type.py
+++ magma/sum_type.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
 from magma.bits import Bits
+from magma.bitutils import clog2
 from magma.protocol_type import MagmaProtocol, MagmaProtocolMeta
 from magma.t import Type, Direction, Kind
 from magma.conversions import from_bits
+from magma.when import when
 
 
 class SumMeta(MagmaProtocolMeta):
@@ -31,7 +33,9 @@ class SumMeta(MagmaProtocolMeta):
                 N = max(N, value.flat_length())
                 Ts[key] = value
         if N > 0:
-            namespace['_magma_T_'] = Bits[N]
+            tag_T = clog2(len(Ts))
+            val_T = Bits[N]
+            namespace['_magma_T_'] = m.AnonProduct[dict(tag=tag_T, val=val_T)]
             namespace['_magma_Ts_'] = Ts
 
         return type.__new__(mcs, name, bases, namespace)
@@ -44,9 +48,8 @@ class Sum(MagmaProtocol, metaclass=SumMeta):
         self._val = val
         for key, T in self._magma_Ts_.items():
             setattr(self, key,
-                    from_bits(T, val[:T.flat_length()]))
+                    from_bits(T, val.val[:T.flat_length()]))
 
 
 def match(value, field):
-    print(value, field)
-    raise Exception()
+    return when(value.tag == list(type(value)._magma_Ts_.keys()).index(field))
