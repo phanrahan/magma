9dd2f30e813bc7a0d6493514a1feb6bcda80cf99
Raj Setaluri <raj.setaluri@gmail.com>
2022-06-21 12:35:06 -0700

    [MLIR] Update golds for complex bind test with flattened tuples

--- tests/test_backend/test_mlir/golds/complex_bind_flatten_all_tuples.mlir
+++ tests/test_backend/test_mlir/golds/complex_bind_flatten_all_tuples.mlir
@@ -1,4 +1,7 @@
-hw.module @complex_bind_asserts(%I_I: i1, %O: i1, %CLK: i1, %I0: i1) -> () {
+hw.module @complex_bind_child(%I_I: i1) -> (O_I: i1) {
+    hw.output %I_I : i1
+}
+hw.module @complex_bind_asserts(%I_I: i1, %O: i1, %CLK: i1, %I0: i1, %I1: i1, %I2: i1) -> () {
     %1 = sv.wire sym @complex_bind_asserts._magma_inline_wire0 {name="_magma_inline_wire0"} : !hw.inout<i1>
     sv.assign %1, %O : i1
     %0 = sv.read_inout %1 : !hw.inout<i1>
@@ -23,7 +26,12 @@ hw.module @complex_bind(%I_I: i1, %CLK: i1) -> (O: i1) {
         sv.bpassign %4, %5 : i1
     }
     %3 = sv.read_inout %4 : !hw.inout<i1>
-    hw.instance "complex_bind_asserts_inst" sym @complex_bind.complex_bind_asserts_inst @complex_bind_asserts(I_I: %I_I: i1, O: %3: i1, CLK: %CLK: i1, I0: %0: i1) -> () {doNotPrint = 1}
+    %6 = hw.instance "complex_bind_child_inst0" @complex_bind_child(I_I: %I_I: i1) -> (O_I: i1)
+    %7 = sv.xmr "I_I" : !hw.inout<i1>
+    %8 = sv.read_inout %7 : !hw.inout<i1>
+    %9 = sv.xmr "complex_bind_child_inst0.O_I" : !hw.inout<i1>
+    %10 = sv.read_inout %9 : !hw.inout<i1>
+    hw.instance "complex_bind_asserts_inst" sym @complex_bind.complex_bind_asserts_inst @complex_bind_asserts(I_I: %I_I: i1, O: %3: i1, CLK: %CLK: i1, I0: %0: i1, I1: %8: i1, I2: %10: i1) -> () {doNotPrint = 1}
     hw.output %3 : i1
 }
 sv.bind #hw.innerNameRef<@complex_bind::@complex_bind.complex_bind_asserts_inst>
--- tests/test_backend/test_mlir/golds/complex_bind_flatten_all_tuples.v
+++ tests/test_backend/test_mlir/golds/complex_bind_flatten_all_tuples.v
@@ -1,37 +1,55 @@
-module complex_bind_asserts(	// <stdin>:1:1
-  input I_I, O, CLK, I0);
+module complex_bind_child(	// <stdin>:1:1
+  input  I_I,
+  output O_I);
 
-  wire _magma_inline_wire0;	// <stdin>:2:10
-  wire _magma_inline_wire1;	// <stdin>:5:10
-  wire _magma_inline_wire2;	// <stdin>:8:10
-
-  assign _magma_inline_wire0 = O;	// <stdin>:3:5
-  assign _magma_inline_wire1 = I_I;	// <stdin>:6:5
-  assign _magma_inline_wire2 = I0;	// <stdin>:9:5
-  assert property (@(posedge CLK) _magma_inline_wire1 |-> ##1 _magma_inline_wire0);assert property (_magma_inline_wire1 |-> _magma_inline_wire2;	// <stdin>:4:10, :7:10, :10:10, :11:5
+  assign O_I = I_I;	// <stdin>:2:5
 endmodule
 
-module complex_bind(	// <stdin>:13:1
+module complex_bind_asserts(	// <stdin>:4:1
+  input I_I, O, CLK, I0, I1, I2);
+
+  wire _magma_inline_wire0;	// <stdin>:5:10
+  wire _magma_inline_wire1;	// <stdin>:8:10
+  wire _magma_inline_wire2;	// <stdin>:11:10
+
+  assign _magma_inline_wire0 = O;	// <stdin>:6:5
+  assign _magma_inline_wire1 = I_I;	// <stdin>:9:5
+  assign _magma_inline_wire2 = I0;	// <stdin>:12:5
+  assert property (@(posedge CLK) _magma_inline_wire1 |-> ##1 _magma_inline_wire0);assert property (_magma_inline_wire1 |-> _magma_inline_wire2;	// <stdin>:7:10, :10:10, :13:10, :14:5
+endmodule
+
+module complex_bind(	// <stdin>:16:1
   input  I_I, CLK,
   output O);
 
-  wire complex_bind_asserts_inst_I0;	// <stdin>:26:5
-  reg  Register_inst0;	// <stdin>:17:10
+  wire complex_bind_asserts_inst_I0;	// <stdin>:34:5
+  wire complex_bind_asserts_inst_I1;	// <stdin>:34:5
+  wire complex_bind_asserts_inst_I2;	// <stdin>:34:5
+  wire complex_bind_child_inst0_O_I;	// <stdin>:29:10
+  reg  Register_inst0;	// <stdin>:20:10
 
-  always_ff @(posedge CLK)	// <stdin>:18:5
-    Register_inst0 <= I_I;	// <stdin>:19:9
-  initial	// <stdin>:22:5
-    Register_inst0 = 1'h0;	// <stdin>:21:10, :23:9
-  assign complex_bind_asserts_inst_I0 = ~I_I;	// <stdin>:15:10, :26:5
+  always_ff @(posedge CLK)	// <stdin>:21:5
+    Register_inst0 <= I_I;	// <stdin>:22:9
+  initial	// <stdin>:25:5
+    Register_inst0 = 1'h0;	// <stdin>:24:10, :26:9
+  complex_bind_child complex_bind_child_inst0 (	// <stdin>:29:10
+    .I_I (I_I),
+    .O_I (complex_bind_child_inst0_O_I)
+  );
+  assign complex_bind_asserts_inst_I0 = ~I_I;	// <stdin>:18:10, :34:5
+  assign complex_bind_asserts_inst_I1 = I_I;	// <stdin>:30:10, :31:10, :34:5
+  assign complex_bind_asserts_inst_I2 = complex_bind_child_inst0.O_I;	// <stdin>:32:10, :33:11, :34:5
   /* This instance is elsewhere emitted as a bind statement.
-    complex_bind_asserts complex_bind_asserts_inst (	// <stdin>:26:5
+    complex_bind_asserts complex_bind_asserts_inst (	// <stdin>:34:5
       .I_I (I_I),
-      .O   (Register_inst0),	// <stdin>:25:10
+      .O   (Register_inst0),	// <stdin>:28:10
       .CLK (CLK),
-      .I0  (complex_bind_asserts_inst_I0)	// <stdin>:26:5
+      .I0  (complex_bind_asserts_inst_I0),	// <stdin>:34:5
+      .I1  (complex_bind_asserts_inst_I1),	// <stdin>:34:5
+      .I2  (complex_bind_asserts_inst_I2)	// <stdin>:34:5
     );
   */
-  assign O = Register_inst0;	// <stdin>:25:10, :27:5
+  assign O = Register_inst0;	// <stdin>:28:10, :35:5
 endmodule
 
 
@@ -41,5 +59,7 @@ bind complex_bind complex_bind_asserts complex_bind_asserts_inst (
   .I_I (I_I),
   .O   (Register_inst0),
   .CLK (CLK),
-  .I0  (complex_bind_asserts_inst_I0)
+  .I0  (complex_bind_asserts_inst_I0),
+  .I1  (complex_bind_asserts_inst_I1),
+  .I2  (complex_bind_asserts_inst_I2)
 );
