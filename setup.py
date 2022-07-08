69aee02eccd3fc8cc5781bfd8f8c871de17c3cf7
Raj Setaluri <raj.setaluri@gmail.com>
2022-07-08 13:20:49 -0500

    Relase 2.2.26

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.2.25',
+    version='2.2.26',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
