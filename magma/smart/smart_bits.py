618d48a6713d72dd7e1e3803ed82144077236fef
rsetaluri <raj.setaluri@gmail.com>
2022-08-04 12:38:40 -0700

    [SmartBits] Move make_smart() to separate file

--- magma/smart/smart_bits.py
+++ magma/smart/smart_bits.py
@@ -8,9 +8,6 @@ from magma.conversions import uint, bits, sint
 from magma.conversions import concat as bits_concat
 from magma.debug import debug_wire
 from magma.protocol_type import MagmaProtocolMeta, MagmaProtocol
-from magma.t import Direction
-from magma.type_utils import TypeTransformer, issint
-from magma.value_utils import ValueTransformer, make_selector
 
 
 def _is_int(value):
@@ -442,27 +439,3 @@ def eval(expr: SmartExpr, width: int, signed: bool = False):
     lhs = SmartBits[width, signed]()
     lhs @= expr
     return lhs
-
-
-class SmartifyTypeTransformer(TypeTransformer):
-    def visit_Bits(self, T):
-        signed = issint(T)
-        return SmartBits[len(T), signed]
-
-
-class _InitializeSmartValueTransformer(ValueTransformer):
-    def __init__(self, value):
-        self._value = value
-
-    def visit_Bits(self, value):
-        sel = make_selector(value)
-        init = sel.select(self._value)
-        signed = issint(type(value))
-        return SmartBits[len(value), signed](init)
-
-
-def make_smart(value):
-    Tsmart = SmartifyTypeTransformer().visit(type(value))
-    Tsmart = Tsmart.qualify(Direction.Undirected)
-    smart_value = Tsmart()
-    return _InitializeSmartValueTransformer(value).visit(smart_value)
