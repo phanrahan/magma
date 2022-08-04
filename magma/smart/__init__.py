2d88211b9b785e83b5d7a782be8fc4abcdd9a31b
rsetaluri <raj.setaluri@gmail.com>
2022-08-04 12:38:40 -0700

    [SmartBits] Move make_smart() to separate file

--- magma/smart/__init__.py
+++ magma/smart/__init__.py
@@ -1,2 +1,4 @@
-from .smart_bits import (SmartBits, SmartBit, concat, make_smart,
-                         signed, unsigned, eval)
+from magma.smart.smart_bits import (
+    SmartBits, SmartBit, concat, signed, unsigned, eval
+)
+from magma.smart.make_smart import make_smart
