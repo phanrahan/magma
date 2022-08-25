a0da0fde4451ae5d2016330f888e725122aa5161
Lenny Truong <leonardtruong@fb.com>
2022-08-25 16:27:58 -0700

    Update protocol wire

--- magma/protocol_type.py
+++ magma/protocol_type.py
@@ -101,10 +101,10 @@ class MagmaProtocol(metaclass=MagmaProtocolMeta):
         return self._get_magma_value_().name
 
     @debug_wire
-    def wire(self, other, debug_info):
+    def wire(self, other, debug_info, check_when_context=True):
         if isinstance(other, MagmaProtocol):
             other = other._get_magma_value_()
-        self._get_magma_value_().wire(other, debug_info)
+        self._get_magma_value_().wire(other, debug_info, check_when_context)
 
     def unwire(self, other):
         if isinstance(other, MagmaProtocol):
