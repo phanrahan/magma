3f1cf38a895cfab25717e4cb827850f2d0deb5bf
Lenny Truong <leonardtruong@fb.com>
2022-08-25 14:43:19 -0700

    [when] Fixes bug with lazy array value creation
    Lazily created values should inherit the when context of its parent
    (i.e. the active context when the original value was created), rather
    than getting the currently active when context (problematic if the value
    is referenced in a different context than the original parent value's
    context)

--- tests/test_when.py
+++ tests/test_when.py
@@ -636,3 +636,25 @@ def test_when_lazy_array_resolve(caplog):
 
     m.compile(f"build/{_Test.name}", _Test, output="mlir")
     assert check_gold(__file__, f"{_Test.name}.mlir")
+
+
+def test_when_lazy_array(caplog):
+
+    class _Test(m.Circuit):
+        name = "test_when_lazy_array"
+        io = m.IO(S=m.In(m.Bit), O=m.Out(m.Bits[2]))
+
+        x = m.Bits[2](name="x")
+
+        with m.when(io.S):
+            x[0] @= 0
+            x[1] @= 1
+        with m.otherwise():
+            x[0] @= 1
+            x[1] @= 0
+
+        io.O @= x
+
+    basename = "test_when_lazy_array"
+    m.compile(f"build/{basename}", _Test, output="mlir")
+    assert check_gold(__file__, f"{basename}.mlir")
