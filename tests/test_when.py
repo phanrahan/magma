9866527b6a957db4bc1e4d12b19835c93db9e06c
Lenny Truong <leonardtruong@fb.com>
2022-09-19 11:03:37 -0700

    Remove verilog compile from test

--- tests/test_when.py
+++ tests/test_when.py
@@ -503,7 +503,7 @@ def test_internal_instantiation_complex():
             x @= 0
 
     basename = "test_when_internal_instantiation_complex"
-    m.compile(f"build/{basename}", _Test, output="mlir-verilog")
+    m.compile(f"build/{basename}", _Test, output="mlir")
     assert check_gold(__file__, f"{basename}.mlir")
 
 
@@ -644,7 +644,7 @@ def test_when_lazy_array_resolve(caplog):
         with m.otherwise():
             io.O @= m.sint(m.uint(io.I) >> 1)
 
-    m.compile(f"build/{_Test.name}", _Test, output="mlir-verilog")
+    m.compile(f"build/{_Test.name}", _Test, output="mlir")
     assert check_gold(__file__, f"{_Test.name}.mlir")
 
 
@@ -687,7 +687,7 @@ def test_when_lazy_array_slice(caplog):
 
         io.O @= x
 
-    m.compile(f"build/{_Test.name}", _Test, output="mlir-verilog")
+    m.compile(f"build/{_Test.name}", _Test, output="mlir")
     assert check_gold(__file__, f"{_Test.name}.mlir")
 
 
