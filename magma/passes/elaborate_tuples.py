68c33c6d2f7b4a19ecad26d0e4a8311757878e93
Lenny Truong <leonardtruong@fb.com>
2022-09-27 12:02:10 -0700

    Refactor pass

--- magma/passes/elaborate_tuples.py
+++ magma/passes/elaborate_tuples.py
@@ -18,19 +18,20 @@ def _expand(value):
     if isinstance(value, Array) and _is_tuple_or_nested_tuple(type(value)):
         for elem in value:
             _expand(elem)
-    return
+
+
+def _expand_tuple_values(values):
+    for value in values:
+        if _is_tuple_or_nested_tuple(type(value)):
+            _expand(value)
 
 
 class ElaborateTuplesPass(DefinitionPass):
     def __call__(self, definition):
         with definition.open():
-            for value in definition.interface.ports.values():
-                if _is_tuple_or_nested_tuple(type(value)):
-                    _expand(value)
+            _expand_tuple_values(definition.interface.ports.values())
             for instance in definition.instances:
-                for value in instance.interface.ports.values():
-                    if _is_tuple_or_nested_tuple(type(value)):
-                        _expand(value)
+                _expand_tuple_values(instance.interface.ports.values())
 
 
 elaborate_tuples = pass_lambda(ElaborateTuplesPass)
