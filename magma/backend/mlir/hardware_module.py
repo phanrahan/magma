c20cd9bd13e742ff4024a01fd57dbfcff86ec63e
Lenny Truong <leonardtruong@fb.com>
2022-10-10 13:08:21 -0700

    Fix style

--- magma/backend/mlir/hardware_module.py
+++ magma/backend/mlir/hardware_module.py
@@ -555,10 +555,10 @@ class ModuleVisitor:
         output_to_index = {}
 
         def _visit(value, counter, value_to_index):
-            # NOTE(leonardt): value may already be in value_to_index, in which case we
-            # update it to a new index.  This is okay and it just means that
-            # `value` occurs as more than one input (for example, it could be
-            # used as a conditional driver for multiple values).  We could
+            # NOTE(leonardt): value may already be in value_to_index, in which
+            # case we update it to a new index.  This is okay and it just means
+            # that `value` occurs as more than one input (for example, it could
+            # be used as a conditional driver for multiple values).  We could
             # optimize the logic to always share the same argument, but for now
             # we just use the "last" index.
             value_to_index[value] = next(counter)
