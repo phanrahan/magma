421036afcb1e99b95ea80f03614e7424256fa665
Lenny Truong <leonardtruong@fb.com>
2022-10-14 18:16:30 -0700

    Use tempdir, add comma

--- tests/test_backend/test_mlir/test_mlir_compiler.py
+++ tests/test_backend/test_mlir/test_mlir_compiler.py
@@ -1,11 +1,13 @@
+import tempfile
 import magma as m
 
 from examples import complex_when_lazy_tuple
 
 
 def test_mlir_compiler_finalize_when_order():
-    m.compile(
-        "build/complex_when_lazy_tuple",
-        complex_when_lazy_tuple,
-        output="mlir",
-    )
+    with tempfile.TemporaryDirectory() as tmpdir:
+        m.compile(
+            f"{tmpdir}/complex_when_lazy_tuple",
+            complex_when_lazy_tuple,
+            output="mlir",
+        )
