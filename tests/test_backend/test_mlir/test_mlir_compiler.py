e08dab4e90ed9fcf3e94cdd5cbae214911d2afcc
Lenny Truong <lenny@cs.stanford.edu>
2022-10-14 18:13:40 -0700

    Update tests/test_backend/test_mlir/test_mlir_compiler.py
    Co-authored-by: rsetaluri <rsetaluri@users.noreply.github.com>

--- tests/test_backend/test_mlir/test_mlir_compiler.py
+++ tests/test_backend/test_mlir/test_mlir_compiler.py
@@ -3,5 +3,8 @@ from examples import complex_when_lazy_tuple
 
 
 def test_mlir_compiler_finalize_when_order():
-    m.compile("build/complex_when_lazy_tuple", complex_when_lazy_tuple,
-              output="mlir")
+    m.compile(
+        "build/complex_when_lazy_tuple",
+        complex_when_lazy_tuple,
+        output="mlir",
+    )
