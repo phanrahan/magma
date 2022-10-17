ef9556c8f4e99bd8327beb1b27ad9554f63e1211
rsetaluri <raj.setaluri@gmail.com>
2022-10-17 13:03:31 -0700

    Fix smart bits eval import

--- magma/smart/__init__.py
+++ magma/smart/__init__.py
@@ -1,4 +1,4 @@
 from magma.smart.smart_bits import (
-    SmartBits, SmartBit, concat, signed, unsigned, eval
+    SmartBits, SmartBit, concat, signed, unsigned, eval as evaluate
 )
 from magma.smart.make_smart import make_smart
