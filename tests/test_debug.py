dfb34c5f5ead23aeb171af787c8855af865428fb
Lenny Truong <leonardtruong@fb.com>
2022-11-29 12:55:03 -0800

    Update test

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,11 +1,12 @@
 import os
 
-from magma.debug import get_debug_info
+from magma.debug import get_debug_info, debug_info
 
 
 def test_magma_debug_ext():
     filedir = os.path.realpath(os.path.dirname(__file__))
-    assert get_debug_info(0) == (
+    assert get_debug_info(0) == debug_info(
         f"{filedir}/test_debug_ext.py",
-        8
+        8,
+        None
     )
