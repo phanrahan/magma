4a2a2992af02043f8af973947836a4cb6a95e7f0
Lenny Truong <leonardtruong@fb.com>
2022-11-29 12:41:33 -0800

    Remove extension from setup.py

--- setup.py
+++ setup.py
@@ -1,55 +1,4 @@
-from distutils.version import LooseVersion
-import multiprocessing
-import os
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-import subprocess
-import sys
-
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-
-        cfg = 'Release'
-        build_args = ['--config', cfg]
-
-        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-
-        cpu_count = max(2, multiprocessing.cpu_count() // 2)
-        build_args += ['--', '-j{0}'.format(cpu_count)]
-
-        python_path = sys.executable
-        cmake_args += ['-DPYTHON_EXECUTABLE:FILEPATH=' + python_path]
-
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp)
-
-        subprocess.check_call(
-            ['cmake', '--build', '.'] + build_args,
-            cwd=self.build_temp)
+from setuptools import setup
 
 
 with open("README.md", "r") as fh:
@@ -80,8 +29,6 @@ setup(
         "magma.testing",
         "magma.types"
     ],
-    ext_modules=[CMakeExtension("magma_debug", 'debug-ext')],
-    cmdclass=dict(build_ext=CMakeBuild),
     install_requires=[
         "colorlog",
         "astor",
@@ -96,6 +43,7 @@ setup(
         "ast_tools>=0.0.16",
         "staticfg",
         "networkx",
+        "uinspect"
     ],
     python_requires='>=3.6',
     long_description=long_description,
