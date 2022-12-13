f6e3e0d6511f28cb5c337ca12e5eb072368efad4
Lenny Truong <lenny@stanford.edu>
2022-12-13 12:56:42 -0800

    Fix conftest

--- conftest.py
+++ conftest.py
@@ -5,6 +5,6 @@ from magma.util import reset_global_context
 
 @pytest.fixture(autouse=True)
 def magma_test():
-    m.config.set_debug_mode(True)
+    magma.config.set_debug_mode(True)
     magma.config.set_compile_dir('callee_file_dir')
     reset_global_context()
