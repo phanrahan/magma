37050656083962b376ae7f5e71db1c2c8bfa86e0
Lenny Truong <lenny@stanford.edu>
2022-12-13 13:09:12 -0800

    Fix conftest

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 import pytest
 import magma.config
+from magma.config import config
 from magma.util import reset_global_context
 
 
 @pytest.fixture(autouse=True)
 def magma_test():
-    magma.config.use_namer_dict = True
+    config.use_namer_dict = True
     magma.config.set_compile_dir('callee_file_dir')
     reset_global_context()
