64c5fc63aa68b31da83c1efa354d77dce0a09634
rsetaluri <raj.setaluri@gmail.com>
2023-01-09 12:12:51 -0600

    [Debug] Add primitive debug info test

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,5 +1,8 @@
 import os
+import pathlib
+import pytest
 
+import magma as m
 from magma.debug import get_debug_info, debug_info
 
 
@@ -7,6 +10,18 @@ def test_magma_debug_ext():
     filedir = os.path.realpath(os.path.dirname(__file__))
     assert get_debug_info(2) == debug_info(
         f"{filedir}/test_debug.py",
-        8,
+        11,
         None
     )
+
+
+@pytest.mark.parametrize('T,name', ((m.Bit, "bit"), (m.Bits[8], "bits")))
+def test_primitive_debug_info(T, name):
+
+    class Foo(m.Circuit):
+        ~(T())  # just to instance some primitive
+
+    inst = Foo.instances[0]
+    filename = pathlib.Path(inst.debug_info.filename)
+    filename = pathlib.Path("/".join(filename.parts[-2:]))
+    assert str(filename) == f"magma/{name}.py"
