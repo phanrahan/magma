1ea5463ad60ef6f39523276ede94a1b4df1e8570
Lenny Truong <lenny@stanford.edu>
2023-01-10 09:35:53 -0800

    Update old tests

--- tests/test_deprecated/test_old_io_syntax/test_old_io_syntax_define.py
+++ tests/test_deprecated/test_old_io_syntax/test_old_io_syntax_define.py
@@ -14,7 +14,7 @@ if sys.version_info < (3, 8):
 @pytest.mark.parametrize("target,suffix",
                          [("verilog", "v"), ("coreir", "json")])
 def test_simple_def(target, suffix):
-    m.config.config.use_namer_dict = True
+    m.config.set_debug_mode(True)
     m.set_codegen_debug_info(True)
     with pytest.warns(DeprecationWarning):
         And2 = m.DeclareCircuit('And2', "I0", m.In(m.Bit), "I1", m.In(m.Bit),
@@ -48,7 +48,8 @@ def test_simple_def(target, suffix):
     # Create a fresh context for second compilation.
     m.compile("build/test_simple_def_class", Main, output=target)
     m.set_codegen_debug_info(False)
-    m.config.config.use_namer_dict = False
+    m.config.set_debug_mode(False)
+    m.config.config.use_uinspect = True
     assert check_files_equal(__file__, f"build/test_simple_def_class.{suffix}",
                              f"gold/test_simple_def_class.{suffix}")
 
@@ -56,7 +57,7 @@ def test_simple_def(target, suffix):
 @pytest.mark.parametrize("target,suffix",
                          [("verilog", "v"), ("coreir", "json")])
 def test_for_loop_def(target, suffix):
-    m.config.config.use_namer_dict = True
+    m.config.set_debug_mode(True)
     m.set_codegen_debug_info(True)
     with pytest.warns(DeprecationWarning):
         And2 = m.DeclareCircuit('And2', "I0", m.In(m.Bit), "I1", m.In(m.Bit),
@@ -81,7 +82,8 @@ def test_for_loop_def(target, suffix):
 
     m.compile("build/test_for_loop_def", main, output=target)
     m.set_codegen_debug_info(False)
-    m.config.config.use_namer_dict = False
+    m.config.set_debug_mode(False)
+    m.config.config.use_uinspect = True
     assert check_files_equal(__file__, f"build/test_for_loop_def.{suffix}",
                              f"gold/test_for_loop_def.{suffix}")
 
@@ -89,7 +91,7 @@ def test_for_loop_def(target, suffix):
 @pytest.mark.parametrize("target,suffix",
                          [("verilog", "v"), ("coreir", "json")])
 def test_interleaved_instance_wiring(target, suffix):
-    m.config.config.use_namer_dict = True
+    m.config.set_debug_mode(True)
     m.set_codegen_debug_info(True)
     with pytest.warns(DeprecationWarning):
         And2 = m.DeclareCircuit('And2', "I0", m.In(m.Bit), "I1", m.In(m.Bit),
@@ -114,7 +116,8 @@ def test_interleaved_instance_wiring(target, suffix):
 
     m.compile("build/test_interleaved_instance_wiring", main, output=target)
     m.set_codegen_debug_info(False)
-    m.config.config.use_namer_dict = False
+    m.config.set_debug_mode(False)
+    m.config.config.use_uinspect = True
     assert check_files_equal(__file__, f"build/test_interleaved_instance_wiring.{suffix}",
                              f"gold/test_interleaved_instance_wiring.{suffix}")
 
