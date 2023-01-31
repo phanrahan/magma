003df4d674f609c8f21b48fbd02cc56937708544
Lenny Truong <leonardtruong@fb.com>
2023-01-31 11:25:35 -0800

    Release v2.2.51

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.2.50',
+    version='2.2.51',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
