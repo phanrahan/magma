f455f18c90db55df37eb3fd5a5450229ac56d655
Lenny Truong <leonardtruong@fb.com>
2023-02-01 16:44:34 -0800

    Revert "Fix case for mismatched qualifiers"
    This reverts commit a95452181fce354b0200d8dd7728834de2103d62.

--- magma/digital.py
+++ magma/digital.py
@@ -136,12 +136,8 @@ class DigitalMeta(ABCMeta, Kind):
             return True
         rhs = magma_type(rhs)
         # allows undirected types to match (e.g. for temporary values)
-        return (
-            issubclass(rhs.flip(), cls) or
-            issubclass(cls, rhs.flip()) or
-            issubclass(rhs, cls.qualify(Direction.Undirected)) or
-            issubclass(cls, rhs.qualify(Direction.Undirected))
-        )
+        return (issubclass(rhs.undirected_t, cls.undirected_t) or
+                issubclass(cls.undirected_t, rhs.undirected_t))
 
     def is_bindable(cls, rhs):
         return issubclass(cls, magma_type(rhs))
