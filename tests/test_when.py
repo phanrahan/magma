86c024ace6347cd32468f73baec96d18031aff6c
Lenny Truong <leonardtruong@fb.com>
2023-02-03 15:11:11 -0800

    Remove verilog compile

--- tests/test_when.py
+++ tests/test_when.py
@@ -1307,7 +1307,7 @@ def test_when_3d_array_assign():
             io.O[0][1] @= io.I[1][0]
 
     m.compile("build/test_when_3d_array_assign",
-              test_when_3d_array_assign, output="mlir-verilog")
+              test_when_3d_array_assign, output="mlir")
 
     assert check_gold(__file__, "test_when_3d_array_assign.mlir")
 
