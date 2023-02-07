8c90a84b925e31d0c11adedfe6eb58d3555414ba
Lenny Truong <leonardtruong@fb.com>
2023-02-07 13:17:56 -0800

    Remove wrapper circuit

--- tests/test_type/test_product.py
+++ tests/test_type/test_product.py
@@ -2,9 +2,7 @@ import magma as m
 
 
 def test_anon_product_literal():
-    class Foo(m.Circuit):
-        io = m.IO(I=m.In(m.Bit), O=m.Out(m.Bit))
-
-        x = m.AnonProduct[{"I": m.Bit, "CE": m.Bit}]()
-        x @= m.product(I=io.I, CE=False)
-    assert Foo.x.CE.value() is m.GND
+    I = m.Bit()
+    x = m.AnonProduct[{"I": m.Bit, "CE": m.Bit}]()
+    x @= m.product(I=I, CE=False)
+    assert x.CE.value() is m.GND
