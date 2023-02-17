9f3c6ddac907da9a5360a2e563d6f69b63b58e32
Lenny Truong <leonardtruong@fb.com>
2023-02-17 09:11:07 -0800

    Release 2.2.53
    * [when] Finalize whens before uniquification (#1235)
    * [when] hotfix wiring to array with delayed elaboration (#1236)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.2.52',
+    version='2.2.53',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
