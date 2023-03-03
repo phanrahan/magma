7bea994cdb772b7eea207c72a631558335ce52ac
rsetaluri <raj.setaluri@gmail.com>
2023-03-03 14:36:50 -0800

    [MLIR] Check verilog for split verilog test

---
+++ tests/test_backend/test_mlir/golds/simple_hierarchy_split_verilog_real.v
@@ -0,0 +1,27 @@
+module simple_comb(
+  input  [15:0] a,
+                b,
+                c,
+  output [15:0] y,
+                z);
+
+  assign y = 16'hFFFF;
+  assign z = 16'hFFFF;
+endmodule
+
+module simple_hierarchy(
+  input  [15:0] a,
+                b,
+                c,
+  output [15:0] y,
+                z);
+
+  simple_comb simple_comb_inst0 (
+    .a (a),
+    .b (b),
+    .c (c),
+    .y (y),
+    .z (z)
+  );
+endmodule
+
