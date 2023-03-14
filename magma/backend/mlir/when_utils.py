30ca12f03c67cf85b0b50db2bb5b77b06fa20b96
Lenny Truong <leonardtruong@fb.com>
2023-03-14 15:43:36 -0700

    Remove unused import

--- magma/backend/mlir/when_utils.py
+++ magma/backend/mlir/when_utils.py
@@ -9,7 +9,7 @@ from magma.backend.mlir.magma_common import (
 )
 from magma.backend.mlir.mlir import get_block_stack, MlirValue, push_block
 from magma.backend.mlir.sv import sv
-from magma.common import sort_by_value, _assert_false
+from magma.common import sort_by_value
 from magma.primitives.when import iswhen
 from magma.ref import ArrayRef, TupleRef
 from magma.value_utils import make_selector
