df5b4bb922ada4c2dfe0720b54705dfbba4672ed
Lenny Truong <lenny@cs.stanford.edu>
2023-03-31 11:18:31 -0700

    [MLIR] Add support for sync_read_mem (#1203)
    Co-authored-by: rsetaluri <raj.setaluri@gmail.com>

--- tests/test_backend/test_mlir/test_utils.py
+++ tests/test_backend/test_mlir/test_utils.py
@@ -189,6 +189,7 @@ def get_local_examples() -> List[DefineCircuitKind]:
         examples.simple_undriven,
         examples.complex_undriven,
         examples.simple_memory_wrapper,
+        examples.sync_memory_wrapper,
         examples.simple_undriven_instances,
         examples.simple_neg,
         examples.simple_array_slice,
