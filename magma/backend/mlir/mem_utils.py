79c7f564668517eea4c7336400d73f6b22e02973
rsetaluri <raj.setaluri@gmail.com>
2023-03-31 11:33:39 -0700

    [MLIR] Add support for multiport memory

--- magma/backend/mlir/mem_utils.py
+++ magma/backend/mlir/mem_utils.py
@@ -31,3 +31,47 @@ def make_index_op(ctx, value, idx):
     result = ctx.new_value(hw.InOutType(value.type.T.T))
     sv.ArrayIndexInOutOp(operands=[value, idx], results=[result])
     return result
+
+
+def collect_multiport_memory_operands(
+    operands, start_idx, num_ports, num_operands_per_port
+):
+    """Collect flat list of operands for read or write ports.
+    start_idx: offset in `operands` list to start from
+    num_ports: number of ports to collect
+    num_operands_per_port: number of operands per port
+                           (e.g. 3 for waddr, wdata, wen)
+    """
+    port_operands = []
+    curr_idx = start_idx
+    for i in range(num_ports):
+        port_operands.append(
+            operands[curr_idx:curr_idx + num_operands_per_port]
+        )
+        curr_idx += num_operands_per_port
+    return port_operands
+
+
+def make_multiport_memory_index_ops(ctx, ports, mem):
+    """For each port, emit an array index op and return a list of results."""
+    return [make_index_op(ctx, mem, port[0]) for port in ports]
+
+
+def make_multiport_memory_read_ops(
+        ctx, read_results, read_ports, read_ports_out
+):
+    """If ren, emit an intermediate register to hold read value."""
+    read_targets = []
+    for i, (target, port) in enumerate(zip(read_results, read_ports)):
+        has_en = len(port) == 2
+        read_reg, read_temp = make_mem_read(
+            ctx, target, read_ports_out[i], has_en, f"read_reg_{i}"
+        )
+        if has_en:
+            read_targets.append((read_reg, read_temp, port[1]))
+    return read_targets
+
+
+def emit_conditional_assigns(targets):
+    for target, data, en in targets:
+        emit_conditional_assign(target, data, en)
