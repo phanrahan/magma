856f7ffdff031886b50f9df22665cc3c8e7d766a
rsetaluri <raj.setaluri@gmail.com>
2023-04-06 13:07:17 -0700

    [InlineVerilog2] Add simplest implementation

--- magma/inline_verilog2.py
+++ magma/inline_verilog2.py
@@ -1,2 +1,40 @@
-def inline_verilog2(*args, **kwargs):
-    raise Exception()
+import hashlib
+
+from magma.generator import Generator2
+from magma.interface import IO
+from magma.logging import root_logger
+
+
+_logger = root_logger().getChild("inline_verilog2")
+
+
+# TODO(rsetaluri): Make this re-usable from magma/inline_verilog_exrpession.py.
+def _hash_expr(expr: str):
+    hasher = hashlib.shake_128()
+    hasher.update(expr.encode())
+    return hasher.hexdigest(8)
+
+
+def _clean_kwargs(kwargs):
+    try:
+        kwargs.pop("inline_wire_prefix")
+        _logger.warning("bad")
+    except KeyError:
+        pass
+
+
+class _InlineVerilog2(Generator2):
+    def __init__(self, expr: str):
+        import magma as m
+        self.expr = expr
+        self.io = IO()
+        self.primitive = True
+        self.name = f"InlineVerilog2_{_hash_expr(expr)}"
+
+
+InlineVerilog2 = _InlineVerilog2
+
+
+def inline_verilog2(expr, **kwargs):
+    _clean_kwargs(kwargs)
+    _InlineVerilog2(expr)()
