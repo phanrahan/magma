5e9f0695b05f567942ca9a9bc5b38964cc280311
Lenny Truong <leonardtruong@fb.com>
2023-04-11 16:29:52 -0700

    Revert test

--- tests/test_verilog/test_inline.py
+++ tests/test_verilog/test_inline.py
@@ -282,21 +282,17 @@ assert property (@(posedge {clk}) disable iff (! {rst}) {io.x} |-> ##1 {io.y});
         f"gold/test_inline_verilog_share_default_clocks.v")
 
 
-def test_inline_verilog_output():
-    class Main(m.Circuit):
-        io = m.IO(I=m.In(m.Bit), O=m.Out(m.Bit), arr=m.In(m.Bits[2]))
-        io += m.ClockIO()
-        m.inline_verilog(
-            "assert property (@(posedge CLK) {I} |-> ##1 {O});",
-            O=io.O, I=io.I, inline_wire_prefix="_foo_prefix_")
+def test_inline_verilog_error():
+    with pytest.raises(InlineVerilogError) as e:
+        class Main(m.Circuit):
+            io = m.IO(I=m.In(m.Bit), O=m.Out(m.Bit), arr=m.In(m.Bits[2]))
+            io += m.ClockIO()
+            # Should error because io.O is undriven
+            m.inline_verilog(
+                "assert property (@(posedge CLK) {I} |-> ##1 {O});",
+                O=io.O, I=io.I, inline_wire_prefix="_foo_prefix_")
 
-    m.compile("build/test_inline_verilog_output", Main, output="mlir")
-    assert m.testing.check_files_equal(
-        __file__,
-        "build/test_inline_verilog_output.mlir",
-        "gold/test_inline_verilog_output.mlir"
-    )
-
+    assert str(e.value) == "Found reference to undriven input port: LazyCircuit.O"
 
 
 def test_inline_passthrough_wire():
