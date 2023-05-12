3a57535f3767a94ecdb7c66bbcf639217afd4648
Lenny Truong <leonardtruong@fb.com>
2023-05-12 14:24:42 -0700

    Fix test config

--- examples/riscv_mini/conftest.py
+++ examples/riscv_mini/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 
+from magma.config import config as magma_config
 from magma.util import reset_global_context
 
 
 @pytest.fixture(autouse=True)
-def magma_test():
+def riscv_mini_test():
+    magma_config.compile_dir = 'normal'
     reset_global_context()
