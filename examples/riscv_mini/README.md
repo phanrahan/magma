3d1c232700390cdd59d4dff7db08f0e1f3c7288f
Lenny Truong <leonardtruong@fb.com>
2023-05-23 10:26:52 -0700

    Temporarily remove riscv_mini

--- examples/riscv_mini/README.md
+++
@@ -1,3 +0,0 @@
-# magma_riscv_mini
-
-magma port of https://github.com/ucb-bar/riscv-mini
--- examples/riscv_mini/tests/test_control.py
+++
@@ -1,5 +0,0 @@
-from riscv_mini.control import Control
-
-
-def test_control_gen():
-    control = Control(32)
