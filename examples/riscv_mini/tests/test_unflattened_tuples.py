1d9e0ea664ce4cfb6176646bc15c0838969e59f0
Lenny Truong <leonardtruong@fb.com>
2023-07-12 14:50:31 -0700

    Add unflattened tuples test for riscv_mini

---
+++ examples/riscv_mini/tests/test_unflattened_tuples.py
@@ -0,0 +1,13 @@
+from riscv_mini.core import Core
+import magma as m
+from magma.config import config as magma_config
+from magma.testing.utils import check_gold
+
+
+def test_riscv_mini_unflattened_tuples():
+    magma_config.compile_dir = 'callee_file_dir'
+    core = Core(32)
+    m.compile("build/test_riscv_mini_unflattened_tuples",
+              core, output="mlir-verilog")
+    assert check_gold(__file__,
+                      "test_riscv_mini_unflattened_tuples.v")
