58a15cad6a445f7a1271ebb91e707b3a9aeb1424
Lenny Truong <leonardtruong@fb.com>
2023-07-12 14:52:26 -0700

    Disallow local variables

--- examples/riscv_mini/tests/test_unflattened_tuples.py
+++ examples/riscv_mini/tests/test_unflattened_tuples.py
@@ -7,7 +7,7 @@ from magma.testing.utils import check_gold
 def test_riscv_mini_unflattened_tuples():
     magma_config.compile_dir = 'callee_file_dir'
     core = Core(32)
-    m.compile("build/test_riscv_mini_unflattened_tuples",
-              core, output="mlir-verilog")
+    m.compile("build/test_riscv_mini_unflattened_tuples", core,
+              output="mlir-verilog", disallow_local_variables=True)
     assert check_gold(__file__,
                       "test_riscv_mini_unflattened_tuples.v")
