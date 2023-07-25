d087af5d3315359da8f23f9ef18876343fa87363
Lenny Truong <leonardtruong@fb.com>
2023-07-25 14:34:37 -0700

    Fix gold

--- examples/riscv_mini/tests/test_unflattened_tuples.py
+++ examples/riscv_mini/tests/test_unflattened_tuples.py
@@ -10,8 +10,7 @@ def test_riscv_mini_unflattened_tuples():
     magma_config.compile_dir = 'callee_file_dir'
     tile = Tile(32)
     m.compile("build/test_riscv_mini_unflattened_tuples", tile,
-              output="mlir-verilog", disallow_local_variables=True,
-              check_circt_opt_version=False)
+              output="mlir-verilog", disallow_local_variables=True)
     assert check_gold(__file__,
                       "test_riscv_mini_unflattened_tuples.v")
     dirname = os.path.dirname(__file__)
