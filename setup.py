4639203d714ec34b8bb5321740027aa8eb027475
Lenny Truong <leonardtruong@fb.com>
2023-08-11 11:59:42 -0700

    Fix version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.3.1',
+    version='2.4.0',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
