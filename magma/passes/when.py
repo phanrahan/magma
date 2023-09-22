512c7e135726cf9319f182003cbd449f9a3cb17f
Lenny Truong <lenny@cs.stanford.edu>
2023-09-22 15:42:24 -0700

    [When] Split when blocks with cycles (#1309)
    Fixes #1269
    
    Adds a pass that splits when blocks with a cyclic dependency.

--- magma/passes/when.py
+++ magma/passes/when.py
@@ -1,11 +1,13 @@
 from magma.passes.passes import DefinitionPass, pass_lambda
+from magma.passes.split_when_utils import split_when_cycles
 from magma.primitives.when import WhenBuilder
+from magma.when import WhenBlock
 
 
 class WhenPass(DefinitionPass):
     def __call__(self, definition):
         with definition.open():
-            for builder in definition._context_._builders:
+            for builder in list(definition._context_._builders):
                 if isinstance(builder, WhenBuilder):
                     self.process_when_builder(builder, definition)
 
@@ -24,6 +26,11 @@ class EmitWhenAsserts(WhenPass):
         builder.emit_when_assertions(self._flatten_all_tuples)
 
 
+class SplitCycles(WhenPass):
+    def process_when_builder(self, builder, defn):
+        split_when_cycles(builder, defn)
+
+
 class FinalizeWhens(WhenPass):
     def process_when_builder(self, builder, defn):
         assert not builder._finalized
@@ -32,6 +39,7 @@ class FinalizeWhens(WhenPass):
 
 infer_latch_pass = pass_lambda(InferLatches)
 emit_when_assert_pass = pass_lambda(EmitWhenAsserts)
+split_cycles_pass = pass_lambda(SplitCycles)
 finalize_when_pass = pass_lambda(FinalizeWhens)
 
 
@@ -43,4 +51,5 @@ def run_when_passes(
     infer_latch_pass(main)
     if emit_when_assertions:
         emit_when_assert_pass(main, flatten_all_tuples)
+    split_cycles_pass(main)
     finalize_when_pass(main)
