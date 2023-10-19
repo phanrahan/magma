d580bb2e9212b7f4bb71b7d022f359b649d732c8
Lenny Truong <leonardtruong@meta.com>
2023-10-19 16:51:49 -0700

    Update line numbers

--- tests/test_circuit/gold/test_simple_def.v
+++ tests/test_circuit/gold/test_simple_def.v
@@ -1,12 +1,12 @@
-// Defined at tests/test_circuit/test_define.py:19
+// Defined at tests/test_circuit/test_define.py:18
 module main (input [1:0] I, output  O);
 wire  and2_O;
-// Instanced at tests/test_circuit/test_define.py:22
-// Argument I0(I[0]) wired at tests/test_circuit/test_define.py:24
-// Argument I1(I[1]) wired at tests/test_circuit/test_define.py:25
-// Argument O(and2_O) wired at tests/test_circuit/test_define.py:26
+// Instanced at tests/test_circuit/test_define.py:21
+// Argument I0(I[0]) wired at tests/test_circuit/test_define.py:23
+// Argument I1(I[1]) wired at tests/test_circuit/test_define.py:24
+// Argument O(and2_O) wired at tests/test_circuit/test_define.py:25
 And2 and2 (.I0(I[0]), .I1(I[1]), .O(and2_O));
-// Wired at tests/test_circuit/test_define.py:26
+// Wired at tests/test_circuit/test_define.py:25
 assign O = and2_O;
 endmodule
 
--- tests/test_circuit/gold/test_simple_def_class.v
+++ tests/test_circuit/gold/test_simple_def_class.v
@@ -1,12 +1,12 @@
-// Defined at tests/test_circuit/test_define.py:34
+// Defined at tests/test_circuit/test_define.py:33
 module Main (input [1:0] I, output  O);
 wire  and2_O;
-// Instanced at tests/test_circuit/test_define.py:37
-// Argument I0(I[0]) wired at tests/test_circuit/test_define.py:38
-// Argument I1(I[1]) wired at tests/test_circuit/test_define.py:39
-// Argument O(and2_O) wired at tests/test_circuit/test_define.py:40
+// Instanced at tests/test_circuit/test_define.py:36
+// Argument I0(I[0]) wired at tests/test_circuit/test_define.py:37
+// Argument I1(I[1]) wired at tests/test_circuit/test_define.py:38
+// Argument O(and2_O) wired at tests/test_circuit/test_define.py:39
 And2 and2 (.I0(I[0]), .I1(I[1]), .O(and2_O));
-// Wired at tests/test_circuit/test_define.py:40
+// Wired at tests/test_circuit/test_define.py:39
 assign O = and2_O;
 endmodule
 
