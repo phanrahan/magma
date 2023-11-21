0c16a0df01fe25a0608373fe1d4e4bb1b79980b1
Lenny Truong <leonardtruong@meta.com>
2023-11-21 10:58:34 -0800

    Update golds

--- tests/test_backend/test_mlir/golds/simple_lut.v
+++ tests/test_backend/test_mlir/golds/simple_lut.v
@@ -3,11 +3,11 @@ module LUT(
   output [7:0] O
 );
 
-  wire [3:0] _GEN = {1'h1, 1'h0, 1'h0, 1'h1};
-  wire [3:0] _GEN_0 = {1'h1, 1'h1, 1'h1, 1'h0};
-  wire [3:0] _GEN_1 = {1'h0, 1'h1, 1'h0, 1'h1};
-  wire [3:0] _GEN_2 = {1'h1, 1'h1, 1'h0, 1'h1};
-  wire [3:0] _GEN_3 = {1'h1, 1'h0, 1'h1, 1'h0};
+  wire [3:0] _GEN = '{1'h1, 1'h0, 1'h0, 1'h1};
+  wire [3:0] _GEN_0 = '{1'h1, 1'h1, 1'h1, 1'h0};
+  wire [3:0] _GEN_1 = '{1'h0, 1'h1, 1'h0, 1'h1};
+  wire [3:0] _GEN_2 = '{1'h1, 1'h1, 1'h0, 1'h1};
+  wire [3:0] _GEN_3 = '{1'h1, 1'h0, 1'h1, 1'h0};
   assign O = {1'h1, _GEN[I], _GEN_0[I], _GEN_1[I], 2'h3, _GEN_2[I], _GEN_3[I]};
 endmodule
 
