5103919cd187b683c1ef06486df97140fdb76923
Lenny Truong <leonardtruong@meta.com>
2023-11-29 12:01:53 -0800

    Stringify token

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,4 +5,4 @@ export PATH=$TRAVIS_BUILD_DIR/miniconda/bin:$PATH
 pip install twine
 python setup.py sdist
 
-twine upload dist/*.tar.gz -u __token__ -p $PYPI_TOKEN
+twine upload dist/*.tar.gz -u __token__ -p "${PYPI_TOKEN}"
