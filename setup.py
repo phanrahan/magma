821d87eb9237093f72ad60ba00da5fd6234607fd
rsetaluri <raj.setaluri@gmail.com>
2023-12-06 14:27:02 -0800

    Release 3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='magma-lang',
-    version='2.6.8',
+    version='3.0.0',
     url='https://github.com/phanrahan/magma',
     license='MIT',
     maintainer='Lenny Truong',
