99a663cfa8bab040040debbaa50512d589f8491e
Lenny Truong <leonard@truong.io>
2024-03-13 15:25:00 -0700

    Simplify test

--- examples/tests/test_even_odd_sort.py
+++ examples/tests/test_even_odd_sort.py
@@ -3,15 +3,11 @@ from ..even_odd_sort import Swap
 from hwtypes import BitVector
 
 
-def swap(I: BitVector[2]):
-    return BitVector[2]([I[0] & I[1], I[0] | I[1]])
-
-
 def test_swap():
     tester = f.Tester(Swap)
     for i in range(2):
         for j in range(2):
             tester.circuit.I = I = BitVector[2]([i, j])
             tester.eval()
-            tester.circuit.O.expect(swap(I))
+            tester.circuit.O.expect(BitVector[2](list(sorted(I.bits()))))
     tester.compile_and_run('verilator', magma_output="mlir-verilog")
