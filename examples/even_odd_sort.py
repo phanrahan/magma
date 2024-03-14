da9fee9b9eeb655d254b6a0fa21a58703ae10484
Lenny Truong <leonard@truong.io>
2024-03-13 17:30:20 -0700

    Add EvenOddSwaps

--- examples/even_odd_sort.py
+++ examples/even_odd_sort.py
@@ -3,6 +3,16 @@ import magma as m
 
 class Swap(m.Circuit):
     io = m.IO(I=m.In(m.Bits[2]), O=m.Out(m.Bits[2]))
-    # io.O @= m.bits([io.I.reduce_and(), io.I.reduce_or())
-    print(m.fork(m.Bit.And, m.Bit.Or))
     io.O @= m.uncurry(m.fork(m.Bit.And(), m.Bit.Or()))(io.I)
+    # Alternative, non-functional version
+    # io.O @= m.bits([io.I.reduce_and(), io.I.reduce_or())
+
+
+class EvenOddSwaps(m.Generator):
+    def __init__(self, n: int):
+        """n: even integer"""
+        self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bits[n]))
+        # n // 2 Swap instances, Bits[2] -> Bits[2]
+        # join, Array[n // 2, Bits[2]] -> Array[n // 2, Bits[2]]
+        # flat, Array[n]
+        self.io.O @= m.flat(m.join([Swap() for _ in range(n // 2)]))(self.io.I)
--- examples/tests/test_even_odd_sort.py
+++ examples/tests/test_even_odd_sort.py
@@ -1,13 +1,30 @@
+import pytest
+import tempfile
+import itertools
 import fault as f
-from ..even_odd_sort import Swap
+from ..even_odd_sort import Swap, EvenOddSwaps
 from hwtypes import BitVector
 
 
 def test_swap():
     tester = f.Tester(Swap)
-    for i in range(2):
-        for j in range(2):
-            tester.circuit.I = I = BitVector[2]([i, j])
-            tester.eval()
-            tester.circuit.O.expect(BitVector[2](list(sorted(I.bits()))))
-    tester.compile_and_run('verilator', magma_output="mlir-verilog")
+    for bits in itertools.product(range(2), repeat=2):
+        tester.circuit.I = I = BitVector[2](bits)
+        tester.eval()
+        tester.circuit.O.expect(BitVector[2](list(sorted(bits))))
+    with tempfile.TemporaryDirectory() as dir:
+        tester.compile_and_run('verilator', magma_output="mlir-verilog", directory=dir)
+
+
+@pytest.mark.parametrize('n', [4, 8, 16])
+def test_even_odd_swap(n):
+    tester = f.Tester(EvenOddSwaps(n))
+    for bits in itertools.islice(itertools.product(range(2), repeat=n), 16):
+        tester.circuit.I = I = BitVector[n](bits)
+        tester.eval()
+        expected = []
+        for i in range(n // 2):
+            expected.extend(list(sorted(bits[i * 2:i * 2 + 2])))
+        tester.circuit.O.expect(BitVector[n](expected))
+    with tempfile.TemporaryDirectory() as dir:
+        tester.compile_and_run('verilator', magma_output="mlir-verilog", directory=dir)
