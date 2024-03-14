cd7939b62998e82e3a305ee44e7b211740d162fa
Lenny Truong <leonard@truong.io>
2024-03-14 15:21:10 -0700

    Finish odd even sort

--- examples/even_odd_sort.py
+++ examples/even_odd_sort.py
@@ -1,6 +1,10 @@
 import magma as m
 
 
+def is_power_of_two(n):
+    return (n & (n - 1) == 0) and n != 0
+
+
 class Swap(m.Circuit):
     io = m.IO(I=m.In(m.Bits[2]), O=m.Out(m.Bits[2]))
     io.O @= m.uncurry(m.fork(m.Bit.And(), m.Bit.Or()))(io.I)
@@ -8,26 +12,36 @@ class Swap(m.Circuit):
     # io.O @= m.bits([io.I.reduce_and(), io.I.reduce_or())
 
 
+def swap(I: m.Bits[2]):
+    return Swap()(I)
+
+
 class EvenOddSwaps(m.Generator):
+    """Swap adjacent indices"""
+
     def __init__(self, n: int):
-        """n: even integer"""
+        assert n % 2 == 0, "Expected n to be even"
         self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bits[n]))
-        # n // 2 Swap instances, Bits[2] -> Bits[2]
-        # join, Array[n // 2, Bits[2]] -> Array[n // 2, Bits[2]]
-        # flat, Array[n]
-        self.io.O @= m.flat(m.join([Swap() for _ in range(n // 2)]))(self.io.I)
+        if n > 2:
+            x = m.flat(m.join(
+                [Swap() for _ in range((n - 2) // 2)]
+            ))(self.io.I[1:-1])
+            self.io.O[1:-1] @= x
+        self.io.O[0] @= self.io.I[0]
+        self.io.O[-1] @= self.io.I[-1]
 
 
-def flatten(l: list):
-    return sum(l, [])
+def flatten(t: tuple):
+    return sum(t, tuple())
 
 
 def riffle(n: int):
-    return flatten([[i, i + n // 2] for i in range(n // 2)])
+    assert n % 2 == 0, "Expected n to be even"
+    return flatten(tuple((i, i + n // 2) for i in range(n // 2)))
 
 
-def inverse(l: list):
-    return [l.index(i) for i in range(len(l))]
+def inverse(t: tuple):
+    return tuple(t.index(i) for i in range(len(t)))
 
 
 def unriffle(n: int):
@@ -39,3 +53,45 @@ class Permute(m.Generator):
         n = len(permutation)
         self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bits[n]))
         self.io.O @= m.bits([self.io.I[i] for i in permutation])
+
+
+def Riffle(n: int):
+    return Permute(riffle(n))
+
+
+def Unriffle(n: int):
+    return Permute(unriffle(n))
+
+
+class EvenOddMerger(m.Generator):
+    def __init__(self, n: int):
+        self.name = f'EvenOddMerger{n}'
+        assert is_power_of_two(n)
+        self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bits[n]))
+        if n == 2:
+            self.io.O @= swap(self.io.I)
+            return
+        even_merger = EvenOddMerger(n // 2)()
+        odd_merger = EvenOddMerger(n // 2)()
+        merger = m.flat(m.join(even_merger, odd_merger))
+        sorter = m.compose(EvenOddSwaps(n)(),
+                           Riffle(n)(),
+                           merger,
+                           Unriffle(n)())
+        self.io.O @= sorter(self.io.I)
+
+
+class EvenOddSorter(m.Generator):
+    def __init__(self, n: int):
+        # TODO: update fault to emit names after uniquification
+        self.name = f'EvenOddSorter{n}'
+        assert is_power_of_two(n)
+        self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bits[n]))
+        if n == 1:
+            self.io.O @= self.io.I
+            return
+        bot_sorter = EvenOddSorter(n // 2)()
+        top_sorter = EvenOddSorter(n // 2)()
+        sorter = m.flat(m.join(bot_sorter, top_sorter))
+        merger = EvenOddMerger(n)()
+        self.io.O @= m.compose(merger, sorter)(self.io.I)
--- examples/tests/test_even_odd_sort.py
+++ examples/tests/test_even_odd_sort.py
@@ -2,7 +2,9 @@ import pytest
 import tempfile
 import itertools
 import fault as f
-from ..even_odd_sort import Swap, EvenOddSwaps, Permute, riffle, unriffle
+from ..even_odd_sort import (
+    Swap, EvenOddSwaps, Permute, riffle, unriffle, EvenOddSorter
+)
 from hwtypes import BitVector
 
 
@@ -20,12 +22,12 @@ def test_swap():
 @pytest.mark.parametrize('n', [4, 8, 16])
 def test_even_odd_swap(n):
     tester = f.Tester(EvenOddSwaps(n))
-    for bits in itertools.islice(itertools.product(range(2), repeat=n), 16):
-        tester.circuit.I = I = BitVector[n](bits)
+    for _ in range(16):
+        tester.circuit.I = I = BitVector.random(n)
         tester.eval()
         expected = []
         for i in range(n // 2):
-            expected.extend(list(sorted(bits[i * 2:i * 2 + 2])))
+            expected.extend(list(sorted(I.bits()[i * 2:i * 2 + 2])))
         tester.circuit.O.expect(BitVector[n](expected))
     with tempfile.TemporaryDirectory() as dir:
         tester.compile_and_run('verilator', magma_output="mlir-verilog",
@@ -36,11 +38,24 @@ def test_even_odd_swap(n):
 @pytest.mark.parametrize('fn', [riffle, unriffle])
 def test_permute(n, fn):
     permutation = fn(n)
-    tester = f.Tester(Permute(tuple(permutation)))
+    tester = f.Tester(Permute(permutation))
     for i in range(4):
         tester.circuit.I = I = BitVector.random(n)
         tester.eval()
         tester.circuit.O.expect(BitVector[n]([I[i] for i in permutation]))
+    with tempfile.TemporaryDirectory() as dir:
+        tester.compile_and_run('verilator', magma_output="mlir-verilog",
+                               directory=dir)
+
+
+@pytest.mark.parametrize('n', [4, 8, 16])
+def test_even_odd_sorter(n):
+    tester = f.Tester(EvenOddSorter(n))
+    for _ in range(16):
+        tester.circuit.I = I = BitVector.random(n)
+        tester.print("I=%x\n", tester.circuit.I)
+        tester.eval()
+        tester.circuit.O.expect(BitVector[n](list(sorted(I.bits()))))
     with tempfile.TemporaryDirectory() as dir:
         tester.compile_and_run('verilator', magma_output="mlir-verilog",
                                directory=dir)
